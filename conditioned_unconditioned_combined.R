5c1277ebcd0dbe580a997ef5b222724480e1f359
Katie Millar <ajh20fhu@uea.ac.uk>
2023-11-07 10:14:16 +0000

    Splitting variables in combined data set.
    Using ifelse() to split the data in the combined data set, getting summaries of split variables, visualising the data, assumption checking data and doing statistical analysis.

--- conditioned_unconditioned_combined.R
+++ conditioned_unconditioned_combined.R
@@ -78,3 +78,116 @@ anova(conditioned_unconditioned_combined_glm)
 
 # emmeans for tukey
 emmeans::emmeans(conditioned_unconditioned_combined_glm, pairwise ~ diet)
+
+
+## Splitting the data
+
+## Splitting the data into variables
+unconditioned_conditioned_long$condition <- ifelse(unconditioned_conditioned_long$diet %in% c("4:1 Conditioned", "1:4 Conditioned"), "Conditioned", "Unconditioned")
+unconditioned_conditioned_long$nutrient_composition <- ifelse(unconditioned_conditioned_long$diet %in% c("4:1 Conditioned", "4:1 Unconditioned"), "4:1", "1:4")
+
+## summary of split variables data 
+
+# nutrient composition
+unconditioned_conditioned_nutrient_summary <- unconditioned_conditioned_long %>%  
+  group_by(nutrient_composition) %>% 
+  summarise(mean = mean(fly_numbers),
+            sd = sd(fly_numbers),
+            n = n(),
+            se = sd/sqrt(n))
+
+
+# conditioned or unconditioned 
+unconditioned_conditioned_combined_summary <- unconditioned_conditioned_long %>%  
+  group_by(condition) %>% 
+  summarise(mean = mean(fly_numbers),
+            sd = sd(fly_numbers),
+            n = n(),
+            se = sd/sqrt(n))
+
+
+## Visualising the data
+# creating a boxplot with the data that has been summarised
+unconditioned_and_conditioned_combined_nutrient_plot <- unconditioned_conditioned_nutrient_summary  %>% 
+  ggplot(aes(x = nutrient_composition, y = mean))+
+  geom_bar(stat = "identity",
+           fill = "skyblue",
+           colour = "#FF6863",
+           alpha = 0.6)+
+  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+                colour = "#FF6863",
+                width = 0.2)+
+  geom_jitter(data = unconditioned_conditioned_long,
+              aes(x = nutrient_composition,
+                  y = fly_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)+
+  ylim(0.0, 4.5)+
+  labs(x = "Diet Condition",
+       y = "Male Conditioned Diet Patches",
+       title = "Unconditioned and Conditioned Treatment Rep 1")+
+  theme_classic() 
+
+unconditioned_and_conditioned_combined_condition_plot <- unconditioned_conditioned_combined_summary %>% 
+  ggplot(aes(x = condition, y = mean))+
+  geom_bar(stat = "identity",
+           fill = "skyblue",
+           colour = "#FF6863",
+           alpha = 0.6)+
+  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+                colour = "#FF6863",
+                width = 0.2)+
+  geom_jitter(data =  unconditioned_conditioned_long,
+              aes(x = condition,
+                  y = fly_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)+
+  ylim(0.0, 4.5)+
+  labs(x = "Diet Condition",
+       y = "Male Conditioned Diet Patches",
+       title = "Unconditioned and Conditioned Treatment Rep 1")+
+  theme_classic() 
+
+
+#### visualising the separate variables 
+unconditioned_and_conditioned_combined_nutrient_plot + unconditioned_and_conditioned_combined_condition_plot
+
+
+#### Data Analysis of separated variables
+
+
+# First testing a linear model 
+conditioned_and_unconditioned_combined_separate_lm <- lm(fly_numbers ~  nutrient_composition * condition, data = unconditioned_conditioned_long)
+
+
+
+
+# Assumption Checking of the model 
+performance::check_model(conditioned_and_unconditioned_combined_separate_lm, check = c("qq")) # points are a bit dispersed
+performance::check_model(conditioned_and_unconditioned_combined_separate_lm, check = c("homogeneity")) # not too straight
+performance::check_model(conditioned_and_unconditioned_combined_separate_lm, check = c("linearity")) # very straight
+performance::check_model(conditioned_and_unconditioned_combined_separate_lm, check = c("outliers"))
+
+drop1(conditioned_and_unconditioned_combined_separate_lm, test = "F") ## checking lm incase
+
+# Trying a generalised linear model
+conditioned_and_unconditioned_combined_separate_glm01 <- glm(fly_numbers ~  nutrient_composition * condition, family = poisson(link = "log"), data = unconditioned_conditioned_long)
+summary(conditioned_and_unconditioned_combined_separate_glm01)  #underdispersed again? 
+
+# using quasi but need rec on what to do when there is underdispersion with count data? 
+conditioned_and_unconditioned_combined_separate_glm <- glm(fly_numbers ~  nutrient_composition * condition, family = quasipoisson(link = "log"), data = unconditioned_conditioned_long)
+
+
+
+performance::check_model(conditioned_and_unconditioned_combined_separate_glm, check = c("qq")) # looks pretty good
+performance::check_model(conditioned_and_unconditioned_combined_separate_glm, check = c("homogeneity")) # not too straight
+performance::check_model(conditioned_and_unconditioned_combined_separate_glm, check = c("outliers"))
+
+# glm mgiht be better, especially for qqplot? 
+drop1(conditioned_and_unconditioned_combined_separate_glm, test = "F") ## checking lm incase
+
+
