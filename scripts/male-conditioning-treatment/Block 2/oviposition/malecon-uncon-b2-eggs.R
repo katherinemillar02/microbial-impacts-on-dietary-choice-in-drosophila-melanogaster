d410b365fd42521cb8c07e4bc72f9fd8b11f48d2
Katie Millar <ajh20fhu@uea.ac.uk>
2023-11-27 11:55:26 +0000

    Adding and visualising data for male conditioned and unconditioned treatment - oviposition numbers of diet patches.

---
+++ scripts/male-conditioning-treatment/Block 2/oviposition/malecon-uncon-b2-eggs.R
@@ -0,0 +1,31 @@
+#### INSTALL PACKAGES 
+library(tidyverse)
+library(readxl)
+library(patchwork)
+library(colorBlindness)
+
+#### Upload data
+conduncond_b2_eggs <- read_excel("data/malecon-uncon-oviposition-b2.xlsx")
+
+## Making the data long 
+conduncond_b2_eggs_long <- conduncond_b2_eggs %>% 
+  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
+
+## Median boxplot --
+conduncond_b2_eggs_plot <- conduncond_b2_eggs_long %>% 
+  ggplot(aes(x = diet, y = egg_numbers, fill = diet))+ 
+  geom_boxplot()+
+  theme_classic()+
+  scale_fill_brewer(palette = "Set2")+
+  labs(x = "Diet Condition",
+       y = "Median number of eggs per diet patch", 
+       title = " Male Conditioned Treatment")+
+  theme(legend.position="none")+ 
+  ylim(-0.01,200)+
+  geom_jitter(data =  conduncond_b2_eggs_long,
+              aes(x = diet,
+                  y = egg_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)
