15232ff759fa0362a86ab366de22bc67a1a9d5d7
Katie Millar <ajh20fhu@uea.ac.uk>
2024-01-02 10:35:34 +0000

    Fixing script organisation

---
+++ scripts/male-conditioning-treatment/second-treatments/Block 2/feeding/one-four-b2.R
@@ -0,0 +1,71 @@
+#### INSTALL PACKAGES 
+library(tidyverse)
+library(readxl)
+library(patchwork)
+library(colorBlindness)
+
+#### Upload data
+one_to_four_b2 <- read_excel("data/1-4_t2b2_median.xlsx")
+
+## Making the data long 
+one_to_four_b2_long <- one_to_four_b2   %>% 
+  pivot_longer(cols = ("1:4 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers")
+
+
+one_to_four_b2_plot <- one_to_four_b2_long  %>% 
+  ggplot(aes(x = diet, y = fly_numbers, fill = diet))+ 
+  geom_boxplot()+
+  theme_classic()+
+  scale_fill_brewer(palette = "Set2")+
+  labs(x = "Diet Condition",
+       y = "Median number of flies per diet patch", 
+       title = "1:4 Diets")+
+  theme(legend.position="none")+ 
+  ylim(-0.01,1.5)+
+  geom_jitter(data =  one_to_four_b2_long ,
+              aes(x = diet,
+                  y = fly_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)
+
+
+
+
+## 
+## Statistical analysis ----
+# First testing a linear model 
+onetofour_b2_lm <- lm(fly_numbers ~  diet, data = one_to_four_b2_long)
+
+# Assumption Checking of the model 
+performance::check_model(onetofour_b2_lm, check = c("qq")) # qq looks awful
+performance::check_model(onetofour_b2_lm, check = c("homogeneity")) # bad
+performance::check_model(onetofour_b2_lm, check = c("linearity")) # bad
+performance::check_model(onetofour_b2_lm, check = c("outliers"))
+
+
+
+
+# Trying a generalised linear model
+onetofour_b2_glm_1  <- glm(fly_numbers ~  diet, family = poisson(link = "log"), data = one_to_four_b2_long)
+# underdispersed - trying quasipoisson
+onetofour_b2_glm_2  <- glm(fly_numbers ~  diet, family = quasipoisson(link = "log"), data = one_to_four_b2_long)
+
+
+performance::check_model(onetofour_b2_glm_2, check = c("qq")) # dots seem to match to line better than lm
+performance::check_model(onetofour_b2_glm_2, check = c("homogeneity")) # not flat but better
+performance::check_model(onetofour_b2_glm_2, check = c("outliers"))
+
+# glm may be better - but qq still looks bad 
+
+# summary function, shows t test
+summary(onetofour_b2_glm_2)
+
+# using anova 
+anova(onetofour_b2_glm_2)
+
+# emmeans for tukey analysis 
+emmeans::emmeans(onetofour_b2_glm_2, pairwise ~ diet)
+## no significant difference
+
