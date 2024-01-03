bfbed85b1209868782e5d8b4ec36d4e0d5253b2f
Katie Millar <ajh20fhu@uea.ac.uk>
2024-01-03 15:58:30 +0000

    Changing co-ordinates on plot

--- scripts/male-conditioning-treatment/second-treatments/Block 1/oviposition/fourone-onefour-oviposition.R
+++ scripts/male-conditioning-treatment/second-treatments/Block 1/oviposition/fourone-onefour-oviposition.R
@@ -23,7 +23,7 @@ onetofour_fourtoone_oviposition_b1_plot <-
        y = "Median number of flies per diet patch", 
        title = "1:4 and 4:1 Oviposition")+
   theme(legend.position="none")+ 
-  ylim(-0.01,100)+
+  ylim(-0.01,150)+
   geom_jitter(data =  ## Making the data long 
                 onetofour_fourtoone_oviposition_b1_long,
               aes(x = diet,
