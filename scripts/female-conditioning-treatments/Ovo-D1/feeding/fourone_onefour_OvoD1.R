0887bafa9b215b94fe4b703ee3b138b0e6d96ffa
Katie Millar <ajh20fhu@uea.ac.uk>
2024-01-29 11:57:06 +0000

    Data analysis for 4:1/1:4 Conditioned/Unconditioned Assay. Results from a glm with quasipoisson show there is a significant difference between 4:1 Conditioned and Unconditioned, but no significant difference between 1:4 Conditioned and Unconditioned.

--- scripts/female-conditioning-treatments/Ovo-D1/feeding/fourone_onefour_OvoD1.R
+++ scripts/female-conditioning-treatments/Ovo-D1/feeding/fourone_onefour_OvoD1.R
@@ -33,3 +33,44 @@ fourone_onefour_OvoD1_plot <- fourone_onefour_OvoD1_long %>%
               colour = "#3a3c3d",
               width = 0.2,
               shape = 21)
+
+
+
+#### Data Analysis 
+
+## 
+## Statistical analysis ----
+# First testing a linear model 
+onefour_fourone_OvoD1_lm <- lm(fly_numbers ~  diet, data = fourone_onefour_OvoD1_long)
+
+# Assumption Checking of the model 
+performance::check_model(onefour_fourone_OvoD1_lm , check = c("qq")) # qq looks quite okay.
+performance::check_model(onefour_fourone_OvoD1_lm , check = c("homogeneity")) # bad, quite windy.
+performance::check_model(onefour_fourone_OvoD1_lm , check = c("linearity")) # quite straight, could be okay.
+performance::check_model(onefour_fourone_OvoD1_lm , check = c("outliers")) # don't see anything particularly awful. 
+
+
+
+
+# Trying a generalised linear model
+onefour_fourone_OvoD1_glm_1 <- glm(fly_numbers ~  diet, family = poisson(link = "log"), data = fourone_onefour_OvoD1_long)
+# overdispersed - 
+onefour_fourone_OvoD1_glm_2 <- glm(fly_numbers ~  diet, family = quasipoisson(link = "log"), data = fourone_onefour_OvoD1_long)
+
+
+performance::check_model(onefour_fourone_OvoD1_glm_2, check = c("qq")) # dots are on the line, bit shaky. 
+performance::check_model(onefour_fourone_OvoD1_glm_2, check = c("homogeneity")) # line is a bit windy, better than lm. 
+performance::check_model(onefour_fourone_OvoD1_glm_2, check = c("outliers")) # seems okay/ don't see anything particularly awful. 
+
+# glm with quasipoisson may be the better model out of the ones assumption/ performance checked. 
+
+# summary function, shows t test
+summary(onefour_fourone_OvoD1_glm_2)
+
+# using anova 
+anova(onefour_fourone_OvoD1_glm_2)
+
+# emmeans for tukey analysis 
+emmeans::emmeans(onefour_fourone_OvoD1_glm_2, pairwise ~ diet)
+## This shows no significant difference between 1:4 Conditioned and 1:4 Unconditioned. 
+## Shows a significant difference between 4:1 Conditioned and Unconditioned. 
