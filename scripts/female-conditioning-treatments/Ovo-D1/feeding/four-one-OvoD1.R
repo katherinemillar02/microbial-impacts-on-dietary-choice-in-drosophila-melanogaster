5e686431c7165eb7394da86cbbd9e2473aab2712
Katie Millar <ajh20fhu@uea.ac.uk>
2024-01-29 11:46:25 +0000

    Data analysis for 4:1 Conditioned and Unconditioned feeding, might have to play around with the data a bit for this one, but need to think carefully.

--- scripts/female-conditioning-treatments/Ovo-D1/feeding/four-one-OvoD1.R
+++ scripts/female-conditioning-treatments/Ovo-D1/feeding/four-one-OvoD1.R
@@ -34,4 +34,46 @@ four_to_one_OvoD1_plot <- four_to_one_OvoD1_long  %>%
               shape = 21)
 
 
+## might have messed up plates, need to think about this. 
+
+
+#### Data Analysis 
+
+## 
+## Statistical analysis ----
+# First testing a linear model 
+four_to_one_OvoD1_lm <- lm(fly_numbers ~  diet, data = four_to_one_OvoD1_long)
+
+# Assumption Checking of the model 
+performance::check_model(four_to_one_OvoD1_lm, check = c("qq")) # qq looks okay, dots are a bit spaced out, could be a lot better. 
+performance::check_model(four_to_one_OvoD1_lm, check = c("homogeneity")) # okay, quite straight.
+performance::check_model(four_to_one_OvoD1_lm, check = c("linearity")) # very windy, quite bad. 
+performance::check_model(four_to_one_OvoD1_lm, check = c("outliers")) # don't see anything particularly awful. 
+
+
+
+
+# Trying a generalised linear model
+four_to_one_OvoD1_glm_1 <- glm(fly_numbers ~  diet, family = poisson(link = "log"), four_to_one_OvoD1_long)
+# underdispersed - trying quasipoisson? not the best thing to do though
+four_to_one_OvoD1_glm_2 <- glm(fly_numbers ~  diet, family = quasipoisson(link = "log"), four_to_one_OvoD1_long)
+
+
+performance::check_model(four_to_one_OvoD1_glm_2, check = c("qq")) # looks pretty much the same as lm, maybe (slightly) better. 
+performance::check_model(four_to_one_OvoD1_glm_2, check = c("homogeneity")) # line is not flat, slopes down. 
+performance::check_model(four_to_one_OvoD1_glm_2, check = c("outliers")) # seems okay? 
+
+# lm may be better? quite similar though.  
+
+# summary function, shows t test
+summary(four_to_one_OvoD1_glm_2)
+
+# using anova 
+anova(four_to_one_OvoD1_glm_2)
+
+# emmeans for tukey analysis 
+emmeans::emmeans(four_to_one_OvoD1_glm_2, pairwise ~ diet)
+## This shows there is not a significant difference between 4:1 Conditioned and 4:1 Unconditioned. 
+
+
 
