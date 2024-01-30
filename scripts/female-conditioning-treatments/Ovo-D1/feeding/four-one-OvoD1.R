eaa55021671cf203bd19e1319287fd9b8304085c
Katie Millar <ajh20fhu@uea.ac.uk>
2024-01-30 12:39:19 +0000

    Adding more data analysis of the updated data to female conditioning.

--- scripts/female-conditioning-treatments/Ovo-D1/feeding/four-one-OvoD1.R
+++ scripts/female-conditioning-treatments/Ovo-D1/feeding/four-one-OvoD1.R
@@ -78,3 +78,69 @@ emmeans::emmeans(four_to_one_OvoD1_glm_2, pairwise ~ diet)
 
 
 
+
+
+#### Second analysis of results 
+#### Upload data
+four_to_one_OvoD1_2 <- read_excel("data/OvoD1-4_1-median_2.xlsx")
+
+## Making the data long 
+four_to_one_OvoD1_2_long <- four_to_one_OvoD1_2   %>% 
+  pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "fly_numbers")
+
+## Visualising the data 
+four_to_one_OvoD1_2_plot <- four_to_one_OvoD1_2_long   %>% 
+  ggplot(aes(x = diet, y = fly_numbers, fill = diet))+ 
+  geom_boxplot()+
+  theme_classic()+
+  scale_fill_brewer(palette = "Set2")+
+  labs(x = "Diet Condition",
+       y = "Median number of flies per diet patch", 
+       title = "1:4 Diets")+
+  theme(legend.position="none")+ 
+  ylim(-0.01,4)+
+  geom_jitter(data =  four_to_one_OvoD1_2_long,
+              aes(x = diet,
+                  y = fly_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)
+
+
+
+## 
+## Statistical analysis ----
+# First testing a linear model 
+four_to_one_OvoD1_2_lm <- lm(fly_numbers ~  diet, data = four_to_one_OvoD1_2_long)
+
+# Assumption Checking of the model 
+performance::check_model(four_to_one_OvoD1_2_lm, check = c("qq")) # qq looks okay, dots are a bit spaced out, could be a lot better. 
+performance::check_model(four_to_one_OvoD1_2_lm, check = c("homogeneity")) # quite windy
+performance::check_model(four_to_one_OvoD1_2_lm, check = c("linearity")) # quite windy, not the best
+performance::check_model(four_to_one_OvoD1_2_lm, check = c("outliers")) # don't see anything particularly awful. 
+
+
+
+
+# Trying a generalised linear model
+four_to_one_OvoD1_2_glm_1 <- glm(fly_numbers ~  diet, family = poisson(link = "log"), four_to_one_OvoD1_2_long)
+# overdispersed - trying quasipoisson? not the best thing to do though
+four_to_one_OvoD1_2_glm_2 <- glm(fly_numbers ~  diet, family = quasipoisson(link = "log"), four_to_one_OvoD1_2_long)
+
+
+performance::check_model(four_to_one_OvoD1_2_glm_2 , check = c("qq")) # lines go a bit sideways compared to lm.
+performance::check_model(four_to_one_OvoD1_2_glm_2 , check = c("homogeneity")) # line is flat almost, bit high. 
+performance::check_model(four_to_one_OvoD1_2_glm_2 , check = c("outliers")) # seems okay? 
+
+# lm may be better? quite similar though.  
+
+# summary function, shows t test
+summary(four_to_one_OvoD1_2_glm_2)
+
+# using anova 
+anova(four_to_one_OvoD1_2_glm_2)
+
+# emmeans for tukey analysis 
+emmeans::emmeans(four_to_one_OvoD1_2_glm_2, pairwise ~ diet)
+## This shows there is not a significant difference between 4:1 Conditioned and 4:1 Unconditioned. 
