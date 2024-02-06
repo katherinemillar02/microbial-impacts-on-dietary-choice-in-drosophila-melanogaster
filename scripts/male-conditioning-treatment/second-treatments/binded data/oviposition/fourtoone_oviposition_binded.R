b54008eada213395742c561aaee32944c740306e
Katie Millar <ajh20fhu@uea.ac.uk>
2024-02-06 18:13:03 +0000

    Updating script with new data

--- scripts/male-conditioning-treatment/second-treatments/binded data/oviposition/fourtoone_oviposition_binded.R
+++ scripts/male-conditioning-treatment/second-treatments/binded data/oviposition/fourtoone_oviposition_binded.R
@@ -28,6 +28,38 @@ male_t2_bind_conditioned_4to1_plot  <-
               width = 0.2,
               shape = 21)
 
+########
+## new plot design
+male_oviposition_1 <-
+  ggplot(fourone_oviposition_t2, aes(x = diet, y = egg_numbers, pattern = diet, fill = diet)) +
+  geom_boxplot(aes(fill = diet))+
+  scale_fill_manual(name = "Diet", values = c("#fe7669","#fe7669" )) +
+  geom_boxplot_pattern(position = position_dodge(preserve = "single"), 
+                       color = "black",
+                       pattern_fill = "white",
+                       pattern_angle = 45,
+                       pattern_density = 0.1,
+                       pattern_spacing = 0.025,
+                       pattern_key_scale_factor = 0.6) +
+  scale_pattern_manual(values=c("stripe", "none", "stripe", "none")) +
+  ylim(0,250)+
+  theme_classic()+
+  theme(legend.position = "none")+
+  geom_jitter(data = fourone_oviposition_t2 ,
+              aes(x = diet,
+                  y = egg_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21) +
+  labs(x = "Diet Condition",
+       y = "Median number of flies per diet patch", 
+       title = "")+
+  theme(legend.position="none")+
+  theme(axis.text = element_text(size=6, angle = 0, hjust =0.5))
+
+
+
 
 ## 
 #### Data Analysis ----
