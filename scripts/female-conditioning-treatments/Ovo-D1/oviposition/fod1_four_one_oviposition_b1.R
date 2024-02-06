c1080bde88d52a99a91f4a8fe90e99eafce0b9ae
Katie Millar <ajh20fhu@uea.ac.uk>
2024-02-06 18:12:00 +0000

    Updating script with new data

--- scripts/female-conditioning-treatments/Ovo-D1/oviposition/fod1_four_one_oviposition_b1.R
+++ scripts/female-conditioning-treatments/Ovo-D1/oviposition/fod1_four_one_oviposition_b1.R
@@ -11,7 +11,7 @@ fourone_OvoD1_oviposition <- read_excel("data/female_conditioning/ovod1/block_1/
 
 ## Making the data long 
 fourone_OvoD1_oviposition_long <- fourone_OvoD1_oviposition   %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "fly_numbers")
+  pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
 
 
 ## Visualising the data 
@@ -33,11 +33,58 @@ fourone_OvoD1_oviposition_plot <- fourone_OvoD1_oviposition_long %>%
               shape = 21)
 
 
+## Visualising the data 
+onefour_OvoD1_oviposition_plot <- onefour_OvoD1_oviposition_long %>% 
+  ggplot(aes(x = diet, y = fly_numbers, fill = diet))+ 
+  geom_boxplot(outlier.shape  = NA)+
+  theme_classic()+
+  scale_fill_brewer(palette = "Set3")+
+  labs(x = "Diet Condition",
+       y = "Median number of flies per diet patch", 
+       title = "1:4 OvoD1 Female Conditioned and Unconditioned Oviposition")+
+  theme(legend.position="none")+ 
+  ylim(-0.01,250)+
+  geom_jitter(aes(x = diet,
+                  y = fly_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              #width = 0.2,
+              shape = 21)
+
+
+fod1_oviposition_0 <-
+  ggplot( fourone_OvoD1_oviposition_long, aes(x = diet, y = egg_numbers, pattern = diet, fill = diet)) +
+  geom_boxplot(aes(fill = diet))+
+  scale_fill_manual(name = "Diet", values = c("#fe7669","#fe7669" )) +
+  geom_boxplot_pattern(position = position_dodge(preserve = "single"), 
+                       color = "black",
+                       pattern_fill = "white",
+                       pattern_angle = 45,
+                       pattern_density = 0.1,
+                       pattern_spacing = 0.025,
+                       pattern_key_scale_factor = 0.6) +
+  scale_pattern_manual(values=c("stripe", "none", "stripe", "none")) +
+  ylim(0,250)+
+  theme_classic()+
+  theme(legend.position = "none")+
+  geom_jitter(data =  fourone_OvoD1_oviposition_long ,
+              aes(x = diet,
+                  y = egg_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21) +
+  labs(x = "Diet Condition",
+       y = "Median number of eggs per diet patch", 
+       title = "")+
+  theme(legend.position="none")+
+  theme(axis.text = element_text(size=6, angle = 0, hjust =0.5))
 
 
 
 
-#### Data Analysis 
+
+ #### Data Analysis 
 
 ## 
 ## Statistical analysis ----
