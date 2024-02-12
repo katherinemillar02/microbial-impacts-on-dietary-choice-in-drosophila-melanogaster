59c5321a531e2c92709d8025c6efd644b2f6a212
Katie Millar <ajh20fhu@uea.ac.uk>
2024-02-12 11:51:51 +0000

    Doing virgin visualisation using functiins to lessen lines of code.

--- scripts/female-conditioning-treatments/virgin_conditioning.R
+++ scripts/female-conditioning-treatments/virgin_conditioning.R
@@ -9,39 +9,32 @@ library(colorBlindness)
 library(ggplot2)
 library(ggpattern)
 
+
+
 #### Virgin visualisation
 #### Upload data for plot - median calculated 
 four_to_one_virgin <- read_excel("data/female_conditioning/virgin/rawresults_4-1_virgin.xlsx")
 
+one_to_four_virgin <- read_excel("data/female_conditioning/virgin/rawresults_1-4_virgin.xlsx")
+
 ## Making the median data long 
 four_to_one_virgin_long <- four_to_one_virgin  %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "fly_numbers") %>% 
-  group_by(plate) %>%   mutate(fly_numbers = median(fly_numbers))
+  pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
+ 
 
-# Calculate median for each plate and diet
-four_to_one_virgin_summary <- four_to_one_virgin_long %>%  
-  group_by(plate, diet) %>% 
-  summarise(fly_numbers = median(fly_numbers))  # Add median calculation
+one_to_four_virgin_long <-one_to_four_virgin  %>% 
+  pivot_longer(cols = ("1:4 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
 
-# Visualising the data - median data 
-four_to_one_virgin_plot <- four_to_one_virgin_summary  %>% 
-  ggplot(aes(x = diet, y = fly_numbers, fill = diet))+ 
-  geom_boxplot()+
-  theme_classic()+
-  scale_fill_brewer(palette = "Set2")+
-  labs(x = "Diet Condition",
-       y = "Median number of flies per diet patch", 
-       title = "4:1 OvoD1 Female Conditioned and Unconditioned Feeding")+
-  theme(legend.position = "none")+ 
-  ylim(-0.01,6)+
-  geom_jitter(data = four_to_one_virgin_summary,
-              aes(x = diet,
-                  y = fly_numbers),
-              fill = "skyblue",
-              colour = "#3a3c3d",
-              width = 0.2,
-              shape = 21)
+## creating a function to pull the median
+fly_numbers_summary()<- function(data, group_col) {
+  summary <- data %>%
+    group_by(plate, {{ group_col }}) %>%
+    summarise(median = median(fly_numbers))
+  return(summary)
+}
 
+four_one_virgin_summary <- fly_numbers_summary(four_to_one_virgin_long, diet)
+one_four_virgin_summary <- fly_numbers_summary(one_to_four_virgin_long, diet)
 
 
 #### Creating a function for a plot which will allow me to run the same code for different datasets 
@@ -52,7 +45,7 @@ plot_virgin <- function(summary_data) {
     scale_fill_brewer(palette = "Set2")+
     labs(x = "Diet Condition",
          y = "Median number of flies per diet patch", 
-         title = "4:1 OvoD1 Female Conditioned and Unconditioned Feeding")+
+         title = "")+
     theme(legend.position = "none")+ 
     ylim(-0.01, 6)+
     geom_jitter(data = summary_data,
@@ -65,9 +58,9 @@ plot_virgin <- function(summary_data) {
 }
 
 ## Running different data sets: 
-plot_virgin(four_to_one_virgin_summary) # 4:1
-plot_virgin(one_to_four_virgin_summary) 
-plot_virgin(fourone_onefour_virgin_summary) 
+plot_virgin(four_one_virgin_summary) # 4:1
+plot_virgin(one_four_virgin_summary) # 1:4 
+
 
 
 
