07cc47b78e73a22c2936732730a34efa1c5de8fd
Katie Millar <ajh20fhu@uea.ac.uk>
2024-02-12 11:23:56 +0000

    Working on a virgin conditioning visualisation script.

--- scripts/female-conditioning-treatments/virgin_conditioning.R
+++ scripts/female-conditioning-treatments/virgin_conditioning.R
@@ -9,7 +9,7 @@ library(colorBlindness)
 library(ggplot2)
 library(ggpattern)
 
-
+#### Virgin visualisation
 #### Upload data for plot - median calculated 
 four_to_one_virgin <- read_excel("data/female_conditioning/virgin/rawresults_4-1_virgin.xlsx")
 
@@ -43,29 +43,31 @@ four_to_one_virgin_plot <- four_to_one_virgin_summary  %>%
               shape = 21)
 
 
-####### Virgin Conditioning - 1:4  ####### 
 
+#### Creating a function for a plot which will allow me to run the same code for different datasets 
+plot_virgin <- function(summary_data) {
+  ggplot(summary_data, aes(x = diet, y = fly_numbers, fill = diet))+ 
+    geom_boxplot()+
+    theme_classic()+
+    scale_fill_brewer(palette = "Set2")+
+    labs(x = "Diet Condition",
+         y = "Median number of flies per diet patch", 
+         title = "4:1 OvoD1 Female Conditioned and Unconditioned Feeding")+
+    theme(legend.position = "none")+ 
+    ylim(-0.01, 6)+
+    geom_jitter(data = summary_data,
+                aes(x = diet,
+                    y = fly_numbers),
+                fill = "skyblue",
+                colour = "#3a3c3d",
+                width = 0.2,
+                shape = 21)
+}
 
-four_to_one_virgin_plot <- four_to_one_virgin_summary  %>% 
-  ggplot(aes(x = diet, y = fly_numbers, fill = diet))+ 
-  geom_boxplot()+
-  theme_classic()+
-  scale_fill_brewer(palette = "Set2")+
-  labs(x = "Diet Condition",
-       y = "Median number of flies per diet patch", 
-       title = "4:1 OvoD1 Female Conditioned and Unconditioned Feeding")+
-  theme(legend.position = "none")+ 
-  ylim(-0.01,6)+
-  geom_jitter(data = four_to_one_virgin_summary,
-              aes(x = diet,
-                  y = fly_numbers),
-              fill = "skyblue",
-              colour = "#3a3c3d",
-              width = 0.2,
-              shape = 21)
-
-
-
+## Running different data sets: 
+plot_virgin(four_to_one_virgin_summary) # 4:1
+plot_virgin(one_to_four_virgin_summary) 
+plot_virgin(fourone_onefour_virgin_summary) 
 
 
 
