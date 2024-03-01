12a9a75170d2228a390dc830c1049a4b6a5ab6d9
Katie Millar <ajh20fhu@uea.ac.uk>
2024-03-01 09:10:15 +0000

    Reading in oviposition data for virgin treatments and virgin analysis.

--- data_analysis_oviposition.R
+++ data_analysis_oviposition.R
@@ -5,8 +5,47 @@ library(readxl)
 ## OVIPOSITION
 
 #### VIRGIN FEMALE  ----
+pathvirginoviposition <- "data/female_conditioning/virgin"
 
-#### NEED TO ADD DATA
+# reading in oviposition data for virgin 
+read_raw_virgin_oviposition <-function(path = pathvirginoviposition, pattern_to_exclude = "4-1_1-4_oviposition"){
+  list_of_files <- list.files(path = pathvirginoviposition,
+                              pattern = "oviposition", full.names = T)
+  
+  list_of_files <- list_of_files[!grepl(pattern_to_exclude, list_of_files)]
+  
+  list_of_files %>%
+    map_dfr(~read_excel(.x) %>% mutate(id = .x, .before = "plate")) %>% #.x is the vector you're inputting? so adding a section of id is the name of the data and the folder its in 
+    pivot_longer(cols = c(3:6), ## this will put all the data files into long data, easy to read
+                 names_to = "diet",
+                 values_to = "egg_numbers") %>%
+    drop_na(egg_numbers) ## because the data files are being combined, dropping na where certain data scripts should not be included
+}
+
+
+# using the function to find the path 
+read_raw_virgin_oviposition(pathvirginoviposition)
+
+
+## creating an actual data set that will read the paths
+# first data frame - purr package 
+df_virgin_oviposition <- pathvirginoviposition %>% 
+  map_df(~read_raw_virgin_oviposition(.x)) #.x is a string or NULL - only applies to dfr apparently
+
+# uses what was generated with "df"
+df2_virgin_oviposition <- df_virgin_oviposition %>%
+  separate(diet, into = c("ratio", "condition"), sep = " ") %>%#separate will turn a single factor column into multiple columns
+  group_by(id, plate, ratio, condition) %>% ## group by what is split
+  summarise(count = sum(egg_numbers)) %>% 
+  pivot_wider(names_from = "condition", values_from = "count") 
+
+## new data 
+df2_virgin_oviposition # does it recognise condition from the long data
+
+
+
+
+
 
 #### OVOD1 FEMALE ----
 pathovod1oviposition <- "data/female_conditioning/ovod1/block_1"
