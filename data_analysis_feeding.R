1779eba01e0c3bde2c3c469c4842d3ddc50cc47c
Katie Millar <ajh20fhu@uea.ac.uk>
2024-03-04 10:19:26 +0000

    Adding comments to step through the data analysis.

--- data_analysis_feeding.R
+++ data_analysis_feeding.R
@@ -6,7 +6,7 @@ library(MASS)
 library(performance)
 library(pscl)
 library(DHARMa)
-
+library(glmmTMB)
 
 #### DATA UPLOAD 
 
@@ -237,6 +237,8 @@ fourone_onefour_virgin_summary <- fly_numbers_summary(fourone_onefour_virgin_lon
 
 
 
+
+
 #### Data Analysis Feeding 
 
 ## For the 4:1/1:4 Assay only 
@@ -248,32 +250,33 @@ fourone_onefour_virgin_summary <- fly_numbers_summary(fourone_onefour_virgin_lon
 fourone_onefour_male_b1 <- read_excel("data/male_conditioning/treatment_2/rawdata_m4-1_1-4_t2b1.xlsx")
 fourone_onefour_male_b2 <- read_excel("data/male_conditioning/treatment_2/rawdata_m4-1_1-4_t2b2.xlsx")
 
+# mutating a variable for block 
 fourone_onefour_male_b1 <- fourone_onefour_male_b1  %>% mutate(block = "one")
 fourone_onefour_male_b2 <- fourone_onefour_male_b2  %>% mutate(block = "two")
 
 # binding the data
 fourone_onefour_male <- rbind(fourone_onefour_male_b1, fourone_onefour_male_b2)
-# 4:1 and 1:4 
-fourone_onefour_male_long <- fourone_onefour_male %>% 
+
+
+# 4:1 and 1:4 - making the data long 
+combined_m <- fourone_onefour_male %>% 
   pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers")
 
 
-
-
 ## Doing poisson to begin with 
-male_all_assay <- glm(fly_numbers ~ diet * block, family = poisson, data = fourone_onefour_male_long)
+glm_poisson_m <- glm(fly_numbers ~ diet * block, family = poisson, data = combined_m)
 
-# doing assumption checks 
-performance::check_model(male_all_assay, check = c("qq")) # qq looks ok?
-performance::check_model(male_all_assay, check = c("outliers")) # weird 
+# doing assumption checks before seeing if overdispersion can be checked
+performance::check_model(glm_poisson_m, check = c("qq")) # qq looks ok?
+performance::check_model(glm_poisson_m, check = c("outliers")) # weird 
+performance::check_model(glm_poisson_m, check = c("homogeneity")) # does this look okay maybe? 
 
-# should I not have block in their yet?
 
 # checking for overdispersion
-summary(male_all_assay) # A bit overdispersed 
+summary(glm_poisson_m) # A bit overdispersed 
 
 # Look for 0s 
-check_zeroinflation(male_all_assay)
+check_zeroinflation(glm_poisson_m)
 # there is zero inflation 
 
 # There is overdispersion, this could be independent of or because of the zero inflation
@@ -281,92 +284,65 @@ check_zeroinflation(male_all_assay)
 ## trying to look for significance of experiment with a negative binomial model
 
 # negative binomial model
-male_all_assay_nb_2 <- glm.nb(fly_numbers ~ diet * block, data = fourone_onefour_male_long)
+glm.nb_m <- glm.nb(fly_numbers ~ diet * block, data = combined_m)
+
+# assumption checks for negative binomial model
+# easystats
+performance::check_model(glm.nb_m, check = c("qq")) # looks the same as glm poisson. 
+performance::check_model(glm.nb_m, check = c("outliers")) # weird 
+performance::check_model(glm.nb_m, check = c("homogeneity")) # looks the same again 
+
+# checking for overdispersion 
+summary(glm.nb_m)
+
+# no evidence of overdispersion but still highly skewed residuals do random effects help?
+
+# trying a mixed model with GLM and poisson 
+glm_mm_m <- glmmTMB(fly_numbers ~ diet * block +(1|factor(block)/plate) + (1|observation), family = poisson, data = combined_m)
+
+performance::check_model(glm_mm_m, check = c("qq")) # qq looks a lot better
+
+check_zeroinflation(glm_mm_m) # there is still zero inflation 
+
+# usind DHARMa to look at residuals of new model 
+simulateResiduals(fittedModel = glm_mm_m, plot = T)
 
 
-# zero inflated poisson
+# trying a zero inflated poisson model with poisson, and mixed model 
+zi.p_m <- zeroinfl(fly_numbers ~ diet * block | diet * block, dist = "poisson", link = "logit", data = combined_m)
 
-library(pscl)
 
-zip1 <- zeroinfl(fly_numbers ~ diet * block | diet * block, dist = "poisson", link = "logit", data = fourone_onefour_male_long)
+# trying a zero inflated negative binomial model 
+zi.nb_m <- zeroinfl(fly_numbers ~ diet * block | diet * block, dist = "negbin", link = "logit", data = combined_m)
 
-# zero inflated negative binomial
-
-zinb1 <- zeroinfl(fly_numbers ~ diet * block | diet * block, dist = "negbin", link = "logit", data = fourone_onefour_male_long)
-
-AIC(male_all_assay, male_all_assay_nb_2, zip1, zinb1)
+# comparing AIC of the different models 
+AIC(glm_poisson_m, glm.nb_m, glm_mm_m, zi.p_m, zi.nb_m)
+# glm_mm_m has the lowest AIC, but AIC values close to each other are basically the same (like 5 values?)
 
 # check residuals of zip
 
-sresid <- residuals(zip1, type = "pearson")
-
-pred <- fitted(zip1)
-
+sresid <- residuals(zi.p_m, type = "pearson")
+pred <- fitted(zi.p_m)
 hist(sresid)
-
 plot(sresid ~  pred)
 
-# no evidence of overdispersion but still highly skewed residuals do random effects help?
-library(glmmTMB)
-
-male_all_assay <- glmmTMB(fly_numbers ~ diet * block +(1|factor(block)/plate) + (1|observation), family = poisson, data = fourone_onefour_male_long)
-
-simulateResiduals(fittedModel = male_all_assay, plot = T)
-
-# using drop1 to look for any interaction effect of block
-drop1(male_all_assay, test = "F") # no interaction effect
-
-# dropping block from the model 
-male_all_assay_nb <- glm.nb(fly_numbers ~ diet, data = fourone_onefour_male_long)
 
 
-# looking at the analysis results from this model 
-summary(male_all_assay_nb )
 
-## assumption checks from new model
-performance::check_model(male_all_assay_nb, check = c("qq")) # qq doesn't look great
-performance::check_model(male_all_assay_nb, check = c("outliers"))
+# looking at the results
+summary(zi.p_m) # says block is no significant 
+summary(glm_mm_m) # says block is not significant 
 
-## Checking the AIC of the model 
-model_performance(male_all_assay_nb, check = c("AIC")) # AIC quite high
+# dropping block from the model (zero inflation poisson)
+zi.p_m_2 <- zeroinfl(fly_numbers ~ diet | diet , dist = "poisson", link = "logit", data = combined_m)
 
-## Using the DHARMa package to check the model 
-testDispersion(male_all_assay_nb) # shows how powerful the test is?
-
-# simulating tandom residuals? 
-simulationOutput_male <- simulateResiduals(fittedModel = male_all_assay_nb, plot = F)
-
-
-plotQQunif(simulationOutput_male)
-plotResiduals(simulationOutput_male) 
-testQuantiles(simulationOutput_male) # why NULL?
-
-
-## Randomising quantile residuals
-residuals(simulationOutput)
-# calculates randomised quantile residuals  
-
-residuals(simulationOutput_male, quantileFunction = qnorm, outlierValues = c(-7,7))
+# dropping block from the model (mixed model)
+glm_mm_m_2 <- glmmTMB(fly_numbers ~ diet  +(1|plate) + (1|observation), family = poisson, data = combined_m)
 
 
 
 
 
-
-## Other option is zero inflated model? 
-
-# trying a zero inflated model
-male_all_assay_zi <- zeroinfl(fly_numbers ~ diet, data = fourone_onefour_male_long)
-
-
-summary(male_all_assay_zi) # completely changes results?? 
-
-
-# Compare Performance 
-compare_performance(male_all_assay, male_all_assay_nb, male_all_assay_zi, rank = TRUE, verbose = FALSE)
-
-# Is negative binomial the best? 
-emmeans::emmeans(male_all_assay_nb, pairwise ~ diet)
 
 
 
