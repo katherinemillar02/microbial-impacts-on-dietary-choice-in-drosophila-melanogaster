7148565f1825333dc739cf42ab93284ffb505849
Katie Millar <ajh20fhu@uea.ac.uk>
2024-03-05 11:00:51 +0000

    Adding block 3 virgin in to see new data visualisation.

--- data_visualisation_feeding.R
+++ data_visualisation_feeding.R
@@ -29,7 +29,7 @@ fourone_onefour_ovod1 <- rbind(fourone_onefour_ovod1_b1, fourone_onefour_ovod1_b
 
 
 ## Making the  data long 
-## Virgin
+
 # 4:1
 fourone_of <- four_to_one_ovod1  %>% 
   pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
@@ -58,25 +58,27 @@ one_to_four_virgin_b2 <- read_excel("data/female_conditioning/virgin/rawresults_
 fourone_onefour_virgin_b2 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_1-4_virgin_b2.xlsx")
 
 
-four_to_one_virgin_b3 <- read_excel("data/")
+four_to_one_virgin_b3 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_virgin_b3.xlsx")
+one_to_four_virgin_b3 <- read_excel("data/female_conditioning/virgin/rawresults_1-4_virgin_b3.xlsx")
+fourone_onefour_virgin_b3 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_1-4_virgin_b3.xlsx")
 
 
 
 ## Binding the data 
-four_to_one_virgin <- rbind(four_to_one_virgin_b1, four_to_one_virgin_b2 )
-one_to_four_virgin <- rbind(one_to_four_virgin_b1, one_to_four_virgin_b2)
-fourone_onefour_virgin <- rbind (fourone_onefour_virgin_b1, fourone_onefour_virgin_b2)
+four_to_one_virgin <- rbind(four_to_one_virgin_b1, four_to_one_virgin_b2, four_to_one_virgin_b3)
+one_to_four_virgin <- rbind(one_to_four_virgin_b1, one_to_four_virgin_b2, one_to_four_virgin_b3 )
+fourone_onefour_virgin <- rbind (fourone_onefour_virgin_b1, fourone_onefour_virgin_b2, fourone_onefour_virgin_b3)
 
 ## Making the  data long 
 ## Virgin
 # 4:1
-four_to_one_virgin_long <- four_to_one_virgin  %>% 
+fourone_v <- four_to_one_virgin  %>% 
   pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
 # 1:4 
-one_to_four_virgin_long <- one_to_four_virgin  %>% 
+onefour_v <- one_to_four_virgin  %>% 
   pivot_longer(cols = ("1:4 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
 # 4:1 and 1:4 
-fourone_onefour_virgin_long <- fourone_onefour_virgin %>% 
+combined_v<- fourone_onefour_virgin %>% 
   pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers")
 
 ####--- 
@@ -152,9 +154,9 @@ of3 <- feeding_results(combined_of, boxplot_fill_color = c("lightblue", "lightbl
 of1 + of2 + of3
 
 ## Virgin Female
-vf1 <- feeding_results(one_to_four_virgin_long, boxplot_fill_color = c("lightblue", "lightblue"))
-vf2 <- feeding_results(four_to_one_virgin_long, boxplot_fill_color = c("#FDECCD","#FDECCD")) 
-vf3 <- feeding_results(fourone_onefour_virgin_long, boxplot_fill_color = c("lightblue", "lightblue","#FDECCD","#FDECCD"))
+vf1 <- feeding_results(onefour_v, boxplot_fill_color = c("lightblue", "lightblue"))
+vf2 <- feeding_results(fourone_v, boxplot_fill_color = c("#FDECCD","#FDECCD")) 
+vf3 <- feeding_results(combined_v, boxplot_fill_color = c("lightblue", "lightblue","#FDECCD","#FDECCD"))
 
 vf1 + vf2 + vf3
 
