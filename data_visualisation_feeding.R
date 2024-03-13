f7a8d8f660d1463d405225f757af15b85b6b58a6
Katie Millar <ajh20fhu@uea.ac.uk>
2024-03-13 13:27:23 +0000

    Making data visualisation script neater, adding comments and making it easier to follow.

--- data_visualisation_feeding.R
+++ data_visualisation_feeding.R
@@ -1,34 +1,43 @@
-#### INSTALL PACKAGES 
+#### INSTALL PACKAGES ####
 library(tidyverse)
 library(readxl)
 library(patchwork)
 library(colorBlindness)
 library(ggplot2)
 library(ggpattern)
+#### #### #### #### ####
 
 
-#### Uploading raw data
 
-## OvoD1 Females 
+#### Uploading raw data #### 
+
+
+#####################
+## OvoD1 FEMALES ##
+#####################
+# 4:1 
 four_to_one_ovod1_b1 <- read_excel("data/female_conditioning/ovod1/rawresults_4-1_ovod1_b1.xlsx")
-one_to_four_ovod1_b1 <- read_excel("data/female_conditioning/ovod1/rawresults_1-4_ovod1_b1.xlsx")
-fourone_onefour_ovod1_b1 <- read_excel("data/female_conditioning/ovod1/rawresults_4-1_1-4_ovod1_b1.xlsx")
-
-## OvoD1 Females 
 four_to_one_ovod1_b2 <- read_excel("data/female_conditioning/ovod1/rawresults_4-1_ovod1_b2.xlsx")
+
+# Binding the 4:1 data blocks 
+four_to_one_ovod1 <- rbind(four_to_one_ovod1_b1, four_to_one_ovod1_b2)
+
+# 1:4 
+one_to_four_ovod1_b1 <- read_excel("data/female_conditioning/ovod1/rawresults_1-4_ovod1_b1.xlsx")
 one_to_four_ovod1_b2 <- read_excel("data/female_conditioning/ovod1/rawresults_1-4_ovod1_b2.xlsx")
+
+# Binding the 1:4 data blocks
+one_to_four_ovod1 <- rbind(one_to_four_ovod1_b1, one_to_four_ovod1_b2)
+
+# 4:1 and 1:4 
+fourone_onefour_ovod1_b1 <- read_excel("data/female_conditioning/ovod1/rawresults_4-1_1-4_ovod1_b1.xlsx")
 fourone_onefour_ovod1_b2 <- read_excel("data/female_conditioning/ovod1/rawresults_4-1_1-4_ovod1_b2.xlsx")
 
-
-four_to_one_ovod1 <- rbind(four_to_one_ovod1_b1, four_to_one_ovod1_b2)
-one_to_four_ovod1 <- rbind(one_to_four_ovod1_b1, one_to_four_ovod1_b2)
+# Binding the 4:1/1:4 data blocks
 fourone_onefour_ovod1 <- rbind(fourone_onefour_ovod1_b1, fourone_onefour_ovod1_b2)
 
 
-
-
-
-## Making the  data long 
+## CHANGING THE VARIABLE NAMEAS OF THE DATA - PIVOTING A DATA FRAME -- 
 
 # 4:1
 fourone_of <- four_to_one_ovod1  %>% 
@@ -40,44 +49,52 @@ onefour_of <- one_to_four_ovod1  %>%
 combined_of <- fourone_onefour_ovod1 %>% 
   pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers")
 
-####--- 
 
 
 
-## Virgin females
+
+
+
+
+
+###################################################
+#👰👰👰👰## VIRGIN WILD TYPE FEMALES ## 👰👰👰👰###
+###################################################
+
+
+# 4:1 
 four_to_one_virgin_b1 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_virgin_b1.xlsx")
-one_to_four_virgin_b1 <- read_excel("data/female_conditioning/virgin/rawresults_1-4_virgin_b1.xlsx")
-fourone_onefour_virgin_b1 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_1-4_virgin_b1.xlsx")
-
-
-
-
-
 four_to_one_virgin_b2 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_virgin_b2.xlsx")
-one_to_four_virgin_b2 <- read_excel("data/female_conditioning/virgin/rawresults_1-4_virgin_b2.xlsx")
-fourone_onefour_virgin_b2 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_1-4_virgin_b2.xlsx")
-
-
 four_to_one_virgin_b3 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_virgin_b3.xlsx")
-one_to_four_virgin_b3 <- read_excel("data/female_conditioning/virgin/rawresults_1-4_virgin_b3.xlsx")
-fourone_onefour_virgin_b3 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_1-4_virgin_b3.xlsx")
-
 four_to_one_virgin_b4 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_virgin_b4.xlsx")
+
+# Binding the 4:1 data blocks 
+four_to_one_virgin <- rbind(four_to_one_virgin_b1, four_to_one_virgin_b2, four_to_one_virgin_b3, four_to_one_virgin_b4)
+
+
+# 1:4 
+one_to_four_virgin_b1 <- read_excel("data/female_conditioning/virgin/rawresults_1-4_virgin_b1.xlsx")
+one_to_four_virgin_b2 <- read_excel("data/female_conditioning/virgin/rawresults_1-4_virgin_b2.xlsx")
+one_to_four_virgin_b3 <- read_excel("data/female_conditioning/virgin/rawresults_1-4_virgin_b3.xlsx")
 one_to_four_virgin_b4 <- read_excel("data/female_conditioning/virgin/rawresults_1-4_virgin_b4.xlsx")
+
+# Binding the 1:4 data blocks 
+one_to_four_virgin <- rbind(one_to_four_virgin_b1, one_to_four_virgin_b2, one_to_four_virgin_b3,  one_to_four_virgin_b4 )
+
+
+# 4:1 and 1:4 
+fourone_onefour_virgin_b1 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_1-4_virgin_b1.xlsx")
+fourone_onefour_virgin_b2 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_1-4_virgin_b2.xlsx")
+fourone_onefour_virgin_b3 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_1-4_virgin_b3.xlsx")
 fourone_onefour_virgin_b4 <- read_excel("data/female_conditioning/virgin/rawresults_4-1_1-4_virgin_b4.xlsx")
 
-
-
-
-## Binding the data 
-four_to_one_virgin <- rbind(four_to_one_virgin_b1, four_to_one_virgin_b2, four_to_one_virgin_b3, four_to_one_virgin_b4)
-one_to_four_virgin <- rbind(one_to_four_virgin_b1, one_to_four_virgin_b2, one_to_four_virgin_b3,  one_to_four_virgin_b4 )
+# Binding the 4:1/1:4 data blocks 
 fourone_onefour_virgin <- rbind (fourone_onefour_virgin_b1, fourone_onefour_virgin_b2, fourone_onefour_virgin_b3, fourone_onefour_virgin_b4)
 
 
-## Making the  data long 
-## Virgin
-# 4:1
+## CHANGING THE VARIABLE NAMEAS OF THE DATA - PIVOTING A DATA FRAME -- 
+
+
 fourone_v <- four_to_one_virgin  %>% 
   pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
 # 1:4 
@@ -87,31 +104,43 @@ onefour_v <- one_to_four_virgin  %>%
 combined_v<- fourone_onefour_virgin %>% 
   pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers")
 
-####--- 
+####----------
 
-## Males 
+
+
+
+
+
+###########################
+#### ♂️ MALE CONDITIONING ♂️ #### 
+###########################
+
+
 # 4:1 
 four_to_one_male_b1 <- read_excel("data/male_conditioning/treatment_2/rawdata_m4-1_t2b1.xlsx")
 four_to_one_male_b2 <- read_excel("data/male_conditioning/treatment_2/rawdata_m4-1_t2b2.xlsx")
-# binding the data
+
+# Binding the 4:1 data
 four_to_one_male <- rbind(four_to_one_male_b1, four_to_one_male_b2)
 
 # 1:4 
 one_to_four_male_b1 <- read_excel("data/male_conditioning/treatment_2/rawdata_m1-4_t2b1.xlsx")
 one_to_four_male_b2 <- read_excel("data/male_conditioning/treatment_2/rawdata_m1-4_t2b2.xlsx")
-# binding the data
+
+# Binding the 1:4 data
 one_to_four_male <- rbind(one_to_four_male_b1, one_to_four_male_b2)
 
 # 4:1 + 1:4 
 fourone_onefour_male_b1 <- read_excel("data/male_conditioning/treatment_2/rawdata_m4-1_1-4_t2b1.xlsx")
 fourone_onefour_male_b2 <- read_excel("data/male_conditioning/treatment_2/rawdata_m4-1_1-4_t2b2.xlsx")
-# binding the data
+
+# Binding the 4:1/1:4 data
 fourone_onefour_male <- rbind(fourone_onefour_male_b1, fourone_onefour_male_b2)
 
 
 
+## CHANGING THE VARIABLE NAMEAS OF THE DATA - PIVOTING A DATA FRAME -- 
 
-## Making the  data long 
 # 4:1
 fourone_m <- four_to_one_male  %>% 
   pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "fly_numbers")
@@ -123,7 +152,21 @@ combined_m <- fourone_onefour_male %>%
   pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers")
 
 
-#### Creating a function for a plot which will allow me to run the same code for different datasets 
+
+
+
+
+
+###### ###### ###### ###### DATA VISUALISATION ###### ###### ###### ###### 
+###### 📊📊📊 📊📊📊 📊📊📊 📊📊📊 📊📊📊 📊📊📊 📊📊📊 📊📊📊📊📊📊
+
+
+# Creating a function for a boxplot, 
+# this will allow one to use the feeding data for different data sets 
+
+
+#################################### Feeding Results Function Plot ####################################
+
 feeding_results <- function(summary_data,boxplot_fill_color ) {
   ggplot(summary_data, aes(x = diet, y = fly_numbers, fill = diet, pattern = diet))+ 
     geom_boxplot(fill = "white", color = "black")+
@@ -151,32 +194,58 @@ feeding_results <- function(summary_data,boxplot_fill_color ) {
   
 }
 
-## Code will allow one to see each of the plots
-## Virgin Female
-of1 <- feeding_results(onefour_of, boxplot_fill_color = c("lightblue", "lightblue"))
-of2 <- feeding_results(fourone_of, boxplot_fill_color = c("#FDECCD","#FDECCD")) 
-of3 <- feeding_results(combined_of, boxplot_fill_color = c("lightblue", "lightblue","#FDECCD","#FDECCD"))
-
-of1 + of2 + of3
-
-## Virgin Female
-vf1 <- feeding_results(onefour_v, boxplot_fill_color = c("lightblue", "lightblue"))
-vf2 <- feeding_results(fourone_v, boxplot_fill_color = c("#FDECCD","#FDECCD")) 
-vf3 <- feeding_results(combined_v, boxplot_fill_color = c("lightblue", "lightblue","#FDECCD","#FDECCD"))
-
-vf1 + vf2 + vf3
-
-## Male
-mf1 <- feeding_results(onefour_m, boxplot_fill_color = c("lightblue", "lightblue"))
-mf2 <- feeding_results(fourone_m, boxplot_fill_color = c("#FDECCD","#FDECCD")) 
-mf3 <- feeding_results(combined_m, boxplot_fill_color = c("lightblue", "lightblue","#FDECCD","#FDECCD"))
-
-mf1 + mf2 + mf3
+############################################################################################################
 
 
+################  CODING FOR EACH OF THE DIFFERENT CONDITIONS #################### 
 
 
-#### A violin plot 
+# Plot function included, followed by the specific data set, 
+# followed by the appropriate colours for the plot depending on the assay 
+
+
+#####################
+## OvoD1 FEMALES ##
+#####################
+of_1_4 <- feeding_results(onefour_of, boxplot_fill_color = c("lightblue", "lightblue"))
+of_4_1  <- feeding_results(fourone_of, boxplot_fill_color = c("#FDECCD","#FDECCD")) 
+of_combined  <- feeding_results(combined_of, boxplot_fill_color = c("lightblue", "lightblue","#FDECCD","#FDECCD"))
+
+## Using patchwork() to combine the different plots of different assays
+of_1_4 + of_4_1 + of_combined
+
+
+##############################
+## VIRGIN WILD TYPE FEMALES ##
+##############################
+vf_1_4  <- feeding_results(onefour_v, boxplot_fill_color = c("lightblue", "lightblue"))
+vf_4_1 <- feeding_results(fourone_v, boxplot_fill_color = c("#FDECCD","#FDECCD")) 
+vf_combined <- feeding_results(combined_v, boxplot_fill_color = c("lightblue", "lightblue","#FDECCD","#FDECCD"))
+
+## Using patchwork() to combine the different plots of different assays
+vf_1_4 + vf_4_1 + vf_combined
+
+
+
+
+######################
+## WILD TYPE MALES ##
+#####################
+m_1_4  <- feeding_results(onefour_m, boxplot_fill_color = c("lightblue", "lightblue"))
+m_4_1 <- feeding_results(fourone_m, boxplot_fill_color = c("#FDECCD","#FDECCD")) 
+m_combined <- feeding_results(combined_m, boxplot_fill_color = c("lightblue", "lightblue","#FDECCD","#FDECCD"))
+
+m_1_4 + m_4_1 + m_combined
+
+
+
+
+
+
+####################################################################################################################################
+## Additional code 
+
+#### A Violin Plot 
 feeding_results_violin <- function(summary_data, violin_fill_color) {
   ggplot(summary_data, aes(x = diet, y = fly_numbers, fill = diet)) + 
     geom_violin(trim = FALSE, width = 0.5, draw_quantiles = c(0.25, 0.5, 0.75)) +
@@ -190,7 +259,7 @@ theme_classic() +
 }
 
 
-# male 
+# Male code for example 
 feeding_results_violin(fourone_onefour_male_long, violin_fill_color = c("lightblue", "lightblue","#FDECCD","#FDECCD"))
 
 
