54d7cd395030298b90573e27498a6496888a6558
Katie Millar <ajh20fhu@uea.ac.uk>
2024-03-19 14:23:20 +0000

    Adapting the oviposition data analysis script, adding more comments, removing block 1 from the virgin data set as this was done differently. Adding block as a variable to look for the significance between different block repeats of data. Doing data analysis using binomial models and mixed models, these were all significant with block so I need to know how to properly anlayse.

--- data_analysis_oviposition.R
+++ data_analysis_oviposition.R
@@ -12,7 +12,11 @@ library(lme4)
 
 ## OVIPOSITION ##
 
-#### VIRGIN FEMALE  ----
+#######################
+#### VIRGIN FEMALE ####
+#######################
+
+## Creating a path to Virgin Conditioning Oviposition data
 pathvirginoviposition <- "data/female_conditioning/virgin"
 
 # reading in oviposition data for virgin 
@@ -31,8 +35,14 @@ read_raw_virgin_oviposition <-function(path = pathvirginoviposition, pattern_to_
 }
 
 
+
 # using the function to find the path 
 read_raw_virgin_oviposition(pathvirginoviposition)
+
+
+### making a dataset without block 1
+
+
 
 
 ## creating an actual data set that will read the paths
@@ -40,10 +50,27 @@ read_raw_virgin_oviposition(pathvirginoviposition)
 df_virgin_oviposition <- pathvirginoviposition %>% 
   map_df(~read_raw_virgin_oviposition(.x)) #.x is a string or NULL - only applies to dfr apparently
 
+# Use grepl to identify rows with "1-4" pattern in any column
+pattern <- "b1"
+exclude_rows <- grepl(pattern, df_virgin_oviposition$id)
+
+# Subset the dataframe to exclude rows with the "1-4" pattern
+df_virgin_oviposition <- df_virgin_oviposition[!exclude_rows, ]
+
+
+## Mutating a variable for block 
+df_virgin_oviposition <- df_virgin_oviposition %>% 
+  mutate(block = case_when(
+    str_detect(id, "b2") ~ "two",
+    str_detect(id, "b3") ~ "three",
+    str_detect(id, "b4") ~ "four",
+  ))
+
+
 # uses what was generated with "df"
 df2_virgin_oviposition <- df_virgin_oviposition %>%
   separate(diet, into = c("ratio", "condition"), sep = " ") %>%#separate will turn a single factor column into multiple columns
-  group_by(id, plate, ratio, condition) %>% ## group by what is split
+  group_by(id, plate, ratio, condition, block) %>% ## group by what is split
   summarise(count = sum(egg_numbers)) %>% 
   pivot_wider(names_from = "condition", values_from = "count") 
 
@@ -55,12 +82,16 @@ df2_virgin_oviposition # does it recognise condition from the long data
 
 
 
+
+
+
 #### OVOD1 FEMALE ----
 pathovod1oviposition <- "data/female_conditioning/ovod1"
 
 ## This creates  function
-## Path is interchangeable with path 2 
-read_raw_ovod1_oviposition <-function(path = pathovod1oviposition, pattern_to_exclude = "4-1_1-4_oviposition"){
+
+######################################################################################################################## 
+read_raw_ovod1_oviposition <-function(path = pathovod1oviposition, pattern_to_exclude = "4-1_1-4"){
   list_of_files <- list.files(path = pathovod1oviposition,
                               pattern = "oviposition", full.names = T)
   
@@ -73,6 +104,11 @@ read_raw_ovod1_oviposition <-function(path = pathovod1oviposition, pattern_to_ex
                  values_to = "egg_numbers") %>%
     drop_na(egg_numbers) ## because the data files are being combined, dropping na where certain data scripts should not be included
 }
+########################################################################################################################
+
+
+
+
 
 ## read_raw is the function created, and path shows the path made, so the list of files
 read_raw_ovod1_oviposition(pathovod1oviposition) 
@@ -83,15 +119,27 @@ read_raw_ovod1_oviposition(pathovod1oviposition)
 df_ovod1_oviposition <- pathovod1oviposition %>% 
   map_df(~read_raw_ovod1_oviposition(.x)) #.x is a string or NULL - only applies to dfr apparently
 
+
+## Mutating a variable for block
+df_ovod1_oviposition <- df_ovod1_oviposition %>% 
+  mutate(block = case_when(
+    str_detect(id, "b1") ~ "one",
+    str_detect(id, "b2") ~ "two",
+    
+  ))
+
+
 # uses what was generated with "df"
 df2_ovod1_oviposition <- df_ovod1_oviposition %>%
   separate(diet, into = c("ratio", "condition"), sep = " ") %>%#separate will turn a single factor column into multiple columns
-  group_by(id, plate, ratio, condition) %>% ## group by what is split
+  group_by(id, plate, ratio, condition, block) %>% ## group by what is split
   summarise(count = sum(egg_numbers)) %>% 
   pivot_wider(names_from = "condition", values_from = "count") 
 
 ## new data 
 df2_ovod1_oviposition # does it recognise condition from the long data? 
+
+
 
 
 
@@ -170,8 +218,12 @@ df2_male_oviposition # does it recognise condition from the long data?
 
 
 
-#### DATA ANALYSIS 
-# Male Oviposition
+                                         #### DATA ANALYSIS ####
+
+###########################
+#### MALE OVIPOSITION #####
+###########################
+
 
 ## Male Oviposition Analysis
 binom_m_egg <- glm(cbind(Conditioned, Unconditioned) ~ ratio, family = binomial, data = df2_male_oviposition)
@@ -209,10 +261,157 @@ qnorm(residuals)
 ## Will generate a qq 
 qqnorm(residuals)
 
+
+drop1(glmer.mm_m_egg, tets = "Chisq") ##?? 
+
+
+## Using this model for now 
+summary(glmer.mm_m_egg) ## says block is significant 
+
+
+
+
+
+
+
+
+
+
+
+##################################
+### OvoD1 FEMALE OVIPOSITION ####
+##################################
+
+
+## Model 1 
+
+# Binomial model 
+binom_od1_egg <- glm(cbind(Conditioned, Unconditioned) ~ ratio * block, family = binomial, data = df2_ovod1_oviposition)
+
+# Assumption checking 
+
+# easystats does not work at the moment 
+
+# DHARMa 
+testDispersion(binom_od1_egg) # looks pretty poor
+
+simulationOutput <- simulateResiduals(fittedModel = binom_od1_egg, plot = F)
+plot(simulation_Output) # don't think it looks great 
+
+## Tring to generate an actual qqplot
+
+## Generating residuals 
+residuals <- residuals(binom_od1_egg, type = "pearson")
+
+qnorm(residuals)
+
+## Will generate a qq 
+qqnorm(residuals) ## qq looks bad 
+
 ## Using this model for now 
 summary(glmer.mm_m_egg)
 
 
+## Looking at model 
+summary(binom_od1_egg) # 4:1 Conditioned is significant?
+
+
+## Trying a new model 
+glmer.ovod1_f_egg <- glmer(cbind(Conditioned, Unconditioned) ~ ratio * block + (1|plate)  , family = binomial, data = df2_ovod1_oviposition)
+
+# Assumption checking 
+
+# easystats does not work at the moment 
+
+# DHARMa 
+testDispersion(glmer.ovod1_f_egg) # looks a lot better I think 
+
+simulationOutput <- simulateResiduals(fittedModel = glmer.ovod1_f_egg, plot = F)
+plot(simulation_Output) # I don't know 
+
+## qq plot 
+## Tring to generate an actual qqplot
+
+## Generating residuals 
+residuals <- residuals(glmer.ovod1_f_egg, type = "pearson")
+qnorm(residuals)
+## Will generate a qq 
+qqnorm(residuals) ## qq looks a lot better 
+
+## Looking for significance in block
+drop1(glmer.ovod1_f_egg, test = "Chisq") ## block is very significant 
+
+## Looking at results, keeping block in 
+summary(glmer.ovod1_f_egg)
+
+
+
+
+
+
+##############################
+## VIRGIN Female Oviposition ##
+##############################
+
+## Model 1 
+
+# Binomial model 
+binom_virgin_egg <- glm(cbind(Conditioned, Unconditioned) ~ ratio * block, family = binomial, data = df2_virgin_oviposition)
+
+# Assumption checking 
+
+# easystats does not work at the moment 
+
+# DHARMa 
+testDispersion(binom_virgin_egg) # looks pretty poor
+
+simulationOutput <- simulateResiduals(fittedModel = binom_virgin_egg, plot = F)
+plot(simulation_Output) # don't think it looks great 
+
+## Tring to generate an actual qqplot
+
+## Generating residuals 
+residuals <- residuals(binom_virgin_egg, type = "pearson")
+
+qnorm(residuals)
+
+## Will generate a qq 
+qqnorm(residuals) ## qq looks sort of okay - could be better  
+
+
+summary(binom_virgin_egg) ## very overdispersed 
+
+
+
+## Trying a new model 
+glmer.virgin_f_egg <- glmer(cbind(Conditioned, Unconditioned) ~ ratio * block + (1|plate)  , family = binomial, data = df2_virgin_oviposition)
+
+# Assumption checking 
+
+# easystats does not work at the moment 
+
+# DHARMa 
+testDispersion(glmer.virgin_f_egg) # looks a lot better I think 
+
+simulationOutput <- simulateResiduals(fittedModel = glmer.virgin_f_egg, plot = F)
+plot(simulation_Output) # I don't know 
+
+## qq plot 
+## Tring to generate an actual qqplot
+
+## Generating residuals 
+residuals <- residuals(glmer.virgin_f_egg, type = "pearson")
+qnorm(residuals)
+## Will generate a qq 
+qqnorm(residuals) ## qq looks pretty much the same
+
+## Looking for significance in block
+drop1(glmer.virgin_f_egg, test = "Chisq") ## block is overall sig 
+
+## Looking at results, keeping block in 
+summary(glmer.virgin_f_egg)
+## block 3 sig with block 1
+## block 2 not sig with block 1 
 
 
 
@@ -223,20 +422,17 @@ summary(glmer.mm_m_egg)
 
 
 
-# OvoD1 Oviposition
-binomial_model_ovod1_oviposition <- glm(cbind(Conditioned, Unconditioned) ~ ratio, family = binomial, data = df2_ovod1_oviposition)
-## looking at model 
-summary(binomial_model_ovod1_oviposition) # 4:1 Conditioned is significant?
-
-emmeans::emmeans(binomial_model_ovod1_oviposition, pairwise ~ ratio)
 
 
 
 
-# Female Oviposition
-binomial_model_virgin_oviposition <- glm(cbind(Conditioned, Unconditioned) ~ ratio, family = binomial, data = df2_virgin_oviposition)
 
-summary(binomial_model_virgin_oviposition)
+
+
+
+
+
+
 
 
 
