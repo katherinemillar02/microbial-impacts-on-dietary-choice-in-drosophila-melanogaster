9ac3d3152e15e45920b06a3cd5b81e3563a02ce0
Katie Millar <ajh20fhu@uea.ac.uk>
2024-03-26 14:17:33 +0000

    Adding a statistical power analysis for 1:4 feeding, using the environmental computing website. However, for this, I get some odd results.

--- microbial-wash-power-analysis.R
+++ microbial-wash-power-analysis.R
@@ -10,6 +10,9 @@ library(pwr)
 
 #### Environmental Computing - https://environmentalcomputing.net/statistics/power-analysis/
 
+
+#### Doing it for the 4:1 Feeding Assay
+
 set.seed(1) # This ensures everything is reproducible
 
 ## Using the 4:1 diets
@@ -19,7 +22,7 @@ N <- 15
 ## READING THE DATA IN
 ## Just the 4:1 dataset 
 
-## Assigning the pattern
+## doing a pattern so it excludes 1:4 
 pattern <- "1-4"
 ## Uaing grepl() to exclude the 1-4 rows 
 exclude_rows <- grepl(pattern, df2_male$id)
@@ -115,6 +118,128 @@ abline(h = 0.8)
 
 
 
+
+#### Doing it for the 1:4 Feeding Assay
+
+set.seed(1) # This ensures everything is reproducible
+
+## Using the 1:4 diets
+N <- 15
+## I had 15 plate replicates in the 1:4 Male Conditioned Assays
+
+## READING THE DATA IN
+## Just the 1:4 dataset 
+
+## Assigning the pattern
+pattern2 <- "4-1"
+## Uaing grepl() to exclude the 4-1 rows 
+exclude_rows <- grepl(pattern2, df2_male$id)
+
+## Making a new dataframe with 4:1 excluded 
+df2_male_filtered_2 <- df2_male[!exclude_rows, ]
+
+## Viewing just the dataframe for 4:1 
+View(df2_male_filtered_2)
+
+
+
+
+
+## Working out the trt effect (treatment effect)
+
+## Calculating the mean effect
+conditioned_mean <- mean(df2_male_filtered_2$Conditioned) # 0.7987805
+unconditioned_mean <- mean(df2_male_filtered_2$Unconditioned) # 0.5914634
+
+## Calulcating the treatment effect mean
+trt_mean <- conditioned_mean - unconditioned_mean # 0.2073171
+
+trt.effect <- 0.2073171 # difference between control and treatment means 
+
+
+
+
+
+### Working out the standard deviation
+
+# Concatenate the values from both "Conditioned" and "Unconditioned" columns
+combined_values <- c(df2_male_filtered_2$Conditioned, df2_male_filtered_2$Unconditioned)
+
+# Calculate the standard deviation of the combined values
+combined_sd <- sd(combined_values)
+
+### the sd together?? 
+sigma <- 0.9179025 # standard deviation of control and treatment groups? 
+
+# The mean of 4:1 Unconditioned
+mean.con <-  0.5914634 # mean of control group (mean of 4:1 Unconditioned)
+
+## This is the mean  
+mean.trt <- mean.con + trt.effect # mean of treatment group - adding the effect back 
+
+## Simulating control (unconditioned) data points
+control <- rnorm(N, mean.con, sigma) # 20 data points for the control group taken from a normal distribution with known sample size, mean and s.d.
+
+## Simulated trt data points
+treatment <- rnorm(N, mean.trt, sigma) # data for the treatment group
+
+## Basic t-test of Conditioned vs. Unconditioned
+t.test(control, treatment)
+
+## Visualising Conditioned vs. Unconditioned
+boxplot(cbind(control, treatment))
+
+
+
+
+
+## DOING POWER TESTS -- 
+
+## With the n of replicates used before
+pwr.t.test(n = 15, d = trt.effect / sigma, sig.level = 0.05, power = NULL)
+
+## Using the power to work out what replicates are needed
+pwr.t.test(n = NULL, d = trt.effect / sigma, sig.level = 0.05, power = 0.8)
+    #### says I need an n of 308?? 
+
+
+
+
+##### Plotting power analysis for various sample sizes
+nvals <- seq(1, 30, length.out = 30)
+powvals <- sapply(nvals, function(x) pwr.2p.test(h = trt.effect / sigma, n = x, sig.level = 0.05)$power)
+
+
+## Shows sample size against power!! would need a sample size of like 20?? 
+plot(nvals, powvals,
+     xlab = "sample size", ylab = "power",
+     main = "Power curve for sample size for difference in proportions",
+     lwd = 2, col = "red", type = "l"
+)
+
+abline(h = 0.8)
+
+
+## This one says I need an n = of 308... 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################################################################################################
 
 
 
