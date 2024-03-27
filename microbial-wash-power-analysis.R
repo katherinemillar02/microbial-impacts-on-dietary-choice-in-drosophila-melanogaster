a987c98f61c9ca1cebc5d631c51bd326c6b51292
Katie Millar <ajh20fhu@uea.ac.uk>
2024-03-27 09:14:59 +0000

    Trying to calculate the mean effects of both 4:1 and 1:4 together to see if this is more effective for a statistical power analysis.

--- microbial-wash-power-analysis.R
+++ microbial-wash-power-analysis.R
@@ -35,13 +35,13 @@ View(df2_male_filtered)
 ## Working out the trt effect (treatment effect)
 
 ## Calculating the mean effect
-conditioned_mean <- mean(df2_male_filtered$Conditioned) # 2.8
-unconditioned_mean <- mean(df2_male_filtered$Unconditioned) # 0.636
+conditioned_mean_fourone <- mean(df2_male_filtered$Conditioned) # 2.8
+unconditioned_mean_fourone <- mean(df2_male_filtered$Unconditioned) # 0.636
 
 ## Calulcating the treatment effect mean
-trt_mean <- conditioned_mean - unconditioned_mean #2.163636
+trt_mean_fourone <- conditioned_mean_fourone - unconditioned_mean_fourone #2.163636
 
-trt.effect <- 2.163636 # difference between control and treatment means 
+trt.effect_fourone <- 2.163636 # difference between control and treatment means 
 ## Is this the mean of 4:1 Conditioned - the mean of 4:1 Unconditioned? 
 
 
@@ -50,31 +50,31 @@ trt.effect <- 2.163636 # difference between control and treatment means
 ### Working out the standard deviation
 
 # Concatenate the values from both "Conditioned" and "Unconditioned" columns
-combined_values <- c(df2_male_filtered$Conditioned, df2_male_filtered$Unconditioned)
+combined_values_fourone <- c(df2_male_filtered$Conditioned, df2_male_filtered$Unconditioned)
 
 # Calculate the standard deviation of the combined values
-combined_sd <- sd(combined_values)
+combined_sd_fourone <- sd(combined_values)
 
 ### the sd together?? 
-sigma <- 2.027764 # standard deviation of control and treatment groups? 
+sigma_fourone <- 2.027764 # standard deviation of control and treatment groups? 
 
 # The mean of 4:1 Unconditioned
-mean.con <-  0.6363636 # mean of control group (mean of 4:1 Unconditioned)
+mean.con_fourone <-  0.6363636 # mean of control group (mean of 4:1 Unconditioned)
 
 ## This is the mean  
-mean.trt <- mean.con + trt.effect # mean of treatment group - adding the effect back 
+mean.trt_fourone <- mean.con_fourone + trt.effect_fourone # mean of treatment group - adding the effect back 
 
 ## Simulating control (unconditioned) data points
-control <- rnorm(N, mean.con, sigma) # 20 data points for the control group taken from a normal distribution with known sample size, mean and s.d.
+control_fourone <- rnorm(N, mean.con_fourone, sigma_fourone) # 20 data points for the control group taken from a normal distribution with known sample size, mean and s.d.
 
 ## Simulated trt data points
-treatment <- rnorm(N, mean.trt, sigma) # data for the treatment group
+treatment_fourone <- rnorm(N, mean.trt_fourone, sigma_fourone) # data for the treatment group
 
 ## Basic t-test of Conditioned vs. Unconditioned
-t.test(control, treatment)
+t.test(control_fourone, treatment_fourone)
 
 ## Visualising Conditioned vs. Unconditioned
-boxplot(cbind(control, treatment))
+boxplot(cbind(control_fourone, treatment_fourone))
 
 
 
@@ -83,18 +83,20 @@ boxplot(cbind(control, treatment))
 ## DOING POWER TESTS -- 
 
 ## With the n of replicates used before
-pwr.t.test(n = 15, d = trt.effect / sigma, sig.level = 0.05, power = NULL)
+pwr.t.test(n = 15, d = trt.effect_fourone / sigma_fourone, sig.level = 0.05, power = NULL)
+# using n = 15 gives a power of 0.8 
+
 
 ## Using the power to work out what replicates are needed
-pwr.t.test(n = NULL, d = trt.effect / sigma, sig.level = 0.05, power = 0.8)
-
+pwr.t.test(n = NULL, d = trt.effect_fourone / sigma_fourone, sig.level = 0.05, power = 0.8)
+  ## says I only need an n of 14.8 (15)
 
 
 
 
 ##### Plotting power analysis for various sample sizes
 nvals <- seq(1, 30, length.out = 30)
-powvals <- sapply(nvals, function(x) pwr.2p.test(h = trt.effect / sigma, n = x, sig.level = 0.05)$power)
+powvals <- sapply(nvals, function(x) pwr.2p.test(h = trt.effect_fourone / sigma_fourone, n = x, sig.level = 0.05)$power)
 
 
 ## Shows sample size against power!! would need a sample size of like 20?? 
@@ -108,6 +110,10 @@ abline(h = 0.8)
 
 ## Is this saying a sample size of 15 is enough?? 
 ## Seems coincidental
+
+
+
+
 
 
 
@@ -144,13 +150,13 @@ View(df2_male_filtered_2)
 ## Working out the trt effect (treatment effect)
 
 ## Calculating the mean effect
-conditioned_mean <- mean(df2_male_filtered_2$Conditioned) # 0.7987805
-unconditioned_mean <- mean(df2_male_filtered_2$Unconditioned) # 0.5914634
+conditioned_mean_onefour <- mean(df2_male_filtered_2$Conditioned) # 0.7987805
+unconditioned_mean_onefour <- mean(df2_male_filtered_2$Unconditioned) # 0.5914634
 
 ## Calulcating the treatment effect mean
-trt_mean <- conditioned_mean - unconditioned_mean # 0.2073171
+trt_mean_onefour <- conditioned_mean_onefour - unconditioned_mean_onefour # 0.2073171
 
-trt.effect <- 0.2073171 # difference between control and treatment means 
+trt.effect_onefour <- 0.2073171 # difference between control and treatment means 
 
 
 
@@ -159,31 +165,31 @@ trt.effect <- 0.2073171 # difference between control and treatment means
 ### Working out the standard deviation
 
 # Concatenate the values from both "Conditioned" and "Unconditioned" columns
-combined_values <- c(df2_male_filtered_2$Conditioned, df2_male_filtered_2$Unconditioned)
+combined_values_onefour <- c(df2_male_filtered_2$Conditioned, df2_male_filtered_2$Unconditioned)
 
 # Calculate the standard deviation of the combined values
-combined_sd <- sd(combined_values)
+combined_sd_onefour <- sd(combined_values)
 
 ### the sd together?? 
-sigma <- 0.9179025 # standard deviation of control and treatment groups? 
+sigma_onefour <- 0.9179025 # standard deviation of control and treatment groups? 
 
 # The mean of 4:1 Unconditioned
-mean.con <-  0.5914634 # mean of control group (mean of 4:1 Unconditioned)
+mean.con_onefour <-  0.5914634 # mean of control group (mean of 4:1 Unconditioned)
 
 ## This is the mean  
-mean.trt <- mean.con + trt.effect # mean of treatment group - adding the effect back 
+mean.trt_onefour <- mean.con_onefour + trt.effect_onefour # mean of treatment group - adding the effect back 
 
 ## Simulating control (unconditioned) data points
-control <- rnorm(N, mean.con, sigma) # 20 data points for the control group taken from a normal distribution with known sample size, mean and s.d.
+control_onefour <- rnorm(N, mean.con_onefour, sigma_onefour) # 20 data points for the control group taken from a normal distribution with known sample size, mean and s.d.
 
 ## Simulated trt data points
-treatment <- rnorm(N, mean.trt, sigma) # data for the treatment group
+treatment_onefour <- rnorm(N, mean.trt_onefour, sigma_onefour) # data for the treatment group
 
 ## Basic t-test of Conditioned vs. Unconditioned
-t.test(control, treatment)
+t.test(control_onefour, treatment_onefour)
 
 ## Visualising Conditioned vs. Unconditioned
-boxplot(cbind(control, treatment))
+boxplot(cbind(control_onefour, treatment_onefour))
 
 
 
@@ -192,18 +198,18 @@ boxplot(cbind(control, treatment))
 ## DOING POWER TESTS -- 
 
 ## With the n of replicates used before
-pwr.t.test(n = 15, d = trt.effect / sigma, sig.level = 0.05, power = NULL)
+pwr.t.test(n = 15, d = trt.effect_onefour / sigma_onefour, sig.level = 0.05, power = NULL)
 
 ## Using the power to work out what replicates are needed
-pwr.t.test(n = NULL, d = trt.effect / sigma, sig.level = 0.05, power = 0.8)
+pwr.t.test(n = NULL, d = trt.effect_onefour / sigma, sig.level = 0.05, power = 0.8)
     #### says I need an n of 308?? 
 
 
 
 
 ##### Plotting power analysis for various sample sizes
-nvals <- seq(1, 30, length.out = 30)
-powvals <- sapply(nvals, function(x) pwr.2p.test(h = trt.effect / sigma, n = x, sig.level = 0.05)$power)
+nvals <- seq(1, 350, length.out = 350)
+powvals <- sapply(nvals, function(x) pwr.2p.test(h = trt.effect_onefour / sigma_onefour, n = x, sig.level = 0.05)$power)
 
 
 ## Shows sample size against power!! would need a sample size of like 20?? 
@@ -215,20 +221,63 @@ plot(nvals, powvals,
 
 abline(h = 0.8)
 
-
 ## This one says I need an n = of 308... 
+## I have set the plot for 350 
 
 
 
 
+############################################################################################################
+
+## Doing both 4:1 and 1:4 at the same time 
+ ## even though I don't get why we would do this 
+
+set.seed(1)
+
+N <- 15 # the sample size
+
+## working out the treatment effect
+
+# the mean of unconditioned
+unconditioned_mean_both <- mean(df2_male$Unconditioned) # 0.6139818
+conditioned_mean_both <- mean(df2_male$Conditioned) # 1.802432
+
+trt_both <- conditioned_mean_both - unconditioned_mean_both # 1.18845
 
 
 
+trt.effect_both <- 1.18845
+
+# Standard deviation of control and treatment groups will be sigma
+# Adding conditioned and unconditioned together then adding the standard deviation? 
+combining_values_both <- df2_male$Conditioned + df2_male$Unconditioned 
+sd_both <- sd(combined_values)
 
 
+sigma_both <- 0.9179025 
+
+mean.con_both <- 0.6139818 # mean of control group
+
+mean.trt_both <- mean.con_both + trt.effect_both # mean of treatment group
+
+control_both <- rnorm(N, mean.con_both, sigma_both) # 20 data points for the control group taken from a normal distribution with known sample size, mean and s.d.
+
+treatment_both <- rnorm(N, mean.trt_both, sigma_both) # data for the treatment group
+
+t.test(control_both, treatment_both)
+
+boxplot(cbind(control_both, treatment_both))
 
 
+pwr.t.test(n = 15, d = trt.effect / sigma, sig.level = 0.05, power = NULL)
+   ## an n of 15 only gives a power of 0.9 :) 
 
+
+pwr.t.test(n = NULL, d = trt.effect / sigma, sig.level = 0.05, power = 0.8)
+   ## says I only need an n of 10 when I do a power test of both? 
+
+
+## A bit confused at these results...  
 
 
 
