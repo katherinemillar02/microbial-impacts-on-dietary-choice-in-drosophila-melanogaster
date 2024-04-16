53e267707f27ac8ca396dd77f4789568a8810e7e
Katie Millar <ajh20fhu@uea.ac.uk>
2024-04-16 11:13:19 +0100

    Creating a barplot for the fitness experiment, showing conditioned and unconditioned pupae emergence over hours form eggs laid. To see the difference of the diet on development and fitness.

--- fitness_experiment.R
+++ fitness_experiment.R
@@ -2,15 +2,25 @@
 library(tidyverse)
 library(ggplot2)
 library(readxl)
+library(viridis)
 
 ## Read data in
 pupae_fitness <- read_excel("data/fitness_experiment/pupae_data.xlsx")
 
+## choosing colours from virids to use
+viridis_colors <- viridis(10)
+
+## Creating a plot (bar plot for now)
+pupae_fitness_plot <- ggplot(pupae_fitness, aes(x = `time (hours)`, y = pupae, fill = treatment)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  #geom_line(aes(col = treatment), position = position_dodge(width = 1)) +
+  scale_fill_manual(values = viridis_colors[c(1,8)]) +
+  theme_classic() +
+  labs(x = "Time (hours) since eggs laid", y = "Pupae", title = "Pupae count over time by treatment") 
+  
+  
 
 
-## Creating a plot 
-yes <- ggplot(pupae_fitness, aes(x = `time (hours)`, y = pupae, color = treatment))
- 
 
 
 
