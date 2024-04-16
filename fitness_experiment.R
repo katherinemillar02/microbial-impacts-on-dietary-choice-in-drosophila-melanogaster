e8358f7e5970e707c5a5b9a3c9294a10b9a374c3
Katie Millar <ajh20fhu@uea.ac.uk>
2024-04-16 19:25:48 +0100

    plotting the data for fly emergence.

--- fitness_experiment.R
+++ fitness_experiment.R
@@ -16,12 +16,34 @@ pupae_fitness_plot <- ggplot(pupae_fitness, aes(x = `time (hours)`, y = pupae, f
   #geom_line(aes(col = treatment), position = position_dodge(width = 1)) +
   scale_fill_manual(values = viridis_colors[c(1,8)]) +
   theme_classic() +
-  labs(x = "Time (hours) since eggs laid", y = "Pupae", title = "Pupae count over time by treatment") 
-  
-  
+  labs(x = "Time (hours) since eggs laid", 
+       y = "Pupae") 
+
+
+## Read data in
+fly_fitness <- read_excel("data/fitness_experiment/fly_data.xlsx")
+
+
+fly_fitness_tidy <- tidyr::pivot_longer(data = fly_fitness ,
+                                 cols = c(females, males),
+                                 names_to = "sex",
+                                 values_to = "count")
+
+
+fly_fitness_tidy$sex_treatment <- paste(fly_fitness_tidy$sex, fly_fitness_tidy$treatment, sep = "_")
+
+
+library(ggplot2)
+
+ggplot(fly_fitness_tidy, aes(x = time_hours, y = count, fill = sex_treatment)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Time (hours)", y = "Count", title = "Counts over time by vial and sex-treatment combination") + 
+  theme_classic()
 
 
 
+ggplot(fly_fitness_tidy, aes(x = time_hours, y = count, fill = sex_treatment)) +
+  geom_bar(position="dodge", stat="identity")
 
 
 
