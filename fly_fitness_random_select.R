579694e91645d5c58992c24d583baccbcab207bb
Katie Millar <ajh20fhu@uea.ac.uk>
2024-05-03 17:04:10 +0100

    ?

--- fly_fitness_random_select.R
+++
@@ -1,5 +0,0 @@
-set.seed(123)
-
-vial_selection <- sample(1:30, 10, replace = FALSE)
-
-vial_selection_uc <- sample(1:25, 10, replace = FALSE)
--- power_analysis.R
+++ power_analysis.R
@@ -1,121 +1,4 @@
-## Packages 
-
-
-## Without block 
-### Mixed effect models
-
-## I 'THINK' these are the only numbers you need to change 
-fixed_effect_Group1 <- 0.535
-fixed_effect_Group2 <- 0.228
-fixed_effect_Group3 <- 0.228
-
-number_of_plates <- c(30, 40, 50, 30, 40, 50, 30, 40, 50)
-number <- c(10,10,10,15,15,15,20,20,20) ## This is the number of of observations 
-
-
-simulate_power <- function(plates, n){
-  
-  num_significant <- 0  
-  
-  sims <- 10 ## Set to run to 10 BUT for accuracy I ran it 1000x, to test it out you can run 200x but be warned both take AGES 
-  for(i in 1:sims){
-    
-    # Simulate random effect with sd = 0.5
-    
-    rand_eff <- data.frame(group = as.factor(seq(1:plates)),
-                           b0 = rnorm(plates, mean = 0, sd = 0.4))
-    
-    rand_eff2 <- data.frame(repeats = as.factor(seq(1:n)),
-                            b1 = rnorm(n, mean = 0, sd = 0.3))
-    
-    
-    rand_eff_duplicated <- rand_eff[rep(row.names(rand_eff), each = n), ]
-    rownames(rand_eff_duplicated) <- NULL  # Resetting row names
-    
-    rand_eff_duplicated2 <- rand_eff2[rep(row.names(rand_eff2), times = plates), ]
-    rownames(rand_eff_duplicated2) <- NULL  # Resetting row names
-    
-    rand_eff_total <- as_tibble(cbind(rand_eff_duplicated, rand_eff_duplicated2)) |> 
-      mutate(rand_eff_total = b0 + b1 )
-    
-    Treatment1 <- rpois(n = nrow(rand_eff_total), lambda = exp(fixed_effect_Group1 + rand_eff_total$rand_eff_total))
-    
-    #Treatment1 <- map(rand_eff$b0, ~ rpois(n = n, lambda = exp(fixed_effect_Group1 + .)))
-    Treatment1 <- map_df(
-      seq_along(Treatment1),
-      ~ tibble(Count = Treatment1[[.]], Plate = .)
-    ) |> mutate(Treatment = "1:4 Condition")
-    
-    
-    repeating_vector <- rep(1:n, length.out = nrow(Treatment1))
-    
-    Treatment1$id <- repeating_vector
-    
-    #Treatment2 <- map(rand_eff$b0, ~ rpois(n = n, lambda = exp(fixed_effect_Group2 + . + rnorm(n=1, mean = 0, sd = .3))))
-    Treatment2 <- rpois(n = nrow(rand_eff_total), lambda = exp(fixed_effect_Group2 + rand_eff_total$rand_eff_total))
-    
-    Treatment2 <- map_df(
-      seq_along(Treatment2),
-      ~ tibble(Count = Treatment2[[.]], Plate = .)
-    ) |> mutate(Treatment = "1:4 Uncondition")
-    
-    repeating_vector <- rep(1:n, length.out = nrow(Treatment2))
-    
-    Treatment2$id <- repeating_vector
-    
-    
-    #Treatment3 <- map(rand_eff$b0, ~ rpois(n = n, lambda = exp(fixed_effect_Group3 + . + rnorm(n=1, mean = 0, sd = .3))))
-    
-    Treatment3 <- rpois(n = nrow(rand_eff_total), lambda = exp(fixed_effect_Group3 + rand_eff_total$rand_eff_total))
-    
-    Treatment3 <- map_df(
-      seq_along(Treatment3),
-      ~ tibble(Count = Treatment3[[.]], Plate = .)
-    ) |> mutate(Treatment = "1:4 Microbe Filtered")
-    
-    repeating_vector <- rep(1:n, length.out = nrow(Treatment3))
-    
-    Treatment3$id <- repeating_vector
-    
-    data <- rbind(Treatment1, Treatment2, Treatment3)
-    
-    
-    model <- glmmTMB::glmmTMB(Count ~ Treatment + (1|Plate) +(1|id) , data = data, family  = poisson)
-    
-    
-    if (summary(model)$coefficients$cond[2,4] < 0.05) {
-      num_significant <- num_significant + 1
-    }
-  }
-  
-  return(num_significant / sims)
-  
-}
-
-simulation_results <- map2_dbl(number_of_plates, number, simulate_power)
-
-
-
-
-############################################################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-#### This is a code if your model contains block - less confident but think it follows the same regime
-
-## Using these values for: 
-# 1:4
-# At the moment
-# But I need to double-check the model so these are subject to change
+library(tidyverse)
 
 
 ## Fixed effect 
@@ -151,8 +34,8 @@ simulate_power <- function(plates, n){ # I have n = 10 because there were n = 10
     rand_eff2 <- data.frame(repeats = as.factor(seq(1:n)),
                             b1 = rnorm(n, mean = 0, sd = 0.32))
     
-    rand_eff3 <- data.frame(blocks = as.factor(seq(1:blocks)),
-                            b2 = rnorm(blocks, mean = 0, sd = 0.00003))
+    rand_eff3 <- data.frame(blocks = as.factor(seq(1:number_of_blocks )),
+                            b2 = rnorm(number_of_blocks, mean = 0, sd = 0.00003))
     
     
     rand_eff_duplicated <- rand_eff[rep(row.names(rand_eff), each = n), ]
@@ -240,3 +123,5 @@ simulate_power <- function(plates, n){ # I have n = 10 because there were n = 10
 
 simulation_results <- map2_dbl(number_of_plates, number, simulate_power)
 
+#  0.965 0.980 0.965 0.965 0.965 0.960 
+# only works if you do two blocks, but 10 observations in 10 blocks gives 0.965 power 
