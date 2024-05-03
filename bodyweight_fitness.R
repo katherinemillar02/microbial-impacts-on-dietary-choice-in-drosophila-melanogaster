a0394cb9ae9ec02d6ef94301c3221cd5bcb7589d
Katie Millar <ajh20fhu@uea.ac.uk>
2024-05-03 17:22:37 +0100

    Changing the weight unit to allow for data visualisation.

--- bodyweight_fitness.R
+++ bodyweight_fitness.R
@@ -11,19 +11,19 @@ bodyweight <- read_excel("data/bodyweight_flies.xlsx")
 
 ##
 
-bodyweight$weight_mg <- bodyweight$weight_mg * 10 
+bodyweight$weight_mg <- bodyweight$weight_mg * 1000
 
 bodyweight_plot <- ggplot(bodyweight, aes(x = sex, y = weight_mg, fill = treatment)) +
   geom_boxplot() +
-  geom_jitter(position=position_jitter(height=0, width=0.2)) +
+  geom_jitter(position=position_jitter(height=0, width=0.2), size = 0.5, colour = "black") +
   scale_y_continuous(breaks=seq(0,10,2)) +
   theme_classic() +
   scale_fill_manual(values = viridis_colors[c(4,8)], labels =  c("Conditioned", "Unconditioned")) +
   theme(legend.position = "right") +
-  labs(x = "Time (hours) since eggs laid", 
+  labs(x = "Weight (μg)", 
        y = "Number of Males Emerged") +
   labs(fill = "Treatment")+
-  ylim(0,7)
+  ylim(0,700)
 
 
 
