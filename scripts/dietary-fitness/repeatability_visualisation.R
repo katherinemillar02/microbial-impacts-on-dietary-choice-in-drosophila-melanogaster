059ea185b127fea9e508beb60b873d9e44c682f1
Katie Millar <ajh20fhu@uea.ac.uk>
2024-06-01 22:48:45 +0100

    Fixing fly development.

--- scripts/dietary-fitness/repeatability_visualisation.R
+++ scripts/dietary-fitness/repeatability_visualisation.R
@@ -1,460 +1,153 @@
+library(tidyverse)
+library(readxl)
 
-#### FLY 1 
+library(ggplot2)
+library(tibble)
+library(patchwork)
 
-d1<- tibble(
-  fly = c("f1", "f2"),
-  weight = c(0.937, 0.920))
+# Data for the first plot
+d1_2 <- tibble(
+  weight1 = c(0.937,0.733,0.739,0.782,0.861),
+  weight2 = c(0.920,0.717,0.716,0.773,0.852)
+)
 
-0.937 - 0.920 = 0.017
+# Linear model and R-squared calculation
+model1 <- lm(weight1 ~ weight2, data = d1_2)
+r_squared1 <- summary(model1)$r.squared
+r_squared_text1 <- paste("R-squared:", round(r_squared1, 3))
 
-m1 <- ggplot(d1, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +    
-  ylim(0.6,0.95) +
-  theme_minimal()  
-
-
-d2<- tibble(
-  fly = c("f2", "f3"),
-  weight = c(0.920, 0.833))
-
-0.920 - 0.833 = 0.087
-
-m2 <- ggplot(d2, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() + 
-  ylim(0.6,0.95) +
-  theme_minimal()  
-
-
-
-d3<- tibble(
-  fly = c("f3", "f1"),
-  weight = c(0.833, 0.937))
-
-0.937 - 0.833 = 0.104 
-
-m3 <- ggplot(d3, aes(x = fly, y = weight, group = 1)) +
+# First plot
+m1_2 <- ggplot(d1_2, aes(x = weight1, y = weight2, group = 1)) +
   geom_point() +         
   geom_line() +  
-  ylim(0.6,0.95) +
-  theme_minimal()  
+  ylim(0.6, 1) + 
+  xlim(0.6, 1) +
+  theme_minimal() +
+  annotate("text", x = 0.7, y = 0.9, label = r_squared_text1, size = 5, color = "blue")
 
-male_fly_1 <- m1 + m2 + m3
+# Data for the second plot
+d2_3 <- tibble(
+  weight2 = c(0.920,0.717,0.716,0.773,0.852),
+  weight3 = c(0.833,0.699,0.718,0.762,0.712)
+)
 
-male_fly_1 + ggtitle("Fly 1")
+# Linear model and R-squared calculation
+model2 <- lm(weight2 ~ weight3, data = d2_3)
+r_squared2 <- summary(model2)$r.squared
+r_squared_text2 <- paste("R-squared:", round(r_squared2, 3))
 
-## 0.06933333
-
-#### FLY 2
-
-d4<- tibble(
-  fly = c("f4", "f5"),
-  weight = c(0.733, 0.717))
-
-0.733 - 0.717 = 0.016
-
-m4 <- ggplot(d4, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() + 
-  ylim(0.6,0.95) +
-  theme_minimal()  
-
-
-d5<- tibble(
-  fly = c("f5", "f6"),
-  weight = c(0.717, 0.699))
-
- 0.717 - 0.699 = 0.018
-
-m5 <- ggplot(d5, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +
-  ylim(0.6,0.95) +
-  theme_minimal()  
-
-
-
-
-
-d6<- tibble(
-  fly = c("f6", "f4"),
-  weight = c(0.699, 0.733))
-
-m6 <- ggplot(d6, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +
-  ylim(0.6,0.95) +
-  theme_minimal()  
-
-0.733 - 0.699  = 0.034
-
-male_fly_2 <- m4 + m5 + m6
-
-
-
-male_fly_2 + ggtitle("Fly 2")
-
-
-
-#### FLY 3
-
-d7<- tibble(
-  fly = c("f7", "f8"),
-  weight = c(0.739, 0.716))
-
-0.739 - 0.716 = 0.023 
-
-m7 <- ggplot(d7, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() + 
-  ylim(0.6,0.95) +
-  theme_minimal()  
-
-
-d8<- tibble(
-  fly = c("f8", "f9"),
-  weight = c(0.716, 0.718))
-
-
-0.718 - 0.716 = 0.002
-
-m8 <- ggplot(d8, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +
-  ylim(0.6,0.95) +
-  theme_minimal()  
-
-
-
-d9<- tibble(
-  fly = c("f9", "f7"),
-  weight = c(0.718, 0.739))
-
-0.739 - 0.718 = 0.021
-
-m9 <- ggplot(d9, aes(x = fly, y = weight, group = 1)) +
+# Second plot
+m2_3 <- ggplot(d2_3, aes(x = weight2, y = weight3, group = 1)) +
   geom_point() +         
   geom_line() +  
-  ylim(0.6,0.95) +
-  theme_minimal()  
+  ylim(0.6, 1) + 
+  xlim(0.6, 1) +
+  theme_minimal() +
+  annotate("text", x = 0.7, y = 0.9, label = r_squared_text2, size = 5, color = "blue")
 
-male_fly_3 <- m7 + m8 + m9
+# Data for the third plot
+d3_1 <- tibble(
+  weight3 = c(0.833,0.699,0.718,0.762,0.712),
+  weight1 = c(0.937,0.733,0.739,0.782,0.861)
+)
 
-male_fly_3 + ggtitle("Fly 3")
+# Linear model and R-squared calculation
+model3 <- lm(weight3 ~ weight1, data = d3_1)
+r_squared3 <- summary(model3)$r.squared
+r_squared_text3 <- paste("R-squared:", round(r_squared3, 3))
 
-
-#### FLY 4
-
-
-d10<- tibble(
-  fly = c("f10", "f11"),
-  weight = c(0.782, 0.773))
-
-0.782 - 0.773 = 0.009 
-
-m10 <- ggplot(d10, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +
-  ylim(0.6,0.95) +
-  theme_minimal()  
-
-
-d11<- tibble(
-  fly = c("f11", "f12"),
-  weight = c(0.773, 0.762))
-
-0.773 - 0.762 = 0.011
-
-m11 <- ggplot(d11, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +
-  ylim(0.6,0.95) +
-  theme_minimal()  
-
-
-
-d12<- tibble(
-  fly = c("f12", "f10"),
-  weight = c(0.762, 0.782))
-
-0.782 - 0.762 = 0.02
-
-m12 <- ggplot(d12, aes(x = fly, y = weight, group = 1)) +
+# Third plot
+m3_1 <- ggplot(d3_1, aes(x = weight3, y = weight1, group = 1)) +
   geom_point() +         
   geom_line() +  
-  ylim(0.6,0.95) +
-  theme_minimal()  
+  ylim(0.6, 1) + 
+  xlim(0.6, 1) +
+  theme_minimal() +
+  annotate("text", x = 0.7, y = 0.9, label = r_squared_text3, size = 5, color = "blue")
 
-male_fly_4 <- m10 + m11 + m12
+# Combine the plots using patchwork
+male_plot <- m1_2 + m2_3 + m3_1
+
+male_plot <- male_plot + ggtitle("Male")
 
 
-#### FLY 5
-d13<- tibble(
-  fly = c("f10", "f11"),
-  weight = c(0.861, 0.852))
 
-0.861 - 0.852 = 0.009
 
-m13 <- ggplot(d13, aes(x = fly, y = weight, group = 1)) +
+
+
+
+library(ggplot2)
+library(tibble)
+library(patchwork)
+
+# Female fly measurement 1 and 2
+df1_2 <- tibble(
+  weight1 = c(1.496, 1.286, 1.391, 1.454),
+  weight2 = c(1.457, 1.325, 1.416, 1.388)
+)
+
+# Linear model and R-squared calculation
+model_f1_2 <- lm(weight1 ~ weight2, data = df1_2)
+r_squared_f1_2 <- summary(model_f1_2)$r.squared
+r_squared_text_f1_2 <- paste("R-squared:", round(r_squared_f1_2, 3))
+
+# First plot
+f1_2 <- ggplot(df1_2, aes(x = weight1, y = weight2, group = 1)) +
   geom_point() +         
-  geom_line() +   
-  ylim(0.6,0.95) +
-  theme_minimal()  
+  geom_line() +  
+  ylim(1, 1.6) + 
+  xlim(1, 1.6) + 
+  theme_minimal() +
+  annotate("text", x = 1.2, y = 1.5, label = r_squared_text_f1_2, size = 5, color = "blue")
 
+# Female fly measurement 2 and 3
+df2_3 <- tibble(
+  weight2 = c(1.457, 1.325, 1.416, 1.388),
+  weight3 = c(1.477, 1.398, 1.369, 1.387)
+)
 
-d14<- tibble(
-  fly = c("f14", "f15"),
-  weight = c(0.852, 0.712))
+# Linear model and R-squared calculation
+model_f2_3 <- lm(weight2 ~ weight3, data = df2_3)
+r_squared_f2_3 <- summary(model_f2_3)$r.squared
+r_squared_text_f2_3 <- paste("R-squared:", round(r_squared_f2_3, 3))
 
-0.852 - 0.712 = 0.14
+# Second plot
+f2_3 <- ggplot(df2_3, aes(x = weight2, y = weight3, group = 1)) +
+  geom_point() +         
+  geom_line() +  
+  ylim(1, 1.6) + 
+  xlim(1, 1.6) + 
+  theme_minimal() +
+  annotate("text", x = 1.2, y = 1.5, label = r_squared_text_f2_3, size = 5, color = "blue")
+
+# Female fly measurement 3 and 1
+df3_1 <- tibble(
+  weight3 = c(1.477, 1.398, 1.369, 1.387),
+  weight1 = c(1.496, 1.286, 1.391, 1.454)
+)
+
+# Linear model and R-squared calculation
+model_f3_1 <- lm(weight3 ~ weight1, data = df3_1)
+r_squared_f3_1 <- summary(model_f3_1)$r.squared
+r_squared_text_f3_1 <- paste("R-squared:", round(r_squared_f3_1, 3))
+
+# Third plot
+f3_1 <- ggplot(df3_1, aes(x = weight3, y = weight1, group = 1)) +
+  geom_point() +         
+  geom_line() +  
+  ylim(1, 1.6) + 
+  xlim(1, 1.6) + 
+  theme_minimal() +
+  annotate("text", x = 1.2, y = 1.5, label = r_squared_text_f3_1, size = 5, color = "blue")
+
+# Combine the plots using patchwork
+female_plot <- f1_2 + f2_3 + f3_1
  
-m14 <- ggplot(d14, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() + 
-  ylim(0.6,0.95) +
-  theme_minimal()  
+female_plot <- female_plot + ggtitle("Female")
 
 
+female_plot /
+  male_plot
 
-d15 <- tibble(
-  fly = c("f15", "f16"),
-  weight = c(0.712, 0.861))
 
-0.861 - 0.712 = 0.149
 
-
-m15 <- ggplot(d15, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +  
-  ylim(0.6,0.95) +
-  theme_minimal()  
-
-male_fly_5 <- m13 + m14 + m15
-
-male_fly_1 / 
-  male_fly_2 /
-  male_fly_3 /
-  male_fly_4 /
-  male_fly_5 
-
-
-
-
-
-
-
-d1<- tibble(
-  fly = c("f1", "f2"),
-  weight = c(1.496, 1.457))
-
-
-1.496 -  1.457 = 0.039
-
-f1 <- ggplot(d1, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +   
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-d2<- tibble(
-  fly = c("f2", "f3"),
-  weight = c(1.457, 1.477))
-
-1.477 - 1.457 = 0.02
-
-f2 <- ggplot(d2, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +   
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-
-d3<- tibble(
-  fly = c("f3", "f1"),
-  weight = c(1.477, 1.496))
-
-1.496 - 1.477 = 0.019
-
-f3 <- ggplot(d3, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +  
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-female_fly_1 <- f1 + f2 + f3
-
-
-
-d4 <- tibble(
-  fly = c("f4", "f5"),
-  weight = c(1.508, 1.118))
-
-1.508 - 1.118 = 0.39
-
-
-f4 <- ggplot(d4, aes(x = fly, y = weight, group = 1)) +
-  geom_point()+         
-  geom_line() +  
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-
-female_fly_2 <- f4 
-
-
-
-
-
-
-
-
-
-d7<- tibble(
-  fly = c("f7", "f8"),
-  weight = c(1.286, 1.325))
-
-1.325 - 1.286 =  0.039
-
-f7 <- ggplot(d7, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +  
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-d8<- tibble(
-  fly = c("f2", "f3"),
-  weight = c(1.325, 1.398))
-
-1.398 - 1.325 = 0.073
-
-f8 <- ggplot(d8, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +  
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-
-d9<- tibble(
-  fly = c("f9", "f7"),
-  weight = c(1.398, 1.286))
-
-1.398 - 1.286 = 0.112
-
-f9 <- ggplot(d9, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() + 
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-
-
-female_fly_3 <- f7 + f8 + f9
-
-
-
-d10 <- tibble(
-  fly = c("f10", "f11"),
-  weight = c(1.391, 1.416))
-
-1.416 - 1.391 = 0.025
-
-f10 <- ggplot(d10, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +   
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-d11 <- tibble(
-  fly = c("f11", "f12"),
-  weight = c(1.416, 1.369))
-
-1.416 - 1.369 = 0.047
-
-f11 <- ggplot(d11, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +  
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-
-d12 <- tibble(
-  fly = c("f12", "f10"),
-  weight = c(1.369, 1.391))
-
-
-1.391 - 1.369 = 0.022
-
-f12 <- ggplot(d12, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() + 
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-female_fly_4 <- f10 + f11 + f12 
-
-
-
-
-
-
-d13 <- tibble(
-  fly = c("f13", "f14"),
-  weight = c(1.454, 1.388))
-
-1.454 - 1.388 = 0.066
-
-f13 <- ggplot(d13, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() + 
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-d14 <- tibble(
-  fly = c("f14", "f15"),
-  weight = c(1.388, 1.387))
-
-
-1.388 - 1.387 = 0.001
-
-f14 <- ggplot(d14, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() +      
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-
-d15 <- tibble(
-  fly = c("f15", "f13"),
-  weight = c(1.387, 1.454))
-
-1.454 - 1.387 = 0.067
-
-
-f15 <- ggplot(d15, aes(x = fly, y = weight, group = 1)) +
-  geom_point() +         
-  geom_line() + 
-  ylim(1.2,1.5) +
-  theme_minimal()  
-
-
-female_fly_5 <- f13 + f14 + f15 
-
-
-female_fly_1 /
-#  female_fly_2 /
-  female_fly_3 /
-  female_fly_4 /
-  female_fly_5
