eef5e92116e36d88fb2431fe9d6b5770a5207692
Katie Millar <ajh20fhu@uea.ac.uk>
2024-06-06 21:03:36 +0100

    Readding pattern to the plot.

--- scripts/density-conditioning/density_oviposition_visualisation.R
+++ scripts/density-conditioning/density_oviposition_visualisation.R
@@ -59,31 +59,32 @@ fourone_onefour_50mm_long <- fourone_onefour_50mm  %>%
 #################################### Feeding Results Function Plot ####################################
 
 oviposition_results <- function(summary_data,boxplot_fill_colour ) {
-  ggplot(summary_data, aes(x = diet, y = fly_numbers, fill = diet)) + #, pattern = diet))+ 
+  ggplot(summary_data, aes(x = diet, y = fly_numbers, fill = diet, pattern = diet))+ 
     # geom_jitter(aes(x = diet,
     #                 y = fly_numbers,
     #                 fill = diet),
     #             width = 0.1,
     #             shape = 1) +
     geom_boxplot()+
-    # geom_boxplot_pattern(position = position_dodge(preserve = "single"), 
-    #                     color = "black",
-    #                     pattern_fill = "white",
-    #                     pattern_angle = 45,
-    #                     pattern_density = 0.1,
-    #                     pattern_spacing = 0.025,
-    #                     pattern_key_scale_factor = 0.6) +
+    geom_boxplot_pattern(position = position_dodge(preserve = "single"),
+                        color = "black",
+                        pattern_fill = "white",
+                        pattern_angle = 45,
+                        pattern_density = 0.1,
+                        pattern_spacing = 0.025,
+                        pattern_key_scale_factor = 0.6) +
     geom_point(aes(),
                size = 1,
                shape = 1,
                position = position_jitterdodge()) +
     theme_classic()+
     labs(x = "Diet Condition",
-         y = "Flies", 
+         y = "Eggs per diet patch", 
          title = "")+
     scale_fill_manual(values = boxplot_fill_colour) +  # Set fill colors for the boxplot
-    # scale_pattern_manual(values = c("stripe", "none", "stripe", "none")) +
-    theme(legend.position = "none") +
+    scale_pattern_manual(values = c("stripe", "none", "stripe", "none")) +
+    theme(legend.position = "none",
+          axis.text.x = element_text(size = 6)) + 
     ylim(-0.01, 200) 
   
 }
@@ -92,22 +93,26 @@ oviposition_results <- function(summary_data,boxplot_fill_colour ) {
 viridis_colours <- viridis(10)
 
 
-ninety_1_4 <- oviposition_results(onefour_90mm_long , boxplot_fill_colour = viridis_colours[1:2])
-ninety_4_1 <- oviposition_results(fourone_90mm_long , boxplot_fill_colour = viridis_colours[3:4])
-ninety_combined <- oviposition_results(fourone_onefour_90mm_long, boxplot_fill_colour = viridis_colours[1:84])
+ninety_1_4 <- oviposition_results(onefour_90mm_long , boxplot_fill_colour = viridis_colors[c(6,6)])
+ninety_4_1 <- oviposition_results(fourone_90mm_long , boxplot_fill_colour = viridis_colors[c(8,8)])
+ninety_combined <- oviposition_results(fourone_onefour_90mm_long, boxplot_fill_colour = viridis_colors[c(6,6,8,8)])
+
+ninety_1_4 <- ninety_1_4 + ggtitle("90 mm")
 
 ninety <- ninety_1_4 + ninety_4_1 + ninety_combined
 
 
 
-fifty_1_4 <- oviposition_results(onefour_50mm_long , boxplot_fill_colour = viridis_colours[1:2])
-fifty_4_1 <- oviposition_results(fourone_50mm_long , boxplot_fill_colour = viridis_colours[3:4])
-fifty_combined <- oviposition_results(fourone_onefour_50mm_long, boxplot_fill_colour = viridis_colours[1:4])
+fifty_1_4 <- oviposition_results(onefour_50mm_long , boxplot_fill_colour = viridis_colors[c(6,6)])
+fifty_4_1 <- oviposition_results(fourone_50mm_long , boxplot_fill_colour = viridis_colors[c(8,8)])
+fifty_combined <- oviposition_results(fourone_onefour_50mm_long, boxplot_fill_colour = viridis_colors[c(6,6,8,8)])
+
+fifty_1_4 <- fifty_1_4 + ggtitle("35 mm")
+
 
 fifty <- fifty_1_4 +  fifty_4_1 + fifty_combined 
 
 ninety / 
   fifty
 
-ninety <- ninety + ggtitle("90 mm")
-fifty <- fifty + ggtitle("50 mm")
+
