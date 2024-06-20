dcbe639ce7b1128116d0f98b12623e23d323d2c1
Katie Millar <ajh20fhu@uea.ac.uk>
2024-06-20 15:19:23 +0100

    Rewriting model.

--- scripts/density-conditioning/analysis-density.R
+++ scripts/density-conditioning/analysis-density.R
@@ -36,7 +36,7 @@ emmeans::emmeans(glm_mm_density_90, pairwise ~ diet)
 
 combined_assays <- rbind(fourone_onefour_50mm_long, fourone_onefour_90mm_long)
 
-glm_mm_density_combined <- glmmTMB(fly_numbers ~ ratio * treatment * density + (1|plate) + (1|observation), family = poisson, data = combined_assays)
+glm_mm_density_combined <- glmmTMB(fly_numbers ~ ratio + treatment * density + (1|plate) + (1|observation), family = poisson, data = combined_assays)
 
 drop1(glm_mm_density_combined, test = "Chisq") ## no interaction effect
 
