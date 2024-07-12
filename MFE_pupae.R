cff8065a8caabd16543054066934336a6ce8b3d0
Katie Millar <ajh20fhu@uea.ac.uk>
2024-07-12 10:26:36 +0100

    Updating the manipulated fitness experiment with new data.

--- MFE_pupae.R
+++ MFE_pupae.R
@@ -16,27 +16,6 @@ pupae_fitness <- read_excel("data/fitness_development/MFE_pupae.xlsx")
 
 
 
-
-## Boxplot ##
-pupae_boxplot_2 <- ggplot(pupae_fitness, aes(x = time_hours, y = pupae, fill = treatment)) +
-  geom_boxplot(outliers = FALSE, alpha =.4, position = position_dodge (width = 20)) + #width =10 ) +
-  geom_point(aes(fill = treatment),
-             size = 1.5, shape = 21,
-             position = position_jitterdodge(jitter.width = 6, dodge.width = 20)) +
-  scale_fill_manual(values = viridis_colors[c(4,8)], labels =  c("Conditioned", "Unconditioned")) +
-  scale_x_continuous(breaks = unique(pupae_fitness$time_hours), labels = unique(pupae_fitness$time_hours)) +
-  theme_classic() +
-  theme(legend.position = "top",
-        legend.justification = "right",
-        legend.direction = "vertical",
-        strip.placement = "outside", 
-        strip.background = element_blank(),  
-        strip.text = element_blank())+
-  labs(x = "Time (hours) since eggs laid", 
-       y = "Number of pupae emerged",
-       fill = "Treatment") +
-  facet_grid(~time_hours, scales = "free_x")
-
 ## Boxplot ##
 pupae_boxplot_2 <- ggplot(pupae_fitness, aes(x = factor(time_hours), y = pupae, fill = treatment)) +
   geom_boxplot(outlier.shape = NA, alpha = .4, position = position_dodge(width = 0.9)) +
