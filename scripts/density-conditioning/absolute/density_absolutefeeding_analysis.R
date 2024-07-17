0dfbbea5830a70da9f5e888d6657bea8692b9f5d
Katie Millar <ajh20fhu@uea.ac.uk>
2024-07-17 09:07:27 +0100

    Cleaning density anlaysis script.

--- scripts/density-conditioning/absolute/density_absolutefeeding_analysis.R
+++ scripts/density-conditioning/absolute/density_absolutefeeding_analysis.R
@@ -22,11 +22,11 @@ library(glmmTMB)
 fourone_onefour_35mm <- read_excel("data/density_experiment/densityexperiment_50mm_4-1_1-4.xlsx")
 
 ## Making the data long 
-fourone_onefour_50mm_long <- fourone_onefour_50mm  %>% 
+fourone_onefour_35mm_long <- fourone_onefour_35mm  %>% 
   pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers")
 
 ## Mutating a density variable 
-fourone_onefour_50mm_long <- fourone_onefour_50mm_long %>%
+fourone_onefour_35mm_long <- fourone_onefour_35mm_long %>%
   mutate(density = "50mm")
 
 
@@ -42,7 +42,7 @@ fourone_onefour_90mm_long <- fourone_onefour_90mm_long %>%
   mutate(density = "90mm")
 
 ## Binding the two densities 
-combined_assays <- rbind(fourone_onefour_50mm_long, fourone_onefour_90mm_long)
+combined_assays <- rbind(fourone_onefour_35mm_long, fourone_onefour_90mm_long)
 
 
 
@@ -55,7 +55,9 @@ combined_assays <- combined_assays %>%
 
 ## Binomial GLMM
 ## Testing for a 3-way interaction
-glmm.density.4choice <- glmmTMB(fly_numbers ~ ratio * treatment * density + ratio * treatment + ratio * density + treatment * density + (1|plate) + (1|observation), family = poisson, data = combined_assays)
+glmm.density.4choice <- glmmTMB(fly_numbers ~ ratio * treatment * density
+                                
+                                + (1|plate) + (1|observation), family = poisson, data = combined_assays)
 
 ## Testing for a 3-way interaction effect
 drop1(glmm.density.4choice, test = "Chisq") 
@@ -69,7 +71,18 @@ drop1(glmm.density.4choice.2, test = "Chisq")
   ## Interaction effects found between ratio and treatment, and ratio and density
 
 ## Final model, contains significant interaction effects
-glmm.density.4choice.3 <- glmmTMB(fly_numbers ~  ratio * treatment + ratio * density  + (1|plate) + (1|observation), family = poisson, data = combined_assays)
+glmm.density.4choice.3 <- glmmTMB(fly_numbers ~  ratio * treatment
+                                  +  density  
+                                  + (1|plate) + (1|observation), family = poisson, data = combined_assays)
+
+
+
+## Changing the intercept to ratio: 4:1 
+combined_assays$ratio <- as.factor(combined_assays$ratio)
+combined_assays$ratio <- relevel(combined_assays$ratio, ref = "4:1")
+
+combined_assays$density <- as.factor(combined_assays$density)
+combined_assays$density <- relevel(combined_assays$density, ref = "90mm")
 
 ## Shows thwe remaining interaction effects.
 drop1(glmm.density.4choice.3, test = "Chisq") 
