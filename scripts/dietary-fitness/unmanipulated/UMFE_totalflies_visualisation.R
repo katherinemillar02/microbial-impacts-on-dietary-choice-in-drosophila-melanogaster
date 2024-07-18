44144bbcd2bd3b51cf815ad49753fc119db458d3
Katie Millar <ajh20fhu@uea.ac.uk>
2024-07-18 14:19:08 +0100

    Making a script for visualising data for overall fly emergence.

--- scripts/dietary-fitness/unmanipulated/UMFE_totalflies_visualisation.R
+++ scripts/dietary-fitness/unmanipulated/UMFE_totalflies_visualisation.R
@@ -1,9 +1,36 @@
+# Packages 📦📦📦📦####
+library(tidyverse)
+library(lmerTest)
+library(readxl)
+library(MASS)
+library(performance)
+library(pscl)
+library(DHARMa)
+library(glmmTMB)
+
+
+#### Reading data in: ####
+fly_fitness_UMFE <- read_excel("data//fitness_development/fly_data.xlsx")
 
 
 
 
+
+overallflies_UMFE <- fly_fitness_UMFE %>%
+  filter(sex %in% c("females", "males")) %>%
+  group_by(vial, sex, treatment) %>%
+  summarise(total_count = sum(count, na.rm = TRUE)) %>%
+  ungroup() %>%
+  mutate(sex_treatment = paste(treatment, sex, sep = " ")) %>%
+  mutate(sex_treatment = factor(sex_treatment,
+                                levels = c("conditioned females", "unconditioned females",
+                                           "conditioned males", "unconditioned males")))
+
+
+
+ 
 ## DATA VISUALISA5ION - Visualising overall emergence across vials 
-overall_emergence_sex_treatment <- ggplot(fly_emergence_sex, aes(x = sex, y = total_count, fill = treatment)) +
+overall_emergence_sex_treatment <- ggplot(overallflies_UMFE, aes(x = sex, y = total_count, fill = treatment)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.4, position = position_dodge(width = 0.75)) + 
   geom_point(aes(fill = treatment), 
              size = 2, shape = 21, 
