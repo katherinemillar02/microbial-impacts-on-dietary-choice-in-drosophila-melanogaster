9767e7246ab3dd661de58e4e249e2d05916272b4
Katie Millar <ajh20fhu@uea.ac.uk>
2024-07-23 18:06:49 +0100

    Making an overall emergence pupae plot.

--- scripts/dietary-fitness/manipulated/MFE_pupae.R
+++ scripts/dietary-fitness/manipulated/MFE_pupae.R
@@ -41,11 +41,29 @@ pupae_boxplot_MFE
 
 
 #### Pupae data check. 
-pupae_fitness <- as.data.frame(pupae_fitness)
 
-total_pupae <- pupae_fitness %>% 
-  group_by(treatment, vial) %>% 
+total_pupae <- pupae_fitness_MFE %>% 
+  group_by(treatment, id) %>% 
   summarise(total_pupae = sum(pupae, na.rm = TRUE))
+            
+            
+total_pupae <- as.data.frame(total_pupae)
 
-524  / 14 
-555 / 15
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+overall_emergence_treatment <- ggplot(total_pupae, aes(x = treatment, y = total_pupae, fill = treatment)) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.4, position = position_dodge(width = 0.75)) + 
+  geom_point(aes(fill = treatment), 
+             size = 2, shape = 21, 
+             position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.75)) +
+  scale_y_continuous(breaks = seq(0, 80, 10)) +
+  scale_x_discrete(labels = c("Conditioned", "Unconditioned")) + 
+  theme_classic() +
+  scale_fill_manual(values = viridis_colors[c(4, 8)], labels = c("Conditioned", "Unconditioned")) +
+  theme(legend.position = "top",
+        legend.justification = "right",
+        legend.direction = "vertical") +
+  labs(x = "Treatment", 
+       y = "Number of Pupae Emerged", 
+       fill = "Treatment") +
+  ylim(0, 350)
