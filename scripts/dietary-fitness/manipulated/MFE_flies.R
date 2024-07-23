f02e05ec68cd1d9edf70cd23d9e29957d77f1f45
Katie Millar <ajh20fhu@uea.ac.uk>
2024-07-23 18:04:51 +0100

    Adding an overall emergence plot.

--- scripts/dietary-fitness/manipulated/MFE_flies.R
+++ scripts/dietary-fitness/manipulated/MFE_flies.R
@@ -80,9 +80,30 @@ female_boxplot_2 /
 fly_fitness <- as.data.frame(fly_fitness)
 
 total_flies <- fly_fitness_MFE %>% 
-  group_by(treatment, id) %>% 
+  group_by(treatment, id, sex) %>% 
   summarise(total_flies = sum(males, females, na.rm = TRUE))
 
 
 total_flies <- as.data.frame(total_flies)
 ## more conditioned than unconditioned have come out now? 
+
+
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+overall_emergence_treatment <- ggplot(total_flies, aes(x = treatment, y = total_flies, fill = treatment)) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.4, position = position_dodge(width = 0.75)) + 
+  geom_point(aes(fill = treatment), 
+             size = 2, shape = 21, 
+             position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.75)) +
+  scale_y_continuous(breaks = seq(0, 80, 10)) +
+  scale_x_discrete(labels = c("Females", "Males")) + 
+  theme_classic() +
+  scale_fill_manual(values = viridis_colors[c(4, 8)], labels = c("Conditioned", "Unconditioned")) +
+  theme(legend.position = "top",
+        legend.justification = "right",
+        legend.direction = "vertical") +
+  labs(x = "Treatment", 
+       y = "Number of Flies Emerged", 
+       fill = "Treatment") +
+  ylim(0, 200)
