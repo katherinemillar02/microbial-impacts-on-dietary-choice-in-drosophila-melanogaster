ffe55df8add6c3b9f7dbbb554ff963de46ffa204
Katie Millar <ajh20fhu@uea.ac.uk>
2024-07-31 17:11:40 +0100

    Editing unmanipulated total pupae analysis and visualisation data.

---
+++ scripts/dietary-fitness/unmanipulated/UMFE_totalpupae_viz.R
@@ -0,0 +1,24 @@
+
+## DATA VISUALISA5ION - Visualising overall emergence across vials 
+overall_emergence_pupae_treatment <- ggplot(overallpupae_UMFE, aes(x = treatment, y = total_count, fill = treatment)) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.4, position = position_dodge(width = 0.8)) + 
+  geom_point(aes(fill = treatment), 
+             size = 2, shape = 21, 
+             position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.75)) +
+  scale_y_continuous(breaks = seq(0, 80, 10)) +
+  theme_classic() +
+  scale_fill_manual(values = viridis_colors[c(4, 8)], labels = c("Conditioned", "Unconditioned")) +
+  theme(legend.position = "top",
+        legend.justification = "right",
+        legend.direction = "vertical") +
+  labs(x = "Treatment", 
+       y = "Number of Flies Emerged", 
+       fill = "Treatment") +
+  ylim(0, 125)
+
+
+ggsave(filename = "overall_emergence_pupae_treatment.png", 
+       plot = overall_emergence_pupae_treatment, 
+       width = 10, 
+       height = 6, 
+       dpi = 300)
