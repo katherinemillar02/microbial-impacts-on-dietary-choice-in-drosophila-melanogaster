3ac00ec96cf00c58b0c98e911890a0dab727a657
Katie Millar <ajh20fhu@uea.ac.uk>
2024-09-04 13:13:55 +0100

    Beginning to update and clean script.

--- scripts/density-conditioning/absolute/absolute_dose_feeding.R
+++ scripts/density-conditioning/absolute/absolute_dose_feeding.R
@@ -1,74 +1,110 @@
+## Chapter 4 - Appendix
 
-                        #### DENSITY CONDITIONING PART 2 (FIRST ONE WAS PILOT) ####
 
-                                       #### RELATIVE ANALYSIS ####
+#### Experiment - Density of Conditioning - Feeding
+
+#### Assay: Absolute 
+
+
+#### Packages ####
+library(tidyverse)
+library(lmerTest)
+library(readxl)
+library(MASS)
+library(performance)
+library(pscl)
+library(DHARMa)
+library(glmmTMB)
+library(sjPlot)
+##################---
+
 
                         
-## Packages 
-                        library(tidyverse)
-                        library(lmerTest)
-                        library(readxl)
-                        library(MASS)
-                        library(performance)
-                        library(pscl)
-                        library(DHARMa)
-                        library(glmmTMB)
-                        library(sjPlot)
-                        ##################---
                         
                         
-                        
-                        
-                        
-## Reading the data in:
-                        
-## 90 mm 
+#### 📖 Reading, cleaning, editing the data: ####
+
+
+#### Low density - 90 mm ####
+
+
+        ## 4:1 ##
+# Reading data in with read_excel()
 fourone_90mm <- read_excel("data/density_experiment/densityexperiment_90mm_4-1.xlsx")
-onefour_90mm <- read_excel("data/density_experiment/densityexperiment_90mm_1-4.xlsx")
 
 ## Adding the appropriate data variables
 fourone_90mm_long <- fourone_90mm   %>% 
   pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
 
-onefour_90mm_long <- onefour_90mm  %>% 
-  pivot_longer(cols = ("1:4 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
-
 ## Mutating an id variable to better read the data in 
 fourone_90mm_long <- fourone_90mm_long %>%  
   mutate(id = "4-1_90")
 
+
+         ## 1:4 ##
+# Reading data in with read_excel()
+onefour_90mm <- read_excel("data/density_experiment/densityexperiment_90mm_1-4.xlsx")
+
+
+## Adding the appropriate data variables
+onefour_90mm_long <- onefour_90mm  %>% 
+  pivot_longer(cols = ("1:4 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
+
+## Mutating an id variable to better read the data in 
 onefour_90mm_long <- onefour_90mm_long  %>%  
   mutate(id = "1-4_90")
 
 
 
-## 35 mm 
+
+
+
+
+
+
+#### High density - 35 mm ####
+
+
+           ## 4:1 ##
+# Reading data in with read_excel()
 fourone_35mm <- read_excel("data/density_experiment/densityexperiment_50mm_4-1.xlsx")
-onefour_35mm <- read_excel("data/density_experiment/densityexperiment_50mm_1-4.xlsx")
 
 ## Adding the appropriate data variables
 fourone_35mm_long <- fourone_35mm   %>% 
   pivot_longer(cols = ("4:1 Conditioned":"4:1 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
 
+## Mutating an id variable to mutate a variable 
+fourone_35mm_long <- fourone_35mm_long %>%  
+  mutate(id = "4-1_50")
+
+
+
+
+
+
+
+              ## 1:4 ##
+# Reading data in with read_excel()
+onefour_35mm <- read_excel("data/density_experiment/densityexperiment_50mm_1-4.xlsx")
+
+## Adding the appropriate data variables
 onefour_35mm_long <- onefour_35mm  %>% 
   pivot_longer(cols = ("1:4 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "fly_numbers") 
 
 
 ## Mutating an id variable to mutate a variable 
-fourone_35mm_long <- fourone_35mm_long %>%  
-  mutate(id = "4-1_50")
-
 onefour_35mm_long <- onefour_35mm_long  %>%  
   mutate(id = "1-4_50")
 
 
 
+
 ## Binding the data to one data frame 
-two_choice_density <- rbind( fourone_35mm_long, onefour_35mm_long, fourone_90mm_long, onefour_90mm_long )
+density_feeding_2choice <- rbind( fourone_35mm_long, onefour_35mm_long, fourone_90mm_long, onefour_90mm_long )
 
 
 ## Mutating a density variable 
-two_choice_density_1 <- two_choice_density %>%
+density_feeding_2choice <- density_feeding_2choice %>%
   mutate(density = case_when(
     str_detect(id, "50") ~ "50",
     str_detect(id, "90") ~ "90",
@@ -76,11 +112,16 @@ two_choice_density_1 <- two_choice_density %>%
 
 
 ## Reading the data in, and separating diet into ratio and condition
-two_choice_density_df <- two_choice_density_1 %>%
+density_feeding_2choice_df <- density_feeding_2choice %>%
   separate(diet, into = c("ratio", "condition"), sep = " ") %>%
   group_by(id, observation, plate, ratio, condition, density) %>%
   summarise(count = sum(fly_numbers)) %>%
   pivot_wider(names_from = "condition", values_from = "count")
+
+
+
+
+
 
 
 
