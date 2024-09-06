3309c2db5a779975a1d2ca9ad254bcddc41ec8b6
Katie Millar <ajh20fhu@uea.ac.uk>
2024-09-06 22:09:30 +0100

    Adding overall analysis

--- scripts/dietary-fitness/controlled_density/survivability_and_overallemergence/MFE_overallpupae_analysis.R
+++ scripts/dietary-fitness/controlled_density/survivability_and_overallemergence/MFE_overallpupae_analysis.R
@@ -54,49 +54,53 @@ check_zeroinflation(glmm.p.MFE.totalpupae)
 
 
 
+# Model 2 
+# Negative Binomial GLM #
+glm.nb.MFE.totalpupae <- glm.nb(total_pupae ~ 
+                                  
+                                  treatment,
+                          
+                          data = total_pupae)
+
+
+## Assumption checks 
+# DHARMa
+simulationOutput <- simulateResiduals(fittedModel = glm.nb.MFE.totalpupae, plot = T)
+
+
+# easyststas checks
+check_overdispersion(glm.nb.MFE.totalpupae)
+# No overdispersion 
+
+
+# Comparing models 
+AIC(glmm.p.MFE.totalpupae, glm.nb.MFE.totalpupae)
+ # Negative Binomial GLM
 
 
 
 
 
-# Data analysis
+
+
+
+
+
+
+#### Final Data analysis ####
+
+# Simple analysus
 summary(glmm.p.MFE.totalpupae)
 
+# Confidence intervals 
 exp(confint(glmm.p.MFE.totalpupae))
+
+
+# getting values for the write-up
+emmeans::emmeans(glmm.p.MFE.totalpupae, specs = ~ treatment, type = "response")
+
 
 # Using a table to view the data
 tab_model(glmm.p.MFE.totalpupae, CSS = list(css.table = '+font-family: Arial;'))
-
-
-simulationOutput <- simulateResiduals(fittedModel = glmm.p.MFE.totalpupae, plot = T)
-
-
-
-## DATA VISUALISATION ##
-#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
-
-overall_emergence_pupae <- ggplot(total_pupae, aes(x = treatment, y = total_pupae, fill = treatment)) +
-  geom_boxplot(outlier.shape = NA, alpha = 0.4, position = position_dodge(width = 0.75)) + 
-  geom_point(aes(fill = treatment), 
-             size = 2, shape = 21, 
-             position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.75)) +
-  scale_y_continuous(breaks = seq(0, 80, 10)) +
-  scale_x_discrete(labels = c("Conditioned", "Unconditioned")) + 
-  theme_classic() +
-  scale_fill_manual(values = viridis_colors[c(4, 8)], labels = c("Conditioned", "Unconditioned")) +
-  theme(legend.position = "top",
-        legend.justification = "right",
-        legend.direction = "vertical") +
-  labs(x = "Treatment", 
-       y = "Number of Pupae Emerged", 
-       fill = "Treatment") +
-  ylim(0, 75)
-
-
-## Saving a plot
-ggsave(filename = "overall_emergence_treatment_MFE..png", 
-       plot = overall_emergence_pupae, 
-       width = 10, 
-       height = 6, 
-       dpi = 300)
+# looking okay
 
