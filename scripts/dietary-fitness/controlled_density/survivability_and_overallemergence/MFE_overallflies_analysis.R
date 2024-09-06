ce115e179fafaebb770dbac4b72c871b98cb9cd8
Katie Millar <ajh20fhu@uea.ac.uk>
2024-09-06 22:36:45 +0100

    Cleaning script

--- scripts/dietary-fitness/controlled_density/survivability_and_overallemergence/MFE_overallflies_analysis.R
+++ scripts/dietary-fitness/controlled_density/survivability_and_overallemergence/MFE_overallflies_analysis.R
@@ -172,51 +172,18 @@ glm.zi.p.MFE.flies.2 <- glmmTMB(
 
 
 #### DATA ANALYSIS ####
+
+# Basic analysis
 summary(glm.zi.p.MFE.flies.2)
 
-
-
-
+# Confidence intervals
 exp(confint(glmm.p.total.MFE.2))
 
-
+# Values for analysis write-up
 emmeans::emmeans(glm.zi.p.MFE.flies.2, specs = ~ sex + treatment, type = "response")
 
-
-
+# Table for write-up
 tab_model(glm.zi.p.MFE.flies.2, CSS = list(css.table = '+font-family: Arial;'))
 
 
 
-
-
-## DATA VISUALISATION
-#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
-
-viridis_colors <- viridis(10)
-
-
-overall_emergence_treatment <- ggplot(overallflies_MFE, aes(x = sex, y = total_count, fill = treatment)) +
-  geom_boxplot(outlier.shape = NA, alpha = 0.4, position = position_dodge(width = 0.8)) + 
-  geom_point(aes(fill = treatment), 
-             size = 2, shape = 21, 
-             position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.75)) +
-  scale_y_continuous(breaks = seq(0, 80, 10)) +
-  scale_x_discrete(labels = c("Females", "Males")) + 
-  theme_classic() +
-  scale_fill_manual(values = viridis_colors[c(4, 8)], labels = c("Conditioned", "Unconditioned")) +
-  theme(legend.position = "top",
-        legend.justification = "right",
-        legend.direction = "vertical") +
-  labs(x = "Treatment", 
-       y = "Number of Flies Emerged", 
-       fill = "Treatment") +
-  ylim(0, 30)
-
-
-## Saving a plot
-ggsave(filename = "overall_emergence_treatment.png", 
-       plot = overall_emergence_treatment, 
-       width = 10, 
-       height = 6, 
-       dpi = 300)
