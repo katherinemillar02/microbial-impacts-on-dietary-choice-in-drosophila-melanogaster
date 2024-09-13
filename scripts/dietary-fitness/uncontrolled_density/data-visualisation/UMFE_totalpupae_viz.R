769c9c413baac512c5e785d32f53fbef8976139b
Katie Millar <ajh20fhu@uea.ac.uk>
2024-09-13 15:01:43 +0100

    Cleaning up scripts, making them neat and easier to follow.

--- scripts/dietary-fitness/uncontrolled_density/data-visualisation/UMFE_totalpupae_viz.R
+++ scripts/dietary-fitness/uncontrolled_density/data-visualisation/UMFE_totalpupae_viz.R
@@ -1,10 +1,18 @@
 
+#### Packages 📦📦📦📦####
+library(tidyverse)
+library(lmerTest)
+library(readxl)
+library(MASS)
+library(performance)
+library(pscl)
+library(DHARMa)
+library(glmmTMB)
+library(viridisLite)
+
 
 #### Reading data in: ####
 pupae_fitness_UMFE <- read_excel("data/fitness_development/pupae_data.xlsx")
-
-
-
 
 
 #### This code shows each vial, for each sex, and for each treatment
@@ -13,12 +21,17 @@ pupae_fitness_UMFE <- read_excel("data/fitness_development/pupae_data.xlsx")
 ### EMERGENCE BY SEX
 overallpupae_UMFE <- pupae_fitness_UMFE %>%
   group_by(vial, treatment) %>%
-  summarise(total_count = sum(pupae, na.rm = TRUE)) %>%
+  summarise(total_count = sum(pupae, na.rm = FALSE)) %>%
   ungroup() 
 
 
 
+## Code to make nice plot 
+viridis_colors <- viridis(10)
 
+
+
+                                                 ## THE PLOT ## 
 
 overall_emergence_pupae_treatment <- ggplot(overallpupae_UMFE, aes(x = treatment, y = total_count, fill = treatment)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.4, position = position_dodge(width = 0.8)) + 
@@ -37,7 +50,12 @@ overall_emergence_pupae_treatment <- ggplot(overallpupae_UMFE, aes(x = treatment
   ylim(0, 150) +
   scale_x_discrete(labels = c("Conditioned", "Unconditioned"))
 
+# Run plot 
+overall_emergence_pupae_treatment
 
+
+
+# The code to save the plot as an image
 ggsave(filename = "overall_emergence_pupae_treatment.png", 
        plot = overall_emergence_pupae_treatment, 
        width = 10, 
