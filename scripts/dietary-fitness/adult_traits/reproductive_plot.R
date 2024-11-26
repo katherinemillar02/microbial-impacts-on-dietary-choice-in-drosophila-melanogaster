547fd739adec82aeabe6174ef6615a44c74d76f1
Katie Millar <ajh20fhu@uea.ac.uk>
2024-11-26 11:25:07 +0000

    Trying to make a reproductio plot

--- scripts/dietary-fitness/adult_traits/reproductive_plot.R
+++ scripts/dietary-fitness/adult_traits/reproductive_plot.R
@@ -11,23 +11,21 @@ viridis_colors <- viridis(10)
 
 
 ## Reading pupae data in
-reproductive_adultstraits <- read_excel("data/fitness_development/treatment_reproductive.xlsx")
-reproductive_adultstraits <- as.data.frame(reproductive_adultstraits)
+reproductive_adultstraits_f <- read_excel("data/fitness_development/treatment_reproductive.xlsx", sheet = "females")
+reproductive_adultstraits_f  <- as.data.frame(reproductive_adultstraits_f)
 
-## adding a sex section 
-reproductive_adultstraits$Conditioning <- ifelse(grepl("Conditioned", reproductive_adultstraits$treatment), "Conditioned", "Unconditioned")
-reproductive_adultstraits$Sex <- ifelse(grepl("female", reproductive_adultstraits$treatment), "Focal female", "Focal male")
+
 
 
 # Plot
 # Updated plot
-reproductive_boxplot_adultstraits <- ggplot(reproductive_adultstraits, 
-                                            aes(x = Conditioning, y = offspring, fill = treatment)) +
+reproductive_boxplot_adultstraits <- ggplot(reproductive_adultstraits_f, 
+                                            aes(x = factor(day), y = os, fill = treatment)) +
   geom_boxplot(outlier.shape = NA, alpha = .4, position = position_dodge(width = 0.9)) +
   geom_point(aes(fill = treatment),
              size = 1.5, shape = 21,
              position = position_jitterdodge(jitter.width = 0.3, dodge.width = 0.9)) +
-  scale_fill_manual(values = viridis_colors[c(4, 4, 8, 8)], 
+  scale_fill_manual(values = viridis_colors[c(4, 8)], 
                     labels = c("Conditioned female", "Conditioned male", 
                                "Unconditioned female", "Unconditioned male")) +
   theme_classic() +
@@ -37,50 +35,13 @@ reproductive_boxplot_adultstraits <- ggplot(reproductive_adultstraits,
         strip.placement = "outside", 
         strip.background = element_blank(),  
         strip.text = element_text(face = "bold")) +
-  labs(x = "Conditioning", 
-       y = "Number of pupae emerged",
+  labs(x = "Day in experiment when eggs were laid", 
+       y = "Offspring",
        fill = "Treatment",
-         title = "Eggs Laid 22/10 - 25/10") +
-  facet_wrap(~Sex)
+         title = "Female focal") +
+  facet_wrap(~day)
+
+reproductive_boxplot_adultstraits
 
 
-
-#### SECOND ####
-
-## Reading pupae data in
-reproductive_adultstraits.2 <- read_excel("data/fitness_development/reproductive.count.2.xlsx")
-reproductive_adultstraits.2 <- as.data.frame(reproductive_adultstraits.2)
-
-## adding a sex section 
-reproductive_adultstraits.2$Conditioning <- ifelse(grepl("Conditioned", reproductive_adultstraits.2$treatment), "Conditioned", "Unconditioned")
-reproductive_adultstraits.2$Sex <- ifelse(grepl("female", reproductive_adultstraits.2$treatment), "Focal female", "Focal male")
-
-
-# Plot
-# Updated plot
-reproductive_boxplot_adultstraits.2 <- ggplot(reproductive_adultstraits.2, 
-                                            aes(x = Conditioning, y = offspring, fill = treatment)) +
-  geom_boxplot(outlier.shape = NA, alpha = .4, position = position_dodge(width = 0.9)) +
-  geom_point(aes(fill = treatment),
-             size = 1.5, shape = 21,
-             position = position_jitterdodge(jitter.width = 0.3, dodge.width = 0.9)) +
-  scale_fill_manual(values = viridis_colors[c(4, 4, 8, 8)], 
-                    labels = c("Conditioned female", "Conditioned male", 
-                               "Unconditioned female", "Unconditioned male")) +
-  theme_classic() +
-  theme(legend.position = "none",
-        legend.justification = "right",
-        legend.direction = "vertical",
-        strip.placement = "outside", 
-        strip.background = element_blank(),  
-        strip.text = element_text(face = "bold")) +
-  labs(x = "Conditioning", 
-       y = "Number of pupae emerged",
-       fill = "Treatment",
-         title = "Eggs Laid 29/10 - 01/11") +
-  facet_wrap(~Sex)
-
-
-reproductive_boxplot_adultstraits + reproductive_boxplot_adultstraits.2 
-
-
+## attempt 2
