0355239b1f20ce8e1c768baa28723d1cd38236d9
Katie Millar <katiemillar@Katies-MacBook-Pro.local>
2024-12-01 21:00:59 +0000

    Getting rid of code from an old plot.

--- scripts/dietary-fitness/adult_traits/data_visualisation/survival_visualisation.R
+++ scripts/dietary-fitness/adult_traits/data_visualisation/survival_visualisation.R
@@ -50,27 +50,6 @@ lifespan_male<- lifespan %>% filter(Sex == "Focal male")
 summary(coxph(Surv(days_alive, censor) ~ Conditioning, data = lifespan_female))
 summary(coxph(Surv(days_alive, censor) ~ Conditioning, data = lifespan_male))
 
-## Choosing colours from viridis to use: 
-viridis_colors <- viridis(10)
 
-## messing around
-boxplot_adulttraits_survival <- ggplot(lifespan_adultstraits, aes(x = days_alive, y = treatment, fill = treatment)) +
-  geom_boxplot(outlier.shape = NA, alpha = .4, position = position_dodge(width = 0.9)) +
-  geom_point(aes(fill = treatment),
-             size = 1.5, shape = 21,
-             position = position_jitterdodge(jitter.width = 0.3, dodge.width = 0.9)) +
-  scale_fill_manual(values = viridis_colors[c(4, 4, 8, 8)], labels = c("Conditioned", "Unconditioned")) +
-  theme_classic() +
-  theme(legend.position = "none",
-        legend.justification = "right",
-        legend.direction = "vertical",
-        strip.placement = "outside", 
-        strip.background = element_blank(),  
-        strip.text = element_blank()) +
-  labs(x = "Days alive", 
-       y = "Treatment",
-       fill = "Treatment")+
-  coord_flip()
 
-# plot
-boxplot_adulttraits_survival
+
