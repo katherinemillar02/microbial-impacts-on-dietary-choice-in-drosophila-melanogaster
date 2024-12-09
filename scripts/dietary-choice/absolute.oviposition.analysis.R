50899b69ad0be922808c25e5733f7eec3bf5fcdf
Katie Millar <katiemillar@Katies-MacBook-Pro.local>
2024-12-09 20:31:16 +0000

    Fixing dietary choice analysis

--- scripts/dietary-choice/absolute.oviposition.analysis.R
+++ scripts/dietary-choice/absolute.oviposition.analysis.R
@@ -15,61 +15,6 @@ library(MASS)
 ############### 📦📦📦📦
 
 
-#### Male #### 
-
-## Creating a path to get to the data
-pathmaleoviposition <- "data/male_conditioning"
-
-
-################################################################################################################ --
-read_raw_male_oviposition <-function(path = pathmaleoviposition, pattern_to_exclude = "4-1_1-4"){
-  list_of_files <- list.files(path = pathmaleoviposition,
-                              pattern = "oviposition", full.names = T)
-  
-  list_of_files <- list_of_files[!grepl(pattern_to_exclude, list_of_files)]
-  
-  list_of_files %>%
-    map_dfr(~read_excel(.x) %>% mutate(id = .x, .before = "plate")) %>% #.x is the vector you're inputting? so adding a section of id is the name of the data and the folder its in 
-    pivot_longer(cols = c(3:6), ## this will put all the data files into long data, easy to read
-                 names_to = "diet",
-                 values_to = "egg_numbers") %>%
-    drop_na(egg_numbers) ## because the data files are being combined, dropping na where certain data scripts should not be included
-}
-################################################################################################################--
-
-
-## read_raw is the function created, and path shows the path made, so the list of files
-read_raw_male_oviposition(read_raw_male_oviposition)
-
-
-
-## creating an actual data set that will read the paths
-# first data frame - purr package 
-df_male_oviposition <- pathmaleoviposition %>% 
-  map_df(~read_raw_male_oviposition(.x)) #.x is a string or NULL - only applies to dfr apparently
-
-
-
-## Mutating a variable for block 
-df_male_oviposition <- df_male_oviposition %>% 
-  mutate(block = case_when(
-    str_detect(id, "b1") ~ "one",
-    str_detect(id, "b2") ~ "two"
-  ))
-
-
-
-
-# uses what was generated with "df"
-df2_male_oviposition <- df_male_oviposition %>%
-  separate(diet, into = c("ratio", "condition"), sep = " ") %>%#separate will turn a single factor column into multiple columns
-  group_by(id, plate, ratio, condition, block) %>% ## group by what is split
-  summarise(count = sum(egg_numbers)) %>% 
-  pivot_wider(names_from = "condition", values_from = "count") 
-
-## new data 
-df2_male_oviposition # does it recognise condition from the long data? 
-
 
 
 #### Data Analysis ####
@@ -102,66 +47,6 @@ tab_model(glmm.bin.m.ovi.2choice, CSS = list(css.table = '+font-family: Arial;')
 
 
 
-
-
-
-#### Virgin Female ####
-
-## Creating a path to virgin conditioning oviposition data: 
-pathvirginoviposition <- "data/female_conditioning/virgin"
-
-# Reading in Oviposition data for Virgin Conditioning 
-read_raw_virgin_oviposition <-function(path = pathvirginoviposition, pattern_to_exclude = "4-1_1-4_oviposition"){
-  list_of_files <- list.files(path = pathvirginoviposition,
-                              pattern = "oviposition", full.names = T)
-  
-  list_of_files <- list_of_files[!grepl(pattern_to_exclude, list_of_files)]
-  
-  list_of_files %>%
-    map_dfr(~read_excel(.x) %>% mutate(id = .x, .before = "plate")) %>% #.x is the vector you're inputting? so adding a section of id is the name of the data and the folder its in 
-    pivot_longer(cols = c(3:6), ## this will put all the data files into long data, easy to read
-                 names_to = "diet",
-                 values_to = "egg_numbers") %>%
-    drop_na(egg_numbers) ## because the data files are being combined, dropping na where certain data scripts should not be included
-}
-
-
-
-# using the function to find the path 
-read_raw_virgin_oviposition(pathvirginoviposition)
-
-#### Removing "Block 1" from the data-set, as this is not to be used for oviposition. 
-
-
-## Creating a data-set that will read the paths
-# first data frame - purr package 
-df_virgin_oviposition <- pathvirginoviposition %>% 
-  map_df(~read_raw_virgin_oviposition(.x)) #.x is a string or NULL - only applies to dfr apparently
-
-
-## Mutating a variable for block 
-df_virgin_oviposition <- df_virgin_oviposition %>% 
-  mutate(block = case_when(
-    str_detect(id, "b2") ~ "two",
-    str_detect(id, "b3") ~ "three",
-    str_detect(id, "b4") ~ "four",
-  ))
-
-
-# Generating a new data-frame, separating diet. 
-df2_virgin_oviposition <- df_virgin_oviposition %>%
-  separate(diet, into = c("ratio", "condition"), sep = " ") %>% #separate will turn a single factor column into multiple columns
-  group_by(id, plate, ratio, condition, block) %>% ##  group by what is split
-  summarise(count = sum(egg_numbers)) %>% 
-  pivot_wider(names_from = "condition", values_from = "count") 
-
-
-## New data 
-df2_virgin_oviposition # does it recognise condition from the long data
-
-
-
-
 #### Data Analysis ####
 
 # Final chosen model: Binomial GLMM
@@ -190,69 +75,6 @@ emmeans::emmeans(glmm.bin.vf.ovi.2choice, specs = ~ ratio, type = "response")
 
 # Table for write-up
 tab_model(glmm.bin.vf.ovi.2choice, CSS = list(css.table = '+font-family: Arial;'))
-
-
-
-
-
-#### OvoD1 Female #### 
-
-#### Reading the data in ####
-
-
-# Creating a path to the appropriate data files 
-pathovod1oviposition <- "data/female_conditioning/ovod1"
-
-## Creating a function to read the appropriate data files
-######################################################################################################################## --
-read_raw_ovod1_oviposition <-function(path = pathovod1oviposition, pattern_to_exclude = "4-1_1-4"){
-  list_of_files <- list.files(path = pathovod1oviposition,
-                              pattern = "oviposition", full.names = T)
-  
-  list_of_files <- list_of_files[!grepl(pattern_to_exclude, list_of_files)]
-  
-  list_of_files %>%
-    map_dfr(~read_excel(.x) %>% mutate(id = .x, .before = "plate")) %>% #.x is the vector you're inputting? so adding a section of id is the name of the data and the folder its in 
-    pivot_longer(cols = c(3:6), ## this will put all the data files into long data, easy to read
-                 names_to = "diet",
-                 values_to = "egg_numbers") %>%
-    drop_na(egg_numbers) ## because the data files are being combined, dropping na where certain data scripts should not be included
-}
-######################################################################################################################## --
-
-
-
-
-
-## read_raw is the function created, and path shows the path made, so the list of files
-read_raw_ovod1_oviposition(pathovod1oviposition) 
-
-
-## creating an actual data set that will read the paths
-# first data frame - purr package 
-df_ovod1_oviposition <- pathovod1oviposition %>% 
-  map_df(~read_raw_ovod1_oviposition(.x)) #.x is a string or NULL - only applies to dfr apparently
-
-
-## Mutating a variable for block
-df_ovod1_oviposition <- df_ovod1_oviposition %>% 
-  mutate(block = case_when(
-    str_detect(id, "b1") ~ "one",
-    str_detect(id, "b2") ~ "two",
-    
-  ))
-
-
-# uses what was generated with "df"
-df2_ovod1_oviposition <- df_ovod1_oviposition %>%
-  separate(diet, into = c("ratio", "condition"), sep = " ") %>%#separate will turn a single factor column into multiple columns
-  group_by(id, plate, ratio, condition, block) %>% ## group by what is split
-  summarise(count = sum(egg_numbers)) %>% 
-  pivot_wider(names_from = "condition", values_from = "count") 
-
-## new data 
-df2_ovod1_oviposition # does it recognise condition from the long data? 
-
 
 
 
--- scripts/dietary-choice/dietarychoice.dataread.R
+++ scripts/dietary-choice/dietarychoice.dataread.R
@@ -181,6 +181,309 @@ combined_of_split <- combined_of %>%
 
 ####################################################################################################################################################################################################################
 
+#### ABSOLUTE 
+
+
+
+#### Script for analysis 
+# This script contains the actual analysis, with the final chosen models
+
+
+#### Male #### 
+#### Reading data in #####
+
+## Creating a path to the scripts for treatment 2 condtioning
+malepath <- "data/male_conditioning"
+
+
+## This creates a function, that finds the path, excludes the 4-1 and 1-4 assay
+# Mutates a variable for the data file name (id) 
+# Mutates the data frame to have a variable for the amount of flies on a diet and the diet 
+
+############################################################################################################---
+read_raw_male <- function(path = malepath, pattern_to_exclude = "4-1_1-4"){
+  list_of_files <- list.files(path = malepath,
+                              pattern = "rawdata", full.names = T)
+  
+  list_of_files <- list_of_files[!grepl(pattern_to_exclude, list_of_files)]
+  
+  list_of_files %>%
+    map_dfr(~read_excel(.x) %>% mutate(id = .x, .before = "observation")) %>% #.x is the vector you're inputting? so adding a section of id is the name of the data and the folder its in 
+    pivot_longer(cols = c(4:7), ## this will put all the data files into long data, easy to read
+                 names_to = "diet",
+                 values_to = "fly_numbers") %>%
+    drop_na(fly_numbers) ## because the data files are being combined, dropping na where certain data scripts should not be included
+}
+
+
+############################################################################################################-
+
+
+
+
+
+
+## read_raw is the function created, and path shows the path made, so the list of files
+read_raw_male(malepath) # this will show the new data set 
+
+
+## creating an actual data set that will read the paths
+# first data frame - purr package 
+df_male <- malepath %>% 
+  map_df(~read_raw_male(.x)) #.x is a string or NULL - only applies to dfr apparently
+# this will actually give it a label 
+
+# mutating a variable for block from the data id 
+df_male <- df_male %>%
+  mutate(block = case_when(
+    str_detect(id, "b1") ~ "one",
+    str_detect(id, "b2") ~ "two",
+    
+  ))
+
+# Separate diet column and group by relevant variables
+df2_male <- df_male %>%
+  separate(diet, into = c("ratio", "treatment"), sep = " ") %>%
+  group_by(id, observation, plate, ratio, treatment, block) %>%
+  summarise(count = sum(fly_numbers)) %>%
+  pivot_wider(names_from = "treatment", values_from = "count") 
+
+## The data set 
+df2_male
+
+
+
+
+
+
+
+#### Virgin Female ####
+#### Reading data in ####
+
+## Creating a path to get to the Virgin Conditioning data files 
+pathvirgin <- "data/female_conditioning/virgin"
+
+## This creates a function ## 
+# Reads into the path where the Virgin - Conditioning data files are 
+# Creates a pattern to exclude data files that are 4:1 and 1:4 in a four choice assay 
+# Pivots a data frame, to include fly numbers and diet, and excludes na where there would not be numbers in the data frame
+
+
+############################################################################################################-
+read_raw_virgin <-function(path = pathvirgin, pattern_to_exclude = "4-1_1-4"){
+  list_of_files <- list.files(path = pathvirgin,
+                              pattern = "rawresults", full.names = T)
+  
+  list_of_files <- list_of_files[!grepl(pattern_to_exclude, list_of_files)]
+  
+  list_of_files %>%
+    map_dfr(~read_excel(.x) %>% mutate(id = .x, .before = "observation")) %>% #.x is the vector you're inputting? so adding a section of id is the name of the data and the folder its in 
+    pivot_longer(cols = c(4:7), ## this will put all the data files into long data, easy to read
+                 names_to = "diet",
+                 values_to = "fly_numbers") %>%
+    drop_na(fly_numbers) ## because the data files are being combined, dropping na where certain data scripts should not be included
+}
+############################################################################################################-
+
+
+## read_raw is the function created, and path shows the path made, so the list of files
+read_raw_virgin(pathvirgin) 
+
+## creating an actual data set that will read the paths
+# first data frame - purr package 
+df_virgin <- pathvirgin %>% 
+  map_df(~read_raw_virgin(.x)) #.x is a string or NULL - only applies to dfr apparently
+
+# Mutating a variable for block, using the 'id' of the different datasets
+df_virgin <- df_virgin %>% 
+  mutate(block = case_when(
+    str_detect(id, "b1") ~ "one",
+    str_detect(id, "b2") ~ "two",
+    str_detect(id, "b3") ~ "three",
+    str_detect(id, "b4") ~ "four"
+  ))
+
+
+## This separatess diet into ratio and condition as these are currently joined 
+df2_virgin <- df_virgin %>%
+  separate(diet, into = c("ratio", "condition"), sep = " ") %>% # separate will turn a single factor column into multiple columns
+  group_by(id,observation, plate, ratio,condition, block) %>% ## group by what is split
+  summarise(count = sum(fly_numbers)) %>% 
+  pivot_wider(names_from = "condition", values_from = "count")
+
+# The data frame 
+df2_virgin 
+
+
+
+
+#### OvoD1 Female ####
+
+#### Reading data in ####
+
+pathovod1 <- "data/female_conditioning/ovod1"
+
+## This creates  function
+## Path is interchangeable with path 2 
+read_raw_ovod1 <-function(path = pathovod1, pattern_to_exclude = "4-1_1-4"){
+  list_of_files <- list.files(path = pathovod1,
+                              pattern = "rawresults", full.names = T)
+  
+  list_of_files <- list_of_files[!grepl(pattern_to_exclude, list_of_files)]
+  
+  list_of_files %>%
+    map_dfr(~read_excel(.x) %>% mutate(id = .x, .before = "observation")) %>% #.x is the vector you're inputting? so adding a section of id is the name of the data and the folder its in 
+    pivot_longer(cols = c(4:7), ## this will put all the data files into long data, easy to read
+                 names_to = "diet",
+                 values_to = "fly_numbers") %>%
+    drop_na(fly_numbers) ## because the data files are being combined, dropping na where certain data scripts should not be included
+}
+
+## read_raw is the function created, and path shows the path made, so the list of files
+read_raw_ovod1(pathovod1) 
+
+## creating an actual data set that will read the paths
+# first data frame - purr package 
+df_ovod1 <- pathovod1 %>% 
+  map_df(~read_raw_ovod1(.x)) #.x is a string or NULL - only applies to dfr apparently
+
+# Adding block variables
+df_ovod1 <- df_ovod1 %>% 
+  mutate( block = case_when(
+    str_detect(id, "b1") ~ "one",
+    str_detect(id, "b2") ~ "two"
+  ))
+
+# uses what was generated with "df"
+df2_ovod1 <- df_ovod1 %>%
+  separate(diet, into = c("ratio", "condition"), sep = " ") %>%#separate will turn a single factor column into multiple columns
+  group_by(id,observation, plate, ratio,condition, block) %>% ## group by what is split
+  summarise(count = sum(fly_numbers)) %>% 
+  pivot_wider(names_from = "condition", values_from = "count")
+
+
+
+
+
+
+
+#### Virgin Female ####
+
+## Creating a path to virgin conditioning oviposition data: 
+pathvirginoviposition <- "data/female_conditioning/virgin"
+
+# Reading in Oviposition data for Virgin Conditioning 
+read_raw_virgin_oviposition <-function(path = pathvirginoviposition, pattern_to_exclude = "4-1_1-4_oviposition"){
+  list_of_files <- list.files(path = pathvirginoviposition,
+                              pattern = "oviposition", full.names = T)
+  
+  list_of_files <- list_of_files[!grepl(pattern_to_exclude, list_of_files)]
+  
+  list_of_files %>%
+    map_dfr(~read_excel(.x) %>% mutate(id = .x, .before = "plate")) %>% #.x is the vector you're inputting? so adding a section of id is the name of the data and the folder its in 
+    pivot_longer(cols = c(3:6), ## this will put all the data files into long data, easy to read
+                 names_to = "diet",
+                 values_to = "egg_numbers") %>%
+    drop_na(egg_numbers) ## because the data files are being combined, dropping na where certain data scripts should not be included
+}
+
+
+
+# using the function to find the path 
+read_raw_virgin_oviposition(pathvirginoviposition)
+
+#### Removing "Block 1" from the data-set, as this is not to be used for oviposition. 
+
+
+## Creating a data-set that will read the paths
+# first data frame - purr package 
+df_virgin_oviposition <- pathvirginoviposition %>% 
+  map_df(~read_raw_virgin_oviposition(.x)) #.x is a string or NULL - only applies to dfr apparently
+
+
+## Mutating a variable for block 
+df_virgin_oviposition <- df_virgin_oviposition %>% 
+  mutate(block = case_when(
+    str_detect(id, "b2") ~ "two",
+    str_detect(id, "b3") ~ "three",
+    str_detect(id, "b4") ~ "four",
+  ))
+
+
+# Generating a new data-frame, separating diet. 
+df2_virgin_oviposition <- df_virgin_oviposition %>%
+  separate(diet, into = c("ratio", "condition"), sep = " ") %>% #separate will turn a single factor column into multiple columns
+  group_by(id, plate, ratio, condition, block) %>% ##  group by what is split
+  summarise(count = sum(egg_numbers)) %>% 
+  pivot_wider(names_from = "condition", values_from = "count") 
+
+
+## New data 
+df2_virgin_oviposition # does it recognise condition from the long data
+
+
+
+
+#### OvoD1 Female #### 
+
+#### Reading the data in ####
+
+
+# Creating a path to the appropriate data files 
+pathovod1oviposition <- "data/female_conditioning/ovod1"
+
+## Creating a function to read the appropriate data files
+######################################################################################################################## --
+read_raw_ovod1_oviposition <-function(path = pathovod1oviposition, pattern_to_exclude = "4-1_1-4"){
+  list_of_files <- list.files(path = pathovod1oviposition,
+                              pattern = "oviposition", full.names = T)
+  
+  list_of_files <- list_of_files[!grepl(pattern_to_exclude, list_of_files)]
+  
+  list_of_files %>%
+    map_dfr(~read_excel(.x) %>% mutate(id = .x, .before = "plate")) %>% #.x is the vector you're inputting? so adding a section of id is the name of the data and the folder its in 
+    pivot_longer(cols = c(3:6), ## this will put all the data files into long data, easy to read
+                 names_to = "diet",
+                 values_to = "egg_numbers") %>%
+    drop_na(egg_numbers) ## because the data files are being combined, dropping na where certain data scripts should not be included
+}
+######################################################################################################################## --
+
+
+
+
+
+## read_raw is the function created, and path shows the path made, so the list of files
+read_raw_ovod1_oviposition(pathovod1oviposition) 
+
+
+## creating an actual data set that will read the paths
+# first data frame - purr package 
+df_ovod1_oviposition <- pathovod1oviposition %>% 
+  map_df(~read_raw_ovod1_oviposition(.x)) #.x is a string or NULL - only applies to dfr apparently
+
+
+## Mutating a variable for block
+df_ovod1_oviposition <- df_ovod1_oviposition %>% 
+  mutate(block = case_when(
+    str_detect(id, "b1") ~ "one",
+    str_detect(id, "b2") ~ "two",
+    
+  ))
+
+
+# uses what was generated with "df"
+df2_ovod1_oviposition <- df_ovod1_oviposition %>%
+  separate(diet, into = c("ratio", "condition"), sep = " ") %>%#separate will turn a single factor column into multiple columns
+  group_by(id, plate, ratio, condition, block) %>% ## group by what is split
+  summarise(count = sum(egg_numbers)) %>% 
+  pivot_wider(names_from = "condition", values_from = "count") 
+
+## new data 
+df2_ovod1_oviposition # does it recognise condition from the long data? 
+
+
+
 
 
 
