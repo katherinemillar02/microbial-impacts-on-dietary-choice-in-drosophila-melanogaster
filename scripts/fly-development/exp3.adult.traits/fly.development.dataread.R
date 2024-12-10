e8bcb463584b8c16c92a4ff99682e593fbd38b30
Katie Millar <katiemillar@Katies-MacBook-Pro.local>
2024-12-10 14:08:59 +0000

    Making a new data read script

--- scripts/fly-development/exp3.adult.traits/fly.development.dataread.R
+++ scripts/fly-development/exp3.adult.traits/fly.development.dataread.R
@@ -8,3 +8,13 @@ reproductive_adultstraits_m <- read_excel("data/fitness_development/treatment_re
 reproductive_adultstraits_m$day <- as.character(reproductive_adultstraits_m$day)
 
 
+
+
+## Reading the data in:
+lifespan_adultstraits <- read_excel("data/fitness_development/adulttraits_lifespan.xlsx", range = cell_cols("A:E"))
+
+## Adding a sex section:
+lifespan_adultstraits$Conditioning <- ifelse(grepl("Conditioned", lifespan_adultstraits$treatment), "Conditioned", "Unconditioned")
+lifespan_adultstraits$Sex <- ifelse(grepl("female", lifespan_adultstraits$treatment), "Focal female", "Focal male")
+
+fly.dev.3 <- read_excel("data/fitness_development/adulttraits_flydev.xlsx")
