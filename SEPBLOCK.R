f43b7a91c36790bc2ee3e711c3e3eca398cc3ebd
Katie Millar <katiemillar@Katies-MacBook-Pro.local>
2025-01-15 09:10:31 +0000

    Beginning the separate block analysis script

--- SEPBLOCK.R
+++ SEPBLOCK.R
@@ -1,167 +1,59 @@
 
+#### Packages ####
+source("packages.R")
+source("scripts/dietary-choice/dietarychoice.dataread.R")
+#### #### #### #### #### --
+
 
 ## Combined Block Data Visualisation ####
 
-
-## Code will allow one to see each of the plots
-################## --
-## OvoD1 Female ####
-
-
-# 1:4 
-ov1_egg_1_4 <- oviposition_results(onefour_ovi_of, boxplot_fill_colour = viridis_colors[c(9,9)])
-
-# 4:1 
-ov1_egg_4_1 <- oviposition_results(fourone_ovi_of, boxplot_fill_colour = viridis_colors[c(7,7)])
-
-
-ov1_egg_combined <- oviposition_results(combined_ovi_of, boxplot_fill_colour = viridis_colors[c(9, 9, 7, 7)])
-################ --
-
-viridis_colors <- inferno(10)
-
-
-## Using grid.arrange to put the plots together
-ovod1_female_oviposition <- grid.arrange(ov1_egg_1_4, ov1_egg_4_1,
-                                         nrow = 1,
-                                         widths = c(0.5,0.5),
-                                         heights = c(1))
-
-
-ggsave(filename = "ovod1_female_oviposition.png", 
-       plot = ovod1_female_oviposition, 
-       width = 10, 
-       height = 6, 
-       dpi = 300)
-
-ggsave(filename = "ov1_egg_combined.png", 
-       plot = ov1_egg_combined, 
-       width = 10, 
-       height = 6, 
-       dpi = 300)
-
-
-
-
-############################# --
-
-## Wild Type Virgin Female ####
-
-# 1:4 
-v1_egg_1_4  <- oviposition_results(onefour_ovi_vf, boxplot_fill_colour = viridis_colors[c(9,9)])
-
-# 4:1 
-v1_egg_4_1 <- oviposition_results(fourone_ovi_vf, boxplot_fill_colour = viridis_colors[c(7,7)])
-
-# 4:1 and 1:4 
-v1_egg_combined <- oviposition_results(combined_ovi_vf, boxplot_fill_colour = viridis_colors[c(9,9,7,7)])
-
-
-
-
-
-
-## Using grid arrange to put the plots together
-virgin_female_oviposition <- grid.arrange(v1_egg_1_4, v1_egg_4_1 ,
-                                          nrow = 1,
-                                          widths = c(0.5,0.5),
-                                          heights = c(1))
-
-
-
-
-ggsave(filename = "v1_egg_combined.png", 
-       plot = v1_egg_combined, 
-       width = 10, 
-       height = 6, 
-       dpi = 300)
-
-ggsave(filename = "virgin_female_oviposition.png", 
-       plot = virgin_female_oviposition, 
-       width = 10, 
-       height = 6, 
-       dpi = 300)
-
-
-
-
-#################### --
-## Wild type Male ####
-
-# 1:4 
-m_egg_1_4 <- oviposition_results(onefour_ovi_m, boxplot_fill_colour  = viridis_colors[c(9,9)])
-
-# 4:1 
-m_egg_4_1 <- oviposition_results(fourone_ovi_m, boxplot_fill_colour  = viridis_colors[c(7,7)])
-
-# 1:4 and 4:1 
-m_egg_combined <- oviposition_results(combined_ovi_m, boxplot_fill_colour  = viridis_colors[c(9,9,7,7)])
-
-
-
-
-
-## Using grid arrange to put the plots together
-male_oviposition <- grid.arrange(m_egg_1_4, m_egg_4_1,
-                                 nrow = 1,
-                                 widths = c(0.5,0.5),
-                                 heights = c(1))
-
-
-
-ggsave(filename = "male_oviposition.png", 
-       plot = male_oviposition, 
-       width = 10, 
-       height = 6, 
-       dpi = 300)
-
-
-
-ggsave(filename = "m_egg_combined.png", 
-       plot = m_egg_combined, 
-       width = 10, 
-       height = 6, 
-       dpi = 300)
-
-
-
-
-##### ALL THE PLOTS TOGETHER 
-overall_oviposition <- grid.arrange(m_egg_1_4 + ggtitle("Male Conditioning"), m_egg_4_1 + ggtitle("Male Conditioning"), m_egg_combined + ggtitle("Male Conditioning"),
-                                    ov1_egg_1_4 + ggtitle("OvoD1 Female Conditioning"), ov1_egg_4_1 + ggtitle("OvoD1 Female Conditioning"), ov1_egg_combined + ggtitle("OvoD1 Female Conditioning"),
-                                    v1_egg_1_4 + ggtitle("Virgin Female Conditioning"), v1_egg_4_1 + ggtitle("Virgin Female Conditioning"), v1_egg_combined + ggtitle("Virgin Female Conditioning"),
-                                    nrow = 3,
-                                    widths = c(0.5,0.5,1),
-                                    heights = c(1,1,1))
-
-
-
-
-
-
-
-
-
-###### ###### Individual Block Data Visualisation ####
-viridis_colours <- viridis(10)
-## - 
-############ --
-## OvoD1 Female ####
-############ --
-
-
-## Visualising the individual blocks as data shows these to be significant ## 
-
-
-
-
-# For 1:4 OvoD1
+  ## Visualising the individual blocks as data shows these to be significant ## 
+
+
+oviposition_results <- function(data, boxplot_fill_colour) {
+  # Ensure the data has the necessary columns for faceting
+  data$nutrient_composition <- ifelse(grepl("4:1", data$diet), "4:1", "1:4")
+  data$condition <- ifelse(grepl("Conditioned", data$diet), "Conditioned", "Unconditioned")
+  data$combined_factor <- paste(data$nutrient_composition, data$condition, sep = " ")
+  ggplot(data, aes(x = condition, y = egg_numbers, fill = combined_factor, pattern = combined_factor)) + 
+    geom_boxplot(outlier.shape = NA) +
+    geom_boxplot_pattern(position = position_dodge(preserve = "single"),
+                         color = "black",
+                         pattern_fill = "white",
+                         pattern_angle = 45,
+                         pattern_density = 0.1,
+                         pattern_spacing = 0.025,
+                         pattern_key_scale_factor = 0.6,
+                         outlier.shape = NA) +
+    geom_point(aes(),
+               size = 1,
+               shape = 1,
+               position = position_jitterdodge()) +
+    theme_classic() +
+    labs(x = "",
+         y = "Female flies per diet patch", 
+         title = "") +
+    scale_fill_manual(values = boxplot_fill_colour) + 
+    scale_pattern_manual(values = c("circle", "none", "circle", "none")) +
+    theme(legend.position = "none") +
+    ylim(-0.01, 150) +
+    facet_wrap(~ nutrient_composition, scales = "free_x", nrow = 1, strip.position = "bottom") +
+    theme(
+      strip.placement = "outside",
+      strip.background = element_blank(),
+      strip.text = element_text(size = 12)
+    )
+}
+
+
+## Oviposition: 
+# 1:4 OvoD1
 
 # Block 1 
-ovod1_b1_onefour <- oviposition_results(one_four_b1_egg, boxplot_fill_colour = viridis_colours[1:2])
+ovod1_b1_onefour <- oviposition_results(onefour_ovod1_oviposition_b1, boxplot_fill_colour = viridis_colours[1:2])
 
 # Block 2
-ovod1_b2_onefour <- oviposition_results(one_four_b2_egg, boxplot_fill_colour = viridis_colours[1:2])
+ovod1_b2_onefour <- oviposition_results(onefour_ovod1_oviposition_b2, boxplot_fill_colour = viridis_colours[1:2])
 
 
 # For 4:1 OvoD1
