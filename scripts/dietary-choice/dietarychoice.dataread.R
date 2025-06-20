1d06ab5408b30964c68107daf81a4998a4c69938
Katie Millar <katiemillar@Katies-MacBook-Pro.local>
2025-06-20 16:08:10 +0100

    attempting to implement DRY code for reading in datasets - supervised session with Phil

--- scripts/dietary-choice/dietarychoice.dataread.R
+++ scripts/dietary-choice/dietarychoice.dataread.R
@@ -17,117 +17,34 @@ source("packages.R")
 
 ### DIETS CONDITIONED BY MALES #### 
 
-# Block 1 
-fourone_onefour_male_oviposition_b1 <- read_excel("data/male_conditioning/m_4-1_1-4_b1_oviposition.xlsx")
-# Block 1 - Long 
-fourone_onefour_male_oviposition_b1_long <- fourone_onefour_male_oviposition_b1  %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
-
-# Block 2
-fourone_onefour_male_oviposition_b2 <- read_excel("data/male_conditioning/m_4-1_1-4_b2_oviposition.xlsx")
-# Block 2 - Long 
-fourone_onefour_male_oviposition_b2_long <- fourone_onefour_male_oviposition_b2  %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
 
 
-# Mutating a block variable
-fourone_onefour_male_oviposition_b1 <- fourone_onefour_male_oviposition_b1 %>% mutate(block = "one")
-fourone_onefour_male_oviposition_b2 <- fourone_onefour_male_oviposition_b2%>% mutate(block = "two")
+read_diet_data <- function(path, blocks, condition_type, cols = "plate", value = "egg_numbers"){
+  read_excel(path)  %>% 
+  pivot_longer(cols = -all_of(cols), 
+               names_to = "diet", values_to = value) %>% 
+    mutate(block = blocks) %>% 
+    mutate(treatment = condition_type) %>% 
+    separate(diet, into = c("ratio", "condition"), sep = " ") %>% 
+    separate(treatment, into = c("sex", "treatment"))
+}
 
-# Binding the data for 4:1/1:4 
-fourone_onefour_male_oviposition <- rbind(fourone_onefour_male_oviposition_b1, fourone_onefour_male_oviposition_b2)
+# make into a iteration
+paths <- c("data/male_conditioning/m_4-1_1-4_b1_oviposition.xlsx",
+           "data/male_conditioning/m_4-1_1-4_b2_oviposition.xlsx",
+           "data/female_conditioning/virgin/4-1_1-4_oviposition_virgin_b2.xlsx",
+           "data/female_conditioning/virgin/4-1_1-4_oviposition_virgin_b3.xlsx",
+           "data/female_conditioning/virgin/4-1_1-4_oviposition_virgin_b4.xlsx",
+           "data/female_conditioning/ovod1/4-1_1-4_oviposition_ovod1_b1.xlsx",
+           "data/female_conditioning/ovod1/4-1_1-4_oviposition_ovod1_b2.xlsx"
+           )
 
-## Making the data different dataframes
-combined_ovi_m <- fourone_onefour_male_oviposition  %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
-
-## Splitting "diet" up
-combined_ovi_m_split <- combined_ovi_m %>% 
-  separate(diet, into = c("ratio", "condition"), sep = " ")
-
-
-
-
-
-
-
-### DIETS CONDITIONED BY VIRGIN FEMALES #### 
-
-# Block 2 
-fourone_onefour_oviposition_virgin_b2 <- read_excel("data/female_conditioning/virgin/4-1_1-4_oviposition_virgin_b2.xlsx")
-# Block 2 - Long 
-fourone_onefour_oviposition_virgin_b2_long <- fourone_onefour_oviposition_virgin_b2  %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
-
-# Block 3 
-fourone_onefour_oviposition_virgin_b3 <- read_excel("data/female_conditioning/virgin/4-1_1-4_oviposition_virgin_b3.xlsx")
-# Block 3 - Long
-fourone_onefour_oviposition_virgin_b3_long <- fourone_onefour_oviposition_virgin_b3  %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
-
-# Block 4 
-fourone_onefour_oviposition_virgin_b4 <- read_excel("data/female_conditioning/virgin/4-1_1-4_oviposition_virgin_b4.xlsx")
-# Block 4 - Long 
-fourone_onefour_oviposition_virgin_b4_long <- fourone_onefour_oviposition_virgin_b4  %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
+blocks <- c("one", "two", "two", "three", "four", "one", "two")
+condition_type <- c(rep("male_mated", 2), rep("female_virgin", 3), rep("female_ovod1", 2))
 
 
-## Mutating a block variable to the data-sets
-fourone_onefour_oviposition_virgin_b2 <- fourone_onefour_oviposition_virgin_b2 %>% mutate(block = "two")
-fourone_onefour_oviposition_virgin_b3 <- fourone_onefour_oviposition_virgin_b3 %>% mutate(block = "three")
-fourone_onefour_oviposition_virgin_b4 <- fourone_onefour_oviposition_virgin_b4 %>% mutate(block = "four")
-
-## Binding the different data-sets
-fourone_onefour_oviposition_virgin <- rbind(fourone_onefour_oviposition_virgin_b2, fourone_onefour_oviposition_virgin_b3, fourone_onefour_oviposition_virgin_b4)
-
-## adding some data names
-combined_ovi_v <- fourone_onefour_oviposition_virgin  %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
-
-## Using separate to split the diet into rartio and condition 
-combined_ovi_v_split <- combined_ovi_v %>% 
-  separate(diet, into = c("ratio", "condition"), sep = " ")
-
-
-
-
-
-
-
-### DIETS CONDITIONED BY OVOD1 FEMALES #### 
-
-#### Reading, binding, cleaning data 📖 
-## Reading the data in 
-
-# Block 1 
-fourone_onefour_oviposition_ovod1_b1 <- read_excel("data/female_conditioning/ovod1/4-1_1-4_oviposition_ovod1_b1.xlsx")
-# Block 1 - Long 
-fourone_onefour_oviposition_ovod1_b1_long <- fourone_onefour_oviposition_ovod1_b1  %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
-
-# Block 2
-fourone_onefour_oviposition_ovod1_b2 <- read_excel("data/female_conditioning/ovod1/4-1_1-4_oviposition_ovod1_b2.xlsx")
-# Block 2 - Long 
-fourone_onefour_oviposition_ovod1_b2_long <- fourone_onefour_oviposition_ovod1_b2  %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
-
-# Mutating a block variable 
-fourone_onefour_oviposition_ovod1_b1 <- fourone_onefour_oviposition_ovod1_b1  %>% mutate(block = "one")
-fourone_onefour_oviposition_ovod1_b2 <- fourone_onefour_oviposition_ovod1_b2  %>% mutate(block = "two")
-
-# Binding the data 
-fourone_onefour_oviposition_ovod1 <- rbind(fourone_onefour_oviposition_ovod1_b1, fourone_onefour_oviposition_ovod1_b2)
-
-# Making the data long
-fourone_onefour_oviposition_ovod1_long  <- fourone_onefour_oviposition_ovod1 %>% 
-  pivot_longer(cols = ("4:1 Conditioned":"1:4 Unconditioned"), names_to = "diet", values_to = "egg_numbers")
-
-# Splitting up diet variable 
-fourone_onefour_oviposition_ovod1_long_split <- fourone_onefour_oviposition_ovod1_long %>% 
-  separate(diet, into = c("ratio", "condition"), sep = " ")
-
-
-
+four_choice_oviposition <- list_rbind(pmap(list(paths,blocks,condition_type), 
+                                           read_diet_data))
 
 
 
@@ -142,6 +59,11 @@ fourone_onefour_oviposition_ovod1_long_split <- fourone_onefour_oviposition_ovod
 #### Reading, binding and cleaning the data 
 
 ## Using read excel to upload the data (block one and block two)
+
+
+read_diet_data("data/male_conditioning/rawdata_m4-1_1-4_b1.xlsx", "one", "male virgin", 
+               cols = c("plate", "observation"), value = "fly_numbers")
+
 
 # Block 1 
 fourone_onefour_male_b1 <- read_excel("data/male_conditioning/rawdata_m4-1_1-4_b1.xlsx")
