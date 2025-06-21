e6587272b206398bf621b30cb3ced925b1f6ce64
Katie Millar (BIO - Postgraduate Researcher) <ajh20fhu@uea.ac.uk>
2025-06-21 11:27:56 +0100

    Updating changes that were done previously before the new branch was established.

--- fitness.dataread.R
+++ fitness.dataread.R
@@ -1,6 +1,11 @@
 ##### DATA READ 
 
 
+#### What should be here for counts: 
+## Total counts: 
+# Uncontrolled: Pupae, Fly 
+# Controlled: Pupae, Fly 
+
 # PUPAE , UNCONTROLLED
 pupae_fitness_UMFE <- read_excel("data/fitness_development/pupae_data.xlsx")
 # emergence by sex
@@ -32,7 +37,51 @@ overallflies_UMFE <- fly_fitness_tidy_UMFE %>%
 
 
 
-#### A script for reading data in:
+
+
+
+#### FLIES, CONTROLLED 
+
+
+#### Fly Development #### 
+fly_fitness_MFE <- read_excel("data/fitness_development/MFE_flies.xlsx")
+
+## Separating the data into female and male columns 
+fly_fitness_tidy_MFE <- tidyr::pivot_longer(data = fly_fitness_MFE ,
+                                            cols = c( females, males),
+                                            names_to = "sex",
+                                            values_to = "count") 
+
+
+#### Overall Flies Analysis  ####     
+overallflies_MFE <- fly_fitness_tidy_MFE %>%
+  filter(sex %in% c("females", "males")) %>%
+  group_by(vial, sex, treatment, id) %>%
+  summarise(total_count = sum(count, na.rm = TRUE))
+# ungroup() %>%
+# mutate(sex_treatment = paste(treatment, sex, sep = " ")) %>%
+# mutate(sex_treatment = factor(sex_treatment,
+#                               levels = c("conditioned females", "unconditioned females",
+#                                          "conditioned males", "unconditioned males")))
+
+
+
+
+
+#### PUPAE, CONTROLLED - COUNT 
+
+pupae_fitness_MFE <- read_excel("data/fitness_development/MFE_pupae.xlsx")
+
+## This code does something with 0s - not sure if it needed... 
+pupae_fitness_MFE <- pupae_fitness_MFE %>%
+  mutate(pupae = ifelse(is.na(pupae), 0, pupae))
+
+#### Overall Pupae Analysis #### 
+total_pupae <- pupae_fitness_MFE %>% 
+  group_by(id, vial, treatment) %>% 
+  summarise(total_pupae = sum(pupae, na.rm = FALSE))
+
+
 
 
 
@@ -53,6 +102,7 @@ pupae_fitness_MFE_2 <- uncount(pupae_fitness_MFE, pupae)
 
 
 
+
 #### Fly Development #### 
 fly_fitness_MFE <- read_excel("data/fitness_development/MFE_flies.xlsx")
 
@@ -61,6 +111,7 @@ fly_fitness_tidy_MFE <- tidyr::pivot_longer(data = fly_fitness_MFE ,
                                             cols = c( females, males),
                                             names_to = "sex",
                                             values_to = "count") 
+
 
 # Making NAs 0, so this code works 
 fly_fitness_tidy_MFE_filled <- fly_fitness_tidy_MFE %>%
@@ -72,23 +123,6 @@ fly_fitness_tidy_MFE_2 <- uncount(fly_fitness_tidy_MFE_filled, count)
 
 
 
-
-#### Overall Flies Analysis  ####     
-overallflies_MFE <- fly_fitness_tidy_MFE %>%
-  filter(sex %in% c("females", "males")) %>%
-  group_by(vial, sex, treatment, id) %>%
-  summarise(total_count = sum(count, na.rm = TRUE))
-# ungroup() %>%
-# mutate(sex_treatment = paste(treatment, sex, sep = " ")) %>%
-# mutate(sex_treatment = factor(sex_treatment,
-#                               levels = c("conditioned females", "unconditioned females",
-#                                          "conditioned males", "unconditioned males")))
-
-
-#### Overall Pupae Analysis #### 
-total_pupae <- pupae_fitness_MFE %>% 
-  group_by(id, vial, treatment) %>% 
-  summarise(total_pupae = sum(pupae, na.rm = FALSE))
 
 
 
--- roughscrap.R
+++ roughscrap.R
@@ -118,3 +118,76 @@ simulationOutput <- simulateResiduals(fittedModel = glmm.m.4choice, plot = T)
 
 
 
+
+#### Poisson GLMM #### 
+glmm.p.pupae <- glmmTMB(time_hours ~ treatment  + (1| vial), family = poisson, 
+                        data = pupae_fitness_MFE_2)
+
+
+
+#### ASSUMPTION CHECKS:  
+# DHARMa checks 
+plot(simulateResiduals(glmm.p.pupae)) 
+## Does not look great
+# DHARMa assumptions shows this model is poor for both qq and homogeneity 
+# tests are significant for both 
+
+
+## Performance checks - easystats 
+
+# Looking for zeroinflation
+check_zeroinflation(glmm.p.pupae) 
+## There NO is zero inflation, indicating issues may be coming from overdispersion 
+
+# Looking for overdispersion 
+check_overdispersion(glmm.p.pupae) 
+## There is overdispersion, indicating a negative binomial model might be good to test 
+
+
+
+
+
+
+# Model 2
+#### Negative Binomial GLM ####
+glm.nb_pupae <- glm.nb(time_hours ~ 
+                         treatment   
+                       
+                       , data = pupae_fitness_MFE_2)
+
+
+
+#### ASSUMPTION CHECKS 
+# DHARMa checks 
+plot(simulateResiduals(glm.nb_pupae)) 
+## qq looks a lot better, BUT there are still significant P values
+
+## Performance checks
+check_zeroinflation(glm.nb_pupae)
+## No zeroinflation 
+check_overdispersion(glm.nb_pupae) 
+## No overdispersion 
+
+
+
+
+## In this situation, I do not know what to do? 
+
+
+
+
+## Comparing models:
+AIC(glmm.p.pupae,glm.nb_pupae)
+# Negative Binomial GLM is lower, but only slightly
+# It is still not a great model... need to research into what models could be better
+
+
+
+
+
+
+
+
+
+
+
