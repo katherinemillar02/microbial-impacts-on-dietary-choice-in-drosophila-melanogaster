50f12aab4eab3a4dc4aeb1073a8a7d7962874b38
Katie Millar <katiemillar@Katies-MacBook-Pro.local>
2025-07-11 11:28:53 +0100

    Creating metadata files for the dietary choice functions, and beginning to clean the fitness data.

--- scripts/dietary-choice/dietarychoice.dataread.R
+++ scripts/dietary-choice/dietarychoice.dataread.R
@@ -1,102 +1,12 @@
+source("scripts/functions.R")
 
-## Reading in the packages from the packages script.
-source("packages.R")
-
-# Function: read diet data====
-# Description: Reads the dietary-choice feeding data into the R environment
-#
-# Input:
-#   paths - file paths of csv formatted data
-#   blocks - metadata on replication
-#   condition_type - metadata on sex and genotype
-#   cols: columns to be left out of pivot tables
-#   value: response variable - egg numbers or fly counts
-# Output:
-#   R tibble
-#
-
-read_diet_data <- function(path, blocks, condition_type, value, cols = c("plate", "observation")){
-  read_excel(path)  %>% 
-    pivot_longer(cols = -tidyselect::all_of(cols), 
-                 names_to = "diet", values_to = value) %>% 
-    mutate(block = blocks) %>% 
-    mutate(treatment = condition_type) %>% 
-    separate(diet, into = c("ratio", "condition"), sep = " ") %>% 
-    separate(treatment, into = c("sex", "treatment"))
-}
-
-
-#### Improve code further 
-
-# Function: read many data files ====
-# Description: Generates an iterative use of the read data files function
-#
-# Input:
-#   paths - file paths of csv formatted data
-#   blocks - metadata on replication
-#   condition_type - metadata on sex and genotype
-#   value: response variable - egg numbers or fly counts
-#   cols: columns to be left out of pivot tables
-# Output:
-#   Combined csv files into one tibble
-#
-
-read_many_diet_files <- function(paths, blocks, conditions, 
-                                 value, cols) {
-  list_rbind(
-    pmap(list(paths, blocks, conditions), 
-         function(path, block, condition) {
-           read_diet_data(path, block, condition, value, cols)
-         })
-  )
-}
 
 
 ## FOUR CHOICE====
-paths_ovi_4 <- c("data/male_conditioning/m_4-1_1-4_b1_oviposition.xlsx",
-                 "data/male_conditioning/m_4-1_1-4_b2_oviposition.xlsx",
-                 "data/female_conditioning/virgin/4-1_1-4_oviposition_virgin_b2.xlsx",
-                 "data/female_conditioning/virgin/4-1_1-4_oviposition_virgin_b3.xlsx",
-                 "data/female_conditioning/virgin/4-1_1-4_oviposition_virgin_b4.xlsx",
-                 "data/female_conditioning/ovod1/4-1_1-4_oviposition_ovod1_b1.xlsx",
-                 "data/female_conditioning/ovod1/4-1_1-4_oviposition_ovod1_b2.xlsx",
-                 "data/density_experiment/50mm_combined_oviposition_2.xlsx",
-                 "data/density_experiment/90mm_combined_oviposition_2.xlsx")
-
-blocks_ovi_4 <- c("one", "two", "two", "three", "four", "one", "two", "one", "one")
-condition_type_ovi_4 <- c(rep("male_mated", 2), rep("female_virgin", 3), 
-                          rep("female_ovod1", 2), rep("male_50mm", 1), rep("male_90mm", 1))
-
-
+four_choice_oviposition_metadata <- read_csv("data/four_choice_oviposition_metadata.csv")
 
 ## TWO CHOICE====
-paths_ovi_2 <- c("data/male_conditioning/m_4-1_b1_oviposition.xlsx",
-                 "data/male_conditioning/m_4-1_b2_oviposition.xlsx",
-                 "data/male_conditioning/m_1-4_b1_oviposition.xlsx",
-                 "data/male_conditioning/m_1-4_b2_oviposition.xlsx",
-                 "data/female_conditioning/virgin/1-4_oviposition_virgin_b2.xlsx",
-                 "data/female_conditioning/virgin/1-4_oviposition_virgin_b3.xlsx",
-                 "data/female_conditioning/virgin/1-4_oviposition_virgin_b4.xlsx",
-                 "data/female_conditioning/virgin/4-1_oviposition_virgin_b2.xlsx",
-                 "data/female_conditioning/virgin/4-1_oviposition_virgin_b3.xlsx",
-                 "data/female_conditioning/virgin/4-1_oviposition_virgin_b4.xlsx",
-                 "data/female_conditioning/ovod1/1-4_oviposition_ovod1_b1.xlsx",
-                 "data/female_conditioning/ovod1/1-4_oviposition_ovod1_b2.xlsx",
-                 "data/female_conditioning/ovod1/4-1_oviposition_ovod1_b1.xlsx",
-                 "data/female_conditioning/ovod1/4-1_oviposition_ovod1_b2.xlsx", 
-                 "data/density_experiment/50mm_4-1_oviposition_2.xlsx",
-                 "data/density_experiment/50mm_1-4_oviposition_2.xlsx",
-                 "data/density_experiment/90mm_4-1_oviposition_2.xlsx",
-                 "data/density_experiment/90mm_1-4_oviposition_2.xlsx")
-
-
-
-blocks_ovi_2 <- c("one", "two","one", "two", "two", "three", 
-                  "four","two", "three", "four", "one", "two",
-                  "one", "two", "one", "one","one","one")
-
-condition_type_ovi_2 <- c(rep("male_mated", 4), rep("female_virgin", 6), 
-                          rep("female_ovod1", 4), rep("male_density_50mm", 2), rep("male_density_90mm", 2))
+two_choice_oviposition_metadata <- read_csv("data/two_choice_oviposition_metadata.csv")
 
 
 ## FEEDING EXPERIMENTS==== 
@@ -155,16 +65,16 @@ condition_type2 <- c(rep("male_mated", 4), rep("female_virgin", 8), rep("female_
 
 
 data_configs <- list(
-  four_choice_oviposition = list(paths = paths_ovi_4,
-                                 blocks = blocks_ovi_4,
-                                 conditions = condition_type_ovi_4,
+  four_choice_oviposition = list(paths = four_choice_oviposition_metadata$paths_ovi_4,
+                                 blocks = four_choice_oviposition_metadata$blocks_ovi_4,
+                                 conditions = four_choice_oviposition_metadata$condition_type_ovi_4,
                                  value = "egg_numbers",
                                  cols = "plate"
   ),
   
-  two_choice_oviposition = list(paths = paths_ovi_2,
-                                blocks = blocks_ovi_2,
-                                conditions = condition_type_ovi_2,
+  two_choice_oviposition = list(paths = two_choice_oviposition_metadata$paths_ovi_2,
+                                blocks = two_choice_oviposition_metadata$blocks_ovi_2,
+                                conditions = two_choice_oviposition_metadata$condition_type_ovi_2,
                                 value = "egg_numbers",
                                 cols = "plate"
   ),
