7afa8e84c363e101c99136f16712d55c6700e578
Katie Millar (BIO - Postgraduate Researcher) <ajh20fhu@uea.ac.uk>
2025-07-16 16:51:26 +0100

    Adding read in data from functions for the development and total count data.

--- scripts/fly-development/fitness_dataread.R
+++ scripts/fly-development/fitness_dataread.R
@@ -1,14 +1,10 @@
 # Load the packages script. 
 source("scripts/packages.R")
 
-
-
 ##### DATA READ - FOR FLY FITNESS 
 ## Controlled and Uncontrolled density experiments. 
 
-
-
- # FUNCTIONS ====
+            # FUNCTIONS ====
 
 ## GLOSSARY: 
 
@@ -17,205 +13,61 @@ source("scripts/packages.R")
    # purr - a package that provides tools for working with functions and vectors. 
    # imap_dfr - imap will give both the values and names, looped together and dfr will combine everything into a single data frame 
 # all_of - will make sex into one column? 
-
-
-
-
 # path
 # timecourse_data()
 # al_timecourse_data()
 
 
-
-
-# Adult Development for both controlled and uncontrolled densities
-
-# setting file paths for the two datasets, one from the controlled experiment, and one from the uncontrolled experiment. 
+## Setting file paths of adult data 
 paths_adult <- c(uncontrolled = "data/fitness_development/fly_data.xlsx", 
                  controlled = "data/fitness_development/MFE_flies.xlsx")
 
-# reading the excel files
-timecourse_data <- function(path, sex_cols = c("females", "males"), cols = "vial") {
-  read_excel(path) %>% 
-    pivot_longer(cols = all_of(sex_cols),  # making the sex columns into one column
-                 names_to = "sex",
-                 values_to = "count") %>% 
-    drop_na(count) %>% 
-    uncount(count)
-}
-
-
-# combining paths with the written function 
-all_timecourse_data <- purrr::imap_dfr(paths_adult, ~ 
-                                   timecourse_data(.x) %>% 
-                                   mutate(density_condition = .y)) # mutating a function called density_conditioning, based on what the paths were called above. 
-
-
-
-
-# Pupa Development for both controlled and uncontrolled densities
-
+## Setting file paths of pupa data
 paths_pupa <- c(uncontrolled = "data/fitness_development/pupae_data.xlsx", ## There are two pupa data files, (2) is for visualisation purposes.
-                 controlled = "data/fitness_development/MFE_pupae.xlsx")
+                controlled = "data/fitness_development/MFE_pupae.xlsx")
 
 
 
-timecourse_data_p <- function(path) {
-  read_excel(path) %>%
-    drop_na(pupae) %>%
-    uncount(pupae)
-}
+# adults
+timecourse_adult <- imap_dfr(paths_adult, ~ timecourse_data(.x, count_cols = c("females", "males")) %>%
+                               mutate(condition = .y))
+
+#  pupa
+timecourse_pupae <- imap_dfr(paths_pupa, ~ timecourse_data(.x, count_cols = c("pupae")) %>% 
+                               mutate(condition = .y))
 
 
-all_timecourse_data_p <- purrr::imap_dfr(paths_pupa,  ~ 
-                                         timecourse_data_p(.x) %>% 
-                                         mutate(density_condition = .y))
+# adult
+totalcount_adult <- imap_dfr(paths_adult, ~ totalcount_data(.x, count_cols = c("females", "males")) %>%
+                               mutate(condition = .y))
+
+# pupa
+totalcount_pupa <- imap_dfr(paths_adult, ~ totalcount_data(.x, count_cols = c("females", "males")) %>%
+                               mutate(condition = .y))
 
 
-# END OF FUNCTIONS === ==== ==== ==== =====
-
-
-
-
-
-
-
-
-
-##### ##### CUREENT DATA READ IN ===
-# This is the current data read in before it has been turned into a function. 
-# I am keeping it here for now in case anything I have been working on fails.
-
-
-
-
-
-##### UNCONTROLLED DATA ============================================
-
-## Load data 
-pupa_uc <- read_excel("data/fitness_development/pupae_data.xlsx") # Pupae
-adult_uc <- read_excel("data/fitness_development/fly_data.xlsx") # Flies
- 
-
-## Pupae - Development and Total Count ====
-
-# Development with count
-pupa_timecourse_uc <- pupa_uc %>%
-  rename(count = pupae) %>%
-  drop_na(count)
-
-# Development with uncount
-pupa_timecourse_uc_uncount <- uncount(pupa_timecourse_uc, count)
-
-# Total count 
-pupa_total_uc <- pupa_timecourse_uc %>%
-  group_by(vial, treatment) %>%
-  summarise(total_count = sum(count, na.rm = FALSE)) %>%
-  ungroup()
-
-
-## Adults - Development and Total Count ====
-
-# Development with count
-adult_timecourse_uc <- adult_uc %>%
-  pivot_longer(cols = c(females, males),
-               names_to = "sex",
-               values_to = "count" )%>%
-  drop_na(count)
-
-# Development with uncount
-adult_timecourse_uc_uncount  <- uncount(adult_timecourse_uc, count)
-
-# Total count 
-adult_total_uc <- adult_timecourse_uc %>% 
-  group_by(vial, treatment, sex) %>%
-  summarise(total_count = sum(count, na.rm = TRUE))
 
 
 
 ## Body Weight of Adults ==  
+# Uncontrolled
 adult_bodyweight_uc <- read_excel("data/fitness_development/bodyweight_flies2.xlsx")
-
-# Convert weight to micrograms
 adult_bodyweight_uc$weight_mg <- adult_bodyweight_uc$weight_mg * 1000
-
-
-
-
-
-
-##### CONTROLLED DATA ============================================
-
-## Load data 
-pupa_c <- read_excel("data/fitness_development/MFE_pupae.xlsx") # Pupae 
-adult_c <- read_excel("data/fitness_development/MFE_flies.xlsx") # Flies
-
-
-## Pupae - Development and Total Count ====
-
-# Development with count
-pupa_timecourse_c <- pupa_c %>%
-  rename(count = pupae) %>%
-  drop_na(count)
-
-# Development with uncount
-pupa_timecourse_c_uncount <- uncount(pupa_timecourse_c, count)
-
-# Total count 
-pupa_total_c <- pupa_timecourse_c %>%
-  group_by(vial, treatment) %>%
-  summarise(total_pupa = sum(count, na.rm = FALSE)) %>%
-  ungroup()
-
-
-
-
-
-
-
-
-## Adults - Development and Total Count ====
-
-# Development with count
-adult_timecourse_c <- adult_c %>%
-  pivot_longer(cols = c(females, males),
-               names_to = "sex",
-               values_to = "count" )%>%
-  drop_na(count)
-
-# Development with uncount
-pupa_timecourse_c_uncount  <- uncount(adult_timecourse_c, count)
-
-# Total count 
-adult_total_c <- adult_timecourse_c %>% 
-  group_by(vial, treatment, sex) %>%
-  summarise(total_adult = sum(count, na.rm = TRUE))
-
-
-## Adult Body Weight
+# Controlled 
 bodyweight_MFE <- read_excel("data/fitness_development/weighing_body.xlsx")
 bodyweight_MFE$weight_mg <- bodyweight_MFE$weight_mg * 1000
 
 
-
-
-
-
-
 ## Survivabiliy ====
-
 # Larvae - Pupae 
 lv_pup_c <- pupa_total_c %>%
   mutate(fixed_total = 63,
     survivability = (total_pupa / fixed_total) * 100)
-
 # Pupae - Adult 
 pup_adult <- pupa_total_c %>%
   left_join(adult_total_c, by = c("vial", "treatment"))
-
 pup_adult_c <- pup_adult %>%
   mutate(survivability = (total_adult / total_pupa) * 100)
-
 # Larvae to Fly 
 fly_survivability <- adult_total_c %>%
   mutate(
